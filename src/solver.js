2fea2074d3ec203490763e205e9622674135f45a
vtsukur <volodymyrt@wix.com>
2022-11-01 15:44:57 +0200

    Quick return on sum below min

--- src/solver.js
+++ src/solver.js
@@ -1,17 +1,20 @@
 import _ from 'lodash';
 import { GRID_SIDE_LENGTH } from './problem';
 
+const MIN_SUMS_PER_COUNT = new Array(GRID_SIDE_LENGTH);
 const MAX_SUMS_PER_COUNT = new Array(GRID_SIDE_LENGTH);
 _.range(0, GRID_SIDE_LENGTH).forEach(count => {
     if (count == 0) {
+        MIN_SUMS_PER_COUNT[count] = 1;
         MAX_SUMS_PER_COUNT[count] = GRID_SIDE_LENGTH;
     } else {
+        MIN_SUMS_PER_COUNT[count] = MIN_SUMS_PER_COUNT[count - 1] + (count + 1);
         MAX_SUMS_PER_COUNT[count] = MAX_SUMS_PER_COUNT[count - 1] + (GRID_SIDE_LENGTH - count);
     }
 })
 
 export function digitSetsForSum(sum, count) {
-    if (sum <= 0 || count <= 0 || sum > MAX_SUMS_PER_COUNT[count]) {
+    if (sum <= 0 || count <= 0 || sum < MIN_SUMS_PER_COUNT[count - 1] || sum > MAX_SUMS_PER_COUNT[count - 1]) {
         return [];
     }
 
