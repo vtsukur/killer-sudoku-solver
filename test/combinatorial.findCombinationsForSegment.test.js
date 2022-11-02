e32481ae81514febb2deca9d24c475f5f166e5e8
vtsukur <volodymyrt@wix.com>
2022-11-02 23:38:10 +0200

    More tests

--- test/combinatorial.findCombinationsForSegment.test.js
+++ test/combinatorial.findCombinationsForSegment.test.js
@@ -3,7 +3,7 @@ import { findCombinationsForSegment } from '../src/combinatorial';
 import { Cell, Sum } from '../src/problem';
 
 describe('Tests for the finder of digit combinations to form a segment out of sums', () => {
-    test('Multiple combinations of digits to form a segment', () => {
+    test('Multiple combinations of digits to form a complete segment', () => {
         expect(findCombinationsForSegment([
             new Sum(15, [ new Cell(1, 1), new Cell(1, 2)]),
             new Sum(10, [ new Cell(1, 3), new Cell(2, 3)]),
@@ -18,7 +18,7 @@ describe('Tests for the finder of digit combinations to form a segment out of su
         ]);
     });
 
-    test('Single combination of digits to form a segment', () => {
+    test('Single combination of digits to form a complete segment', () => {
         expect(findCombinationsForSegment([
             new Sum(4, [ new Cell(1, 1), new Cell(1, 2)]),
             new Sum(24, [ new Cell(1, 3), new Cell(1, 4), new Cell(1, 5)]),
@@ -26,6 +26,16 @@ describe('Tests for the finder of digit combinations to form a segment out of su
             new Sum(10, [ new Cell(1, 8), new Cell(1, 9)])
         ])).toEqual([
             [ new Set([1, 3]), new Set([7, 8, 9]), new Set([2, 5]), new Set([4, 6]) ]
+        ]);
+    });
+
+    test('Combinations of digits to form an incomplete segment', () => {
+        expect(findCombinationsForSegment([
+            new Sum(4, [ new Cell(1, 1), new Cell(1, 2)]),
+            new Sum(9, [ new Cell(1, 6), new Cell(1, 7)])
+        ])).toEqual([
+            [ new Set([1, 3]), new Set([2, 7]) ],
+            [ new Set([1, 3]), new Set([4, 5]) ]
         ]);
     });
 });
