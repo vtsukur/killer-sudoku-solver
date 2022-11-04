66688e154bfeb84d5aca52d5f9cfeb5517d59312
vtsukur <volodymyrt@wix.com>
2022-11-04 16:32:01 +0200

    More tests for subgrid initialization

--- test/solver.test.js
+++ test/solver.test.js
@@ -248,5 +248,69 @@ describe('Tests for solver steps', () => {
                 new Sum(13, [ new Cell(3, 1), new Cell(3, 2), new Cell(3, 3) ])
             ]
         ));
+        expect(Subgrid.createWithLeftoverSum(2, solverModel)).toEqual(new Subgrid(
+            2, [
+                new Sum(17, [ new Cell(1, 4), new Cell(2, 4) ]),
+                new Sum(13, [ new Cell(1, 5), new Cell(1, 6), new Cell(2, 5) ]),
+                new Sum(11, [ new Cell(3, 4), new Cell(3, 5) ]),
+                new Sum(4, [ new Cell(2, 6), new Cell(3, 6) ])
+            ]
+        ));
+        expect(Subgrid.createWithLeftoverSum(3, solverModel)).toEqual(new Subgrid(
+            3, [
+                new Sum(7, [ new Cell(1, 7), new Cell(1, 8) ]),
+                new Sum(11, [ new Cell(1, 9), new Cell(2, 9) ]),
+                new Sum(9, [ new Cell(3, 8), new Cell(3, 9) ]),
+                new Sum(18, [ new Cell(2, 7), new Cell(2, 8), new Cell(3, 7) ])
+            ]
+        ));
+        expect(Subgrid.createWithLeftoverSum(4, solverModel)).toEqual(new Subgrid(
+            4, [
+                new Sum(4, [ new Cell(4, 1), new Cell(4, 2) ]),
+                new Sum(2, [ new Cell(4, 3) ]),
+                new Sum(27, [ new Cell(5, 1), new Cell(5, 2), new Cell(6, 1), new Cell(6, 2) ]),
+                new Sum(12, [ new Cell(5, 3), new Cell(6, 3) ])
+            ]
+        ));
+        expect(Subgrid.createWithLeftoverSum(5, solverModel)).toEqual(new Subgrid(
+            5, [
+                new Sum(14, [ new Cell(4, 4), new Cell(4, 5) ]),
+                new Sum(10, [ new Cell(5, 4), new Cell(5, 5) ]),
+                new Sum(21, [ new Cell(4, 6), new Cell(5, 6),
+                    new Cell(6, 4), new Cell(6, 5), new Cell(6, 6) ])
+            ]
+        ));
+        expect(Subgrid.createWithLeftoverSum(6, solverModel)).toEqual(new Subgrid(
+            6, [
+                new Sum(5, [ new Cell(4, 8), new Cell(5, 8) ]),
+                new Sum(19, [ new Cell(4, 9), new Cell(5, 9), new Cell(6, 9) ]),
+                new Sum(21, [ new Cell(4, 7), new Cell(5, 7),
+                    new Cell(6, 7), new Cell(6, 8) ])
+            ]
+        ));
+        expect(Subgrid.createWithLeftoverSum(7, solverModel)).toEqual(new Subgrid(
+            7, [
+                new Sum(19, [ new Cell(7, 1), new Cell(8, 1), new Cell(9, 1) ]),
+                new Sum(14, [ new Cell(7, 2), new Cell(8, 2), new Cell(9, 2), new Cell(9, 3) ]),
+                new Sum(12, [ new Cell(7, 3), new Cell(8, 3) ])
+            ]
+        ));
+        expect(Subgrid.createWithLeftoverSum(8, solverModel)).toEqual(new Subgrid(
+            8, [
+                new Sum(6, [ new Cell(7, 5), new Cell(7, 6) ]),
+                new Sum(6, [ new Cell(8, 4), new Cell(9, 4) ]),
+                new Sum(22, [ new Cell(8, 5), new Cell(9, 5), new Cell(9, 6) ]),
+                new Sum(8, [ new Cell(8, 6) ]),
+                new Sum(3, [ new Cell(7, 4) ])
+            ]
+        ));
+        expect(Subgrid.createWithLeftoverSum(9, solverModel)).toEqual(new Subgrid(
+            9, [
+                new Sum(14, [ new Cell(7, 9), new Cell(8, 9), new Cell(9, 9) ]),
+                new Sum(10, [ new Cell(8, 7), new Cell(8, 8) ]),
+                new Sum(7, [ new Cell(9, 7), new Cell(9, 8) ]),
+                new Sum(14, [ new Cell(7, 7), new Cell(7, 8) ])
+            ]
+        ));
     });
 });
