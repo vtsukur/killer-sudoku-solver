5a8088e6cd21a0e0f8a14689cd47964634545cd0
vtsukur <volodymyrt@wix.com>
2022-11-05 21:10:21 +0200

    Tests for sum

---
+++ test/problem.sum.test.js
@@ -0,0 +1,38 @@
+import _ from 'lodash';
+import { Sum, Cell } from '../src/problem';
+
+describe('Sum tests', () => {
+    test('Sum within row', () => {
+        expect(new Sum(10, [ new Cell(4, 4) ]).isWithinRow).toBe(true);
+        expect(new Sum(10, [ new Cell(4, 4), new Cell(4, 5) ]).isWithinRow).toBe(true);
+    });
+
+    test('Sum not within row', () => {
+        expect(new Sum(10, [ new Cell(4, 4), new Cell(5, 4) ]).isWithinRow).toBe(false);
+    });
+
+    test('Sum within column', () => {
+        expect(new Sum(10, [ new Cell(5, 5) ]).isWithinColumn).toBe(true);
+        expect(new Sum(10, [ new Cell(5, 5), new Cell(6, 5) ]).isWithinColumn).toBe(true);
+    });
+
+    test('Sum not within column', () => {
+        expect(new Sum(10, [ new Cell(5, 5), new Cell(5, 6) ]).isWithinColumn).toBe(false);
+    });
+
+    test('Sum within subgrid', () => {
+        expect(new Sum(10, [ new Cell(6, 6) ]).isWithinSubgrid).toBe(true);
+        expect(new Sum(10, [ new Cell(6, 6), new Cell(6, 7), new Cell(6, 8) ]).isWithinSubgrid).toBe(true);
+    });
+
+    test('Sum not within subgrid', () => {
+        expect(new Sum(10, [ new Cell(5, 6), new Cell(6, 6) ]).isWithinSubgrid).toBe(false);
+    });
+
+    test('Sum not within row neither column nor subgrid', () => {
+        const sum = new Sum(10, [ new Cell(5, 6), new Cell(6, 6), new Cell(6, 5) ]);
+        expect(sum.isWithinRow).toBe(false);
+        expect(sum.isWithinColumn).toBe(false);
+        expect(sum.isWithinSubgrid).toBe(false);
+    });
+});
