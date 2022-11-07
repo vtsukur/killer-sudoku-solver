6b806f98cccc5aae9fb18411173afe29f4438807
vtsukur <volodymyrt@wix.com>
2022-11-07 15:40:09 +0200

    Adding more tests for slicing

--- test/solver.test.js
+++ test/solver.test.js
@@ -126,5 +126,53 @@ describe('Tests for solver', () => {
             Sum.of(1).cell(2, 8).mk(),
             Sum.of(8).cell(2, 7).mk()
         ]));
+
+        const subgrid3 = solver.subgrid(3);
+        expect(new Set(subgrid3.sums)).toEqual(new Set([
+            Sum.of(4).cell(3, 0).cell(3, 1).mk(),
+            Sum.of(2).cell(3, 2).mk(),
+            Sum.of(27).cell(4, 0).cell(4, 1).cell(5, 0).cell(5, 1).mk(),
+            Sum.of(12).cell(4, 2).cell(5, 2).mk()
+        ]));
+
+        const subgrid4 = solver.subgrid(4);
+        expect(new Set(subgrid4.sums)).toEqual(new Set([
+            Sum.of(14).cell(3, 3).cell(3, 4).mk(),
+            Sum.of(10).cell(4, 3).cell(4, 4).mk(),
+            Sum.of(2).cell(5, 3).mk(),
+            Sum.of(19).cell(3, 5).cell(4, 5).cell(5, 4).cell(5, 5).mk()
+        ]));
+
+        const subgrid5 = solver.subgrid(5);
+        expect(new Set(subgrid5.sums)).toEqual(new Set([
+            Sum.of(5).cell(3, 7).cell(4, 7).mk(),
+            Sum.of(19).cell(3, 8).cell(4, 8).cell(5, 8).mk(),
+            Sum.of(8).cell(5, 6).cell(5, 7).mk(),
+            Sum.of(13).cell(3, 6).cell(4, 6).mk()
+        ]));
+
+        const subgrid6 = solver.subgrid(6);
+        expect(new Set(subgrid6.sums)).toEqual(new Set([
+            Sum.of(19).cell(6, 0).cell(7, 0).cell(8, 0).mk(),
+            Sum.of(14).cell(6, 1).cell(7, 1).cell(8, 1).cell(8, 2).mk(),
+            Sum.of(12).cell(6, 2).cell(7, 2).mk()
+        ]));
+
+        const subgrid7 = solver.subgrid(7);
+        expect(new Set(subgrid7.sums)).toEqual(new Set([
+            Sum.of(3).cell(6, 3).mk(),
+            Sum.of(6).cell(6, 4).cell(6, 5).mk(),
+            Sum.of(6).cell(7, 3).cell(8, 3).mk(),
+            Sum.of(22).cell(7, 4).cell(8, 4).cell(8, 5).mk(),
+            Sum.of(8).cell(7, 5).mk()
+        ]));
+
+        const subgrid8 = solver.subgrid(8);
+        expect(new Set(subgrid8.sums)).toEqual(new Set([
+            Sum.of(14).cell(6, 6).cell(6, 7).mk(),
+            Sum.of(14).cell(6, 8).cell(7, 8).cell(8, 8).mk(),
+            Sum.of(10).cell(7, 6).cell(7, 7).mk(),
+            Sum.of(7).cell(8, 6).cell(8, 7).mk()
+        ]));
     });
 });
