3e8853b6625a62cb8ec409c3fdf6b2ae68fa1e5e
Volodymyr Tsukur <volodymyrt@wix.com>
2022-11-18 21:18:48 +0200

    Solving yet another problem

--- test/realKillerSudokuProblems.js
+++ test/realKillerSudokuProblems.js
@@ -42,6 +42,48 @@ export const killerSudokuBySudokuDotCom_2022_04_06 = new Problem([
 ]);
 killerSudokuBySudokuDotCom_2022_04_06.checkCorrectness();
 
+export const killerSudokuBySudokuDotCom_2022_08_12 = new Problem([
+    // upper subgrids
+    Sum.of(6).cell(0, 0).cell(1, 0).mk(),
+    Sum.of(12).cell(0, 1).cell(1, 1).mk(),
+    Sum.of(20).cell(2, 0).cell(3, 0).cell(4, 0).mk(),
+    Sum.of(11).cell(0, 2).cell(0, 3).mk(),
+    Sum.of(10).cell(1, 2).cell(1, 3).mk(),
+    Sum.of(18).cell(2, 1).cell(2, 2).cell(2, 3).mk(),
+    Sum.of(17).cell(0, 4).cell(0, 5).cell(0, 6).mk(),
+    Sum.of(7).cell(1, 4).cell(1, 5).mk(),
+    Sum.of(14).cell(2, 4).cell(3, 4).mk(),
+    Sum.of(5).cell(2, 5).cell(2, 6).mk(),
+    Sum.of(13).cell(0, 7).cell(0, 8).mk(),
+    Sum.of(14).cell(1, 6).cell(1, 7).cell(1, 8).mk(),
+    Sum.of(10).cell(2, 7).cell(2, 8).mk(),
+
+    // middle subgrids
+    Sum.of(3).cell(3, 1).cell(4, 1).mk(),
+    Sum.of(12).cell(3, 2).cell(4, 2).mk(),
+    Sum.of(13).cell(5, 0).cell(6, 0).cell(6, 1).mk(),
+    Sum.of(21).cell(5, 1).cell(5, 2).cell(5, 3).cell(6, 2).mk(),
+    Sum.of(10).cell(3, 3).cell(4, 3).mk(),
+    Sum.of(8).cell(4, 4).cell(5, 4).mk(),
+    Sum.of(20).cell(3, 5).cell(3, 6).cell(3, 7).cell(3, 8).mk(),
+    Sum.of(11).cell(4, 5).cell(4, 6).mk(),
+    Sum.of(14).cell(5, 5).cell(6, 5).mk(),
+    Sum.of(14).cell(4, 7).cell(5, 6).cell(5, 7).mk(),
+    Sum.of(12).cell(4, 8).cell(5, 8).cell(6, 8).mk(),
+
+    // lower subgrids
+    Sum.of(22).cell(7, 0).cell(7, 1).cell(8, 0).cell(8, 1).mk(),
+    Sum.of(10).cell(7, 2).cell(8, 2).mk(),
+    Sum.of(15).cell(6, 3).cell(6, 4).cell(7, 3).mk(),
+    Sum.of(8).cell(8, 3).cell(8, 4).mk(),
+    Sum.of(17).cell(7, 4).cell(7, 5).cell(7, 6).mk(),
+    Sum.of(15).cell(8, 5).cell(8, 6).cell(8, 7).mk(),
+    Sum.of(4).cell(6, 6).mk(),
+    Sum.of(12).cell(6, 7).cell(7, 7).mk(),
+    Sum.of(7).cell(7, 8).cell(8, 8).mk()
+]);
+killerSudokuBySudokuDotCom_2022_08_12.checkCorrectness();
+
 export const killerSudokuBySudokuDotCom_2022_10_18 = new Problem([
     // upper subgrids
     Sum.of(8).cell(0, 0).cell(0, 1).mk(),
--- test/solver.test.js
+++ test/solver.test.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 import { Cell, Sum, GRID_CELL_COUNT, UNIQUE_SEGMENT_COUNT, UNIQUE_SEGMENT_LENGTH } from '../src/problem';
 import { Solver } from '../src/solver';
-import { killerSudokuBySudokuDotCom_2022_04_06, killerSudokuBySudokuDotCom_2022_10_18, killerSudokuBySudokuDotCom_2022_10_19, killerSudokuBySudokuDotCom_2022_10_22, killerSudokuBySudokuDotCom_2022_10_25, killerSudokuBySudokuDotCom_2022_11_01, killerSudokuBySudokuDotCom_2022_11_10, killerSudokuBySudokuDotCom_expertLevel_1 } from './realKillerSudokuProblems';
+import { killerSudokuBySudokuDotCom_2022_04_06, killerSudokuBySudokuDotCom_2022_08_12, killerSudokuBySudokuDotCom_2022_10_18, killerSudokuBySudokuDotCom_2022_10_19, killerSudokuBySudokuDotCom_2022_10_22, killerSudokuBySudokuDotCom_2022_10_25, killerSudokuBySudokuDotCom_2022_11_01, killerSudokuBySudokuDotCom_2022_11_10, killerSudokuBySudokuDotCom_expertLevel_1 } from './realKillerSudokuProblems';
 
 describe('Tests for solver', () => {
     test('Create solver (shallow coverage)', () => {
@@ -44,6 +44,23 @@ describe('Tests for solver', () => {
             [ 2, 9, 1, 5, 3, 8, 4, 7, 6 ],
             [ 7, 4, 3, 1, 6, 2, 8, 5, 9 ],
             [ 6, 5, 8, 4, 7, 9, 2, 1, 3 ]
+        ]);
+    });
+
+    test('Find solution (Killer Sudoku by Sudoku.com - Daily Challenge 2022-08-12)', () => {
+        const solver = new Solver(killerSudokuBySudokuDotCom_2022_08_12);
+        const solution = solver.solve();
+
+        expect(solution).toEqual([
+            [ 1, 3, 2, 9, 4, 7, 6, 8, 5 ],
+            [ 5, 9, 8, 2, 1, 6, 7, 3, 4 ],
+            [ 7, 6, 4, 8, 5, 3, 2, 1, 9 ],
+            [ 4, 2, 7, 3, 9, 1, 5, 6, 8 ],
+            [ 9, 1, 5, 7, 6, 8, 3, 4, 2 ],
+            [ 6, 8, 3, 4, 2, 5, 1, 9, 7 ],
+            [ 2, 5, 6, 1, 8, 9, 4, 7, 3 ],
+            [ 3, 4, 9, 6, 7, 2, 8, 5, 1 ],
+            [ 8, 7, 1, 5, 3, 4, 9, 2, 6 ]
         ]);
     });
 
