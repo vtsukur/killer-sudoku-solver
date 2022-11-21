76038a77a74a2a86ed0167709813aa6f9fc1872b
Volodymyr Tsukur <volodymyrt@wix.com>
2022-11-21 21:05:01 +0200

    Further refactoring of Problem

--- src/problem/problem.js
+++ src/problem/problem.js
@@ -17,36 +17,50 @@ export class Problem {
         const cellCountMap = Problem.#cellCountMapFor(cells);
         if (cellCountMap.size === Grid.CELL_COUNT) return;
 
-        const missing = Problem.#findMissingCells(cellCountMap);
-        const hasMissing = missing.length > 0;
-        const duplicates = Problem.#findDuplicateCells(cellCountMap);
-        const hasDuplicates = duplicates.length > 0;
+        const missingCellKeys = Problem.#findMissingCellKeys(cellCountMap);
+        const hasMissingCells = missingCellKeys.length > 0;
+        const duplicateCellKeys = Problem.#findDuplicateCellKeys(cellCountMap);
+        const hasDuplicateCells = duplicateCellKeys.length > 0;
 
-        if (hasMissing || hasDuplicates) {
+        if (hasMissingCells || hasDuplicateCells) {
             let message = '';
-
-            if (hasMissing) {
-                message = `${missing.length} missing cell(s): ${missing.join(', ')}. `;
+            if (hasMissingCells) {
+                message = `${missingCellKeys.length} missing cell(s): ${missingCellKeys.join(', ')}. `;
             }
-            if (hasDuplicates) {
-                message = `${message}${duplicates.length} duplicate cell(s): ${duplicates.join(', ')}.`
+            if (hasDuplicateCells) {
+                message = `${message}${duplicateCellKeys.length} duplicate cell(s): ${duplicateCellKeys.join(', ')}.`
             }
-            Problem.#throwValidationError(message.trim().slice(0, -1));
+            // removing trailing whitespaces and last dot.
+            message = message.trim().slice(0, -1);
+            Problem.#throwValidationError(message);
         }
     }
 
     static #cellCountMapFor(cells) {
-        const cellMap = new Map();
+        const map = new Map();
         cells.forEach(cell => {
-            const value = cellMap.get(cell.key());
-            cellMap.set(cell.key(), value ? value + 1 : 1);
+            const count = map.get(cell.key());
+            map.set(cell.key(), count ? count + 1 : 1);
         });
-        return cellMap;
+        return map;
     }
 
-    static #findDuplicateCells(cellMap) {
+    static #findMissingCellKeys(cellCountMap) {
+        const missing = [];
+        _.range(Grid.SIDE_LENGTH).forEach(row => {
+            _.range(Grid.SIDE_LENGTH).forEach(col => {
+                const cellKey = new Cell(row, col).key();
+                if (!cellCountMap.has(cellKey)) {
+                    missing.push(cellKey);
+                }
+            });
+        });
+        return missing;
+    }
+
+    static #findDuplicateCellKeys(cellCountMap) {
         const duplicates = [];
-        for (const sameCellCountEntry of cellMap.entries()) {
+        for (const sameCellCountEntry of cellCountMap.entries()) {
             const cellKey = sameCellCountEntry[0];
             const sameCellCount = sameCellCountEntry[1];
             if (sameCellCount > 1) {
@@ -54,19 +68,6 @@ export class Problem {
             }
         }
         return duplicates;
-    }
-
-    static #findMissingCells(cellMap) {
-        const missing = [];
-        _.range(Grid.SIDE_LENGTH).forEach(row => {
-            _.range(Grid.SIDE_LENGTH).forEach(col => {
-                const cellKey = new Cell(row, col).key();
-                if (!cellMap.has(cellKey)) {
-                    missing.push(cellKey);
-                }
-            });
-        });
-        return missing;
     }
 
     static #checkCages(cages) {
