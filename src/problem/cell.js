714e593441eb92f272f85db8d7bcfee57aa69cd5
Volodymyr Tsukur <volodymyrt@wix.com>
2022-11-21 22:43:34 +0200

    Transforming Cell validators to convert to values

--- src/problem/cell.js
+++ src/problem/cell.js
@@ -4,30 +4,26 @@ export class Cell {
     #key;
 
     constructor(row, col) {
-        Cell.#validate(row, col);
-        this.row = row;
-        this.col = col;
+        this.row = Cell.#validateIndex('Row', row);
+        this.col = Cell.#validateIndex('Column', col);
         this.nonet = Math.floor(row / House.NONET_SIDE_LENGTH) * House.NONET_SIDE_LENGTH + Math.floor(col / House.NONET_SIDE_LENGTH);
         this.#key = Cell.keyOf(row, col);
-    }
-
-    static at(row, col) {
-        return new Cell(row, col);
-    }
-
-    static #validate(row, col) {
-        Cell.#validateIndex('Row', row);
-        Cell.#validateIndex('Column', col);
     }
 
     static #validateIndex(type, actualValue) {
         if (!Cell.#coordWithinRange(actualValue)) {
             throw `Invalid cell. ${type} outside of range. Expected to be within [0, ${House.SIZE}). Actual: ${actualValue}`;
+        } else {
+            return actualValue;
         }
     }
 
     static #coordWithinRange(i) {
         return i >= 0 && i < House.SIZE;
+    }
+
+    static at(row, col) {
+        return new Cell(row, col);
     }
 
     static keyOf(row, col) {
