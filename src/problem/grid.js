48f310c0a7180ecaccdc5b2fa8dadcb80dceaf53
Volodymyr Tsukur <volodymyrt@wix.com>
2022-11-21 20:54:19 +0200

    Adding tests for Grid, refactoring Grid and also adding new run test configuration

--- src/problem/grid.js
+++ src/problem/grid.js
@@ -6,14 +6,14 @@ export class Grid {
         return Grid.#SIDE_LENGTH;
     }
 
-    static #TOTAL_SUM = Grid.#SIDE_LENGTH * House.SUM;
-    static get TOTAL_SUM() {
-        return Grid.#TOTAL_SUM;
-    }
-
     static #CELL_COUNT = Grid.#SIDE_LENGTH * House.SIZE;
     static get CELL_COUNT() {
         return Grid.#CELL_COUNT;
+    }
+
+    static #TOTAL_SUM = Grid.#SIDE_LENGTH * House.SUM;
+    static get TOTAL_SUM() {
+        return Grid.#TOTAL_SUM;
     }
 
     constructor() {
---
+++ test/problem/grid.test.js
@@ -0,0 +1,19 @@
+import { Grid } from '../../src/problem/grid';
+
+describe('Grid tests', () => {
+    test('Grid is not construtable', () => {
+        expect(() => new Grid()).toThrow('Grid is not constructable');
+    });
+
+    test('Grid side length is 9', () => {
+        expect(Grid.SIDE_LENGTH).toEqual(9);
+    });
+
+    test('Grid cell count is 81', () => {
+        expect(Grid.CELL_COUNT).toEqual(81);
+    });
+
+    test('Sum of all cells in the grid is 405', () => {
+        expect(Grid.SIDE_LENGTH).toEqual(9);
+    });
+});
