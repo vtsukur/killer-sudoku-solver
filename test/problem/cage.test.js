86c0324ddf77ef9ea3998af423f33c75efe96da8
Volodymyr Tsukur <volodymyrt@wix.com>
2022-11-21 23:25:12 +0200

    Adding more tests for Cage to verify multiple cell duplications

--- test/problem/cage.test.js
+++ test/problem/cage.test.js
@@ -17,9 +17,15 @@ describe('Cage tests', () => {
         expect(cage.cells).toEqual([ Cell.at(4, 4), Cell.at(4, 5) ]);
     });
 
-    test('Construction of invalid cage with duplicate cell', () => {
+    test('Construction of invalid cage with a duplicate cell', () => {
         expect(() => Cage.ofSum(9).at(4, 4).at(4, 4).mk()).toThrow(
             'Invalid cage. 1 duplicate cell(s): (4, 4)'
+        );
+    });
+
+    test('Construction of invalid cage with 2 duplicate cells', () => {
+        expect(() => Cage.ofSum(9).at(0, 0).at(0, 1).at(0, 0).at(0, 1).mk()).toThrow(
+            'Invalid cage. 2 duplicate cell(s): (0, 0), (0, 1)'
         );
     });
 
