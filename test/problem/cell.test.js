4eee44a88ee861879ef873bc4afa33b5424f1042
Volodymyr Tsukur <volodymyrt@wix.com>
2022-11-22 11:54:41 +0200

    Moving Nonet index tests to Nonet tests

--- test/problem/cell.test.js
+++ test/problem/cell.test.js
@@ -2,10 +2,11 @@ import _ from 'lodash';
 import { Cell } from '../../src/problem/cell';
 
 describe('Cell tests', () => {
-    test('Construction of cell storing row and column', () => {
+    test('Construction of cell storing row, column and nonet', () => {
         const cell = Cell.at(4, 4);
         expect(cell.row).toBe(4);
         expect(cell.col).toBe(4);
+        expect(cell.nonet).toBe(4);
     });
 
     test('Construction of invalid cell with row outside of the range: <0', () => {
@@ -22,71 +23,6 @@ describe('Cell tests', () => {
 
     test('Construction of invalid cell with column outside of the range: >8', () => {
         expect(() => Cell.at(4, 9)).toThrow('Invalid cell. Column outside of range. Expected to be within [0, 9). Actual: 9');
-    });
-
-    test('Cells have correct nonet index', () => {
-        // north-west nonet (0)
-        _.range(0, 3).forEach(r => {
-            _.range(0, 3).forEach(c => {
-                expect(Cell.at(r, c).nonet).toBe(0);
-            })
-        })
-
-        // north nonet (1)
-        _.range(0, 3).forEach(r => {
-            _.range(3, 6).forEach(c => {
-                expect(Cell.at(r, c).nonet).toBe(1);
-            })
-        })
-
-        // north-east nonet (2)
-        _.range(0, 3).forEach(r => {
-            _.range(6, 9).forEach(c => {
-                expect(Cell.at(r, c).nonet).toBe(2);
-            })
-        })
-
-        // east nonet (3)
-        _.range(3, 6).forEach(r => {
-            _.range(0, 3).forEach(c => {
-                expect(Cell.at(r, c).nonet).toBe(3);
-            })
-        })
-
-        // center nonet (4)
-        _.range(3, 6).forEach(r => {
-            _.range(3, 6).forEach(c => {
-                expect(Cell.at(r, c).nonet).toBe(4);
-            })
-        })
-
-        // west nonet (5)
-        _.range(3, 6).forEach(r => {
-            _.range(6, 9).forEach(c => {
-                expect(Cell.at(r, c).nonet).toBe(5);
-            })
-        })
-
-        // south-west nonet (6)
-        _.range(6, 9).forEach(r => {
-            _.range(0, 3).forEach(c => {
-                expect(Cell.at(r, c).nonet).toBe(6);
-            })
-        })
-
-        // south nonet (7)
-        _.range(6, 9).forEach(r => {
-            _.range(3, 6).forEach(c => {
-                expect(Cell.at(r, c).nonet).toBe(7);
-            })
-        })
-
-        // south-east nonet (8)
-        _.range(6, 9).forEach(r => {
-            _.range(6, 9).forEach(c => {
-                expect(Cell.at(r, c).nonet).toBe(8);
-            })
-        })
     });
 
     test('Cell key', () => {
--- test/problem/nonet.test.js
+++ test/problem/nonet.test.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import { Nonet } from '../../src/problem/nonet';
 
 describe('Nonet tests', () => {
@@ -7,5 +8,70 @@ describe('Nonet tests', () => {
 
     test('None side length is 3', () => {
         expect(Nonet.SIDE_LENGTH).toEqual(3);
+    });
+
+    test('Computation of nonet index', () => {
+        // north-west nonet (0)
+        _.range(0, 3).forEach(row => {
+            _.range(0, 3).forEach(col => {
+                expect(Nonet.indexOf(row, col)).toBe(0);
+            })
+        })
+
+        // north nonet (1)
+        _.range(0, 3).forEach(row => {
+            _.range(3, 6).forEach(col => {
+                expect(Nonet.indexOf(row, col)).toBe(1);
+            })
+        })
+
+        // north-east nonet (2)
+        _.range(0, 3).forEach(row => {
+            _.range(6, 9).forEach(col => {
+                expect(Nonet.indexOf(row, col)).toBe(2);
+            })
+        })
+
+        // east nonet (3)
+        _.range(3, 6).forEach(row => {
+            _.range(0, 3).forEach(col => {
+                expect(Nonet.indexOf(row, col)).toBe(3);
+            })
+        })
+
+        // center nonet (4)
+        _.range(3, 6).forEach(row => {
+            _.range(3, 6).forEach(col => {
+                expect(Nonet.indexOf(row, col)).toBe(4);
+            })
+        })
+
+        // west nonet (5)
+        _.range(3, 6).forEach(row => {
+            _.range(6, 9).forEach(col => {
+                expect(Nonet.indexOf(row, col)).toBe(5);
+            })
+        })
+
+        // south-west nonet (6)
+        _.range(6, 9).forEach(row => {
+            _.range(0, 3).forEach(col => {
+                expect(Nonet.indexOf(row, col)).toBe(6);
+            })
+        })
+
+        // south nonet (7)
+        _.range(6, 9).forEach(row => {
+            _.range(3, 6).forEach(col => {
+                expect(Nonet.indexOf(row, col)).toBe(7);
+            })
+        })
+
+        // south-east nonet (8)
+        _.range(6, 9).forEach(row => {
+            _.range(6, 9).forEach(col => {
+                expect(Nonet.indexOf(row, col)).toBe(8);
+            })
+        })
     });
 });
