eac37ee1e52b97787de8497510f6b855d68c33a1
Volodymyr Tsukur <volodymyrt@wix.com>
2022-11-24 22:31:40 +0200

    Refactoring tests

--- test/solver/elements/cageModel.test.js
+++ test/solver/elements/cageModel.test.js
@@ -3,11 +3,13 @@ import { CellModel } from '../../../src/solver/models/elements/cellModel';
 import { Cage } from '../../../src/problem/cage';
 import { Cell } from '../../../src/problem/cell';
 
+const cell1 = Cell.at(0, 0);
+const cell2 = Cell.at(0, 1);
+const cell3 = Cell.at(0, 2);
+
 describe('CageModel tests', () => {
     test('Reducing CageModel of size 2 with a single combination', () => {
-        const cell1 = Cell.at(0, 0);
         const cellModel1 = new CellModel(cell1);
-        const cell2 = Cell.at(0, 1);
         const cellModel2 = new CellModel(cell2);
         const cage = Cage.ofSum(17).cell(cell1).cell(cell2).mk();
         const cageModel = new CageModel(cage, [ cellModel1, cellModel2 ]);
@@ -23,9 +25,7 @@ describe('CageModel tests', () => {
     });
 
     test('Reducing CageModel of size 2 with several combinations', () => {
-        const cell1 = Cell.at(0, 0);
         const cellModel1 = new CellModel(cell1);
-        const cell2 = Cell.at(0, 1);
         const cellModel2 = new CellModel(cell2);
         const cage = Cage.ofSum(13).cell(cell1).cell(cell2).mk();
         const cageModel = new CageModel(cage, [ cellModel1, cellModel2 ]);
@@ -43,11 +43,8 @@ describe('CageModel tests', () => {
     });
 
     test('Reducing CageModel of size 3 with a single combination', () => {
-        const cell1 = Cell.at(0, 0);
         const cellModel1 = new CellModel(cell1);
-        const cell2 = Cell.at(0, 1);
         const cellModel2 = new CellModel(cell2);
-        const cell3 = Cell.at(0, 2);
         const cellModel3 = new CellModel(cell3);
         const cage = Cage.ofSum(24).cell(cell1).cell(cell2).cell(cell3).mk();
         const cageModel = new CageModel(cage, [ cellModel1, cellModel2, cellModel3 ]);
