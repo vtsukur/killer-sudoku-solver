e1fb9f2a3f14dd101a43029e05815170c82d04c1
Volodymyr Tsukur <volodymyrt@wix.com>
2022-12-01 14:04:10 +0100

    Adding new problem to solve from dailykillersudoku.com

--- test/problem/realProblemSamples.js
+++ test/problem/realProblemSamples.js
@@ -449,3 +449,34 @@ export const sudokuDotCom_randomExpertLevelChallenge = new Problem([
     Cage.ofSum(13).at(7, 7).at(8, 7).mk(),
     Cage.ofSum(11).at(7, 8).at(8, 8).mk()
 ]);
+
+export const dailyKillerSudokuDotCom_puzzle24789_difficulty10 = new Problem([
+    // upper nonets
+    Cage.ofSum(8).at(0, 0).at(0, 1).at(1, 0).mk(),
+    Cage.ofSum(15).at(2, 0).at(3, 0).mk(),
+    Cage.ofSum(19).at(1, 1).at(1, 2).at(2, 1).mk(),
+    Cage.ofSum(12).at(0, 2).at(0, 3).mk(),
+    Cage.ofSum(35).at(2, 2).at(2, 3).at(3, 2).at(3, 3).at(3, 4).at(4, 3).mk(),
+    Cage.ofSum(31).at(0, 4).at(0, 5).at(0, 6).at(1, 3).at(1, 4).at(1, 5).at(1, 6).mk(),
+    Cage.ofSum(6).at(2, 4).at(2, 5).mk(),
+    Cage.ofSum(16).at(0, 7).at(1, 7).mk(),
+    Cage.ofSum(11).at(0, 8).at(1, 8).mk(),
+    Cage.ofSum(14).at(2, 6).at(2, 7).at(2, 8).at(3, 8).mk(),
+
+    // middle nonets
+    Cage.ofSum(33).at(3, 1).at(4, 0).at(4, 1).at(5, 0).at(5, 1).at(6, 0).at(6, 1).mk(),
+    Cage.ofSum(6).at(4, 2).at(5, 2).mk(),
+    Cage.ofSum(20).at(4, 4).at(4, 5).at(5, 4).at(5, 5).mk(),
+    Cage.ofSum(30).at(3, 5).at(3, 6).at(4, 6).at(5, 6).at(5, 7).mk(),
+    Cage.ofSum(7).at(3, 7).at(4, 7).mk(),
+    Cage.ofSum(19).at(4, 8).at(5, 8).at(6, 8).mk(),
+
+    // lower nonets
+    Cage.ofSum(10).at(7, 0).at(7, 1).mk(),
+    Cage.ofSum(13).at(8, 0).at(8, 1).mk(),
+    Cage.ofSum(13).at(6, 2).at(7, 2).at(8, 2).at(8, 3).mk(),
+    Cage.ofSum(24).at(5, 3).at(6, 3).at(6, 4).at(6, 5).at(7, 5).mk(),
+    Cage.ofSum(14).at(7, 3).at(7, 4).mk(),
+    Cage.ofSum(14).at(8, 4).at(8, 5).at(8, 6).mk(),
+    Cage.ofSum(35).at(6, 6).at(6, 7).at(7, 6).at(7, 7).at(7, 8).at(8, 7).at(8, 8).mk()
+]);
