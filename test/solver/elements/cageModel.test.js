7c0c4066e49248ee9b301c36a006d21276d25ff8
Volodymyr Tsukur <volodymyrt@wix.com>
2022-12-04 20:44:11 +0200

    Adding one more test for lareg CageModel initial reduction

--- test/solver/elements/cageModel.test.js
+++ test/solver/elements/cageModel.test.js
@@ -124,4 +124,38 @@ describe('CageModel tests', () => {
         // ]);
         // });
     });
+
+    test('Reduction for CageModel of size 7 and sum 31 with 2 combinations', () => {
+        const cell_04 = Cell.at(0, 4);
+        const cell_05 = Cell.at(0, 5);
+        const cell_06 = Cell.at(0, 6);
+        const cell_13 = Cell.at(1, 3);
+        const cell_14 = Cell.at(1, 4);
+        const cell_15 = Cell.at(1, 5);
+        const cell_16 = Cell.at(1, 6);
+
+        const cellModel_04 = new CellModel(cell_04);
+        const cellModel_05 = new CellModel(cell_05);
+        const cellModel_06 = new CellModel(cell_06);
+        const cellModel_13 = new CellModel(cell_13);
+        const cellModel_14 = new CellModel(cell_14);
+        const cellModel_15 = new CellModel(cell_15);
+        const cellModel_16 = new CellModel(cell_16);
+        const cellModels = [ cellModel_04, cellModel_05, cellModel_06,
+            cellModel_13, cellModel_14, cellModel_15, cellModel_16 ];
+
+        const cage = Cage.ofSum(31)
+            .cell(cell_04).cell(cell_05).cell(cell_06)
+            .cell(cell_13).cell(cell_14).cell(cell_15).cell(cell_16)
+            .mk();
+        const cageModel = new CageModel(cage, cellModels, false);
+
+        cageModel.initialReduce();
+
+        const initialCombos = Array.from(cageModel.combos);
+        expect(initialCombos).toEqual([
+            [ 1, 2, 3, 4, 5, 7, 9 ],
+            [ 1, 2, 3, 4, 6, 7, 8 ]
+        ]);
+    });
 });
