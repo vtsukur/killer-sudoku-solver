4028f4b0a76ee0c1775c6f6aeeba58e96137db00
Volodymyr Tsukur <volodymyrt@wix.com>
2022-12-19 21:14:05 +0200

    Stub for finding redundant house sums strategy

--- src/solver/strategies/masterStrategy.js
+++ src/solver/strategies/masterStrategy.js
@@ -1,11 +1,13 @@
 import { findAndReduceCagePermsByHouseStrategy } from './tactics/findAndReduceCagePermsByHouseStrategy';
 import { findAndSliceResidualSumsStrategy } from './tactics/findAndSliceResidualSumsStrategy';
+import { findRedundantNonetSumsStrategy } from './tactics/findRedundantNonetSumsStrategy';
 import { initPermsForCagesStrategy } from './tactics/initPermsForCagesStrategy';
 import { placeNumsForSingleOptionCellsStrategy } from './tactics/placeNumsForSingleOptionCellsStrategy';
 import { reducePermsInCagesStrategy } from './tactics/reducePermsInCagesStrategy';
 import { reflectSolvedCellsStrategy } from './tactics/reflectSolvedCellsStrategy';
 
 export function masterStrategy() {
+    this.run(findRedundantNonetSumsStrategy);
     this.run(findAndSliceResidualSumsStrategy);
     this.run(initPermsForCagesStrategy);
 
