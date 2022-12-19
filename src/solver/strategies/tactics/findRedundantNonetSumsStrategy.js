b88c705c31746307f614e1f1a7549d55cbe09491
Volodymyr Tsukur <volodymyrt@wix.com>
2022-12-19 22:25:55 +0200

    Adding cages with redundant nonet cells for extra reduction

--- src/solver/strategies/tactics/findRedundantNonetSumsStrategy.js
+++ src/solver/strategies/tactics/findRedundantNonetSumsStrategy.js
@@ -1,3 +1,41 @@
-export function findRedundantNonetSumsStrategy() {
+import { Cage } from '../../../problem/cage';
+import { House } from '../../../problem/house';
+import { CageModel } from '../../models/elements/cageModel';
 
+export function findRedundantNonetSumsStrategy() {
+    const nonetCageMsMap = new Map();
+    this.model.nonetModels.forEach(nonetM => {
+        nonetCageMsMap.set(nonetM.idx, new Set());
+    });
+
+    for (const cageM of this.model.cageModelsMap.values()) {
+        for (const cellM of cageM.cellModels) {
+            nonetCageMsMap.get(cellM.cell.nonet).add(cageM);
+        }
+    }
+
+    for (const entry of nonetCageMsMap.entries()) {
+        const idx = entry[0];
+        const cageMs = entry[1];
+
+        const redundantCells = [];
+        let cagesSum = 0;
+        for (const cageM of cageMs) {
+            for (const cellM of cageM.cellModels) {
+                const cell = cellM.cell;
+                if (cell.nonet !== idx) {
+                    redundantCells.push(cell);
+                }
+            }
+            cagesSum += cageM.cage.sum;
+        }
+
+        if (redundantCells.length > 0 && redundantCells.length <= 5) {
+            const cageBuilder = Cage.ofSum(cagesSum - House.SUM);
+            redundantCells.forEach(cell => cageBuilder.cell(cell));
+            const cage = cageBuilder.mk();
+
+            this.model.registerCage(cage, !CageModel.positioningFlagsFor(cage.cells).isWithinHouse);
+        }
+    }
 }
