20cac52756e951fc27b8f2d605db656142b77a06
Volodymyr Tsukur <volodymyrt@wix.com>
2022-12-23 11:57:55 +0200

    Advancing new strategy to determine single innie or outie cells

--- src/solver/strategies/tactics/findNonetBasedFormulasStrategy.js
+++ src/solver/strategies/tactics/findNonetBasedFormulasStrategy.js
@@ -5,12 +5,23 @@ import { House } from '../../../problem/house';
 let i = 0;
 export function findNonetBasedFormulasStrategy() {
     ++i;
-    
+
     _.range(0, House.SIZE).forEach(idx => {
         const nonetM = this.model.nonetModels[idx];
         const area = findAreaWithSingleInnieOrOutieCell(nonetM, this.model);
-        if (!_.isUndefined(area)) {
+        if (!_.isUndefined(area) && area.outerCageMs.size > 0) {
+            const outerCageMs = new Set(area.outerCageMs);
+            for (const outerCageM of outerCageMs) {
+                area.removeCageM(outerCageM);
+                const unfilledInnerCellMs = area.unfilledInnerCellMs(this.model);
+                if (unfilledInnerCellMs.size === 1 && area.outerCellMs.size <= 3) {
 
+                }
+                if (area.outerCellMs.size === 1 && unfilledInnerCellMs.size <= 3) {
+
+                }
+                area.addCageM(outerCageM);
+            }
         }
     });
 }
@@ -22,6 +33,7 @@ class ExpandableNonOverlappingNonetAreaModel {
     #cellKeys;
     #innerCellMs;
     #outerCellMs;
+    #outerCageMs;
 
     constructor(nonetM) {
         this.#nonetM = nonetM;
@@ -30,6 +42,7 @@ class ExpandableNonOverlappingNonetAreaModel {
         this.#cellKeys = new Set();
         this.#innerCellMs = new Set();
         this.#outerCellMs = new Set();
+        this.#outerCageMs = new Set();
     }
 
     addCageM(cageM) {
@@ -41,7 +54,19 @@ class ExpandableNonOverlappingNonetAreaModel {
                 this.#innerCellMs.add(cellM);
             } else {
                 this.#outerCellMs.add(cellM);
+                this.#outerCageMs.add(cageM);
             }
+        });
+    }
+
+    removeCageM(cageM) {
+        this.#cageMs.delete(cageM);
+        this.#outerCageMs.delete(cageM);
+        cageM.cellModels.forEach(cellM => {
+            this.#cellMs.delete(cellM);
+            this.#cellKeys.delete(cellM.cell.key);
+            this.#innerCellMs.delete(cellM);
+            this.#outerCellMs.delete(cellM);
         });
     }
 
@@ -53,8 +78,22 @@ class ExpandableNonOverlappingNonetAreaModel {
         return this.#innerCellMs;
     }
 
+    unfilledInnerCellMs(model) {
+        const result = new Set();
+        for (const { row, col } of this.#nonetM.cellIterator()) {
+            if (!this.#cellKeys.has(Cell.keyOf(row, col))) {
+                result.add(model.cellModelAt(row, col));
+            }
+        }
+        return result;
+    }
+
     get outerCellMs() {
         return this.#outerCellMs;
+    }
+
+    get outerCageMs() {
+        return this.#outerCageMs;
     }
 }
 
