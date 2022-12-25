b10697fbcccef1308ce3457e0981981dffda751b
Volodymyr Tsukur <volodymyrt@wix.com>
2022-12-25 21:02:00 +0200

    Adding stub for strategy to try options

--- src/solver/strategies/tactics/deepTryOptionsStrategy.js
+++ src/solver/strategies/tactics/deepTryOptionsStrategy.js
@@ -1,5 +1,45 @@
+import _ from 'lodash';
+import { House } from '../../../problem/house';
+import { masterStrategy } from '../masterStrategy';
+
 export function deepTryOptionsStrategy() {
     if (this.hasCageModelsToReevaluatePerms || this.model.solved) return;
 
-    
+    const cellMTargets = findCellMTargets(this.model);
+    if (cellMTargets.length === 0) return;
+
+    const cellMTarget = cellMTargets[0];
+
+    for (const tryNum of cellMTarget.numOpts()) {
+        const ctxCpy = this.deepCopy();
+        const cellMTargetCpy = ctxCpy.model.cellModelAt(cellMTarget.cell.row, cellMTarget.cell.col);
+        cellMTargetCpy.placeNum(tryNum);
+
+        try {
+            ctxCpy.run(masterStrategy);
+        } catch {
+            continue;
+        }
+
+        if (ctxCpy.model.solved) {
+            cellMTarget.placeNum(tryNum);
+            this.cageModelsToReevaluatePerms = cellMTarget.withinCageModels;
+            break;
+        }
+    }
+}
+
+function findCellMTargets(model) {
+    const result = [];
+
+    _.range(House.SIZE).forEach(row => {
+        _.range(House.SIZE).forEach(col => {
+            const cellM = model.cellModelAt(row, col);
+            if (cellM.numOpts().size === 2) {
+                result.push(cellM);
+            }
+        });
+    });
+
+    return result;
 }
