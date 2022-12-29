c5f0ae26ee15b88801f44edee530ef7deeadb15e
Volodymyr Tsukur <volodymyrt@wix.com>
2022-12-29 14:07:22 +0200

    Recognizing dotted lines in problem

--- test/reader/imageReader.test.js
+++ test/reader/imageReader.test.js
@@ -1,33 +1,30 @@
 import Jimp from 'jimp';
 import cv from '@techstark/opencv-js';
+import _ from 'lodash';
 
 async function loadImage() {
     var jimpSrc = await Jimp.read('./test/reader/problems/dailyKillerSudokuDotCom_24919.png');
     var src = cv.matFromImageData(jimpSrc.bitmap);
 
-  // following lines is copy&paste of opencv.js dilate tutorial:
-//   let dst = new cv.Mat();
-//   let M = cv.Mat.ones(5, 5, cv.CV_8U);
-//   let anchor = new cv.Point(-1, -1);
-//   cv.dilate(src, dst, M, anchor, 1, cv.BORDER_CONSTANT, cv.morphologyDefaultBorderValue());
     cv.cvtColor(src, src, cv.COLOR_RGBA2GRAY, 0);
     cv.Canny(src, src, 50, 200, 3);
-    let lines = new cv.Mat();
-    cv.HoughLines(src, lines, 1, Math.PI / 180,
-              30, 0, 0, 0, Math.PI);
 
     let dst = cv.Mat.zeros(src.rows, src.cols, cv.CV_8UC3);
-    cv.HoughLinesP(src, lines, 1, Math.PI / 180, 2, 0, 0);
-    // draw lines
-    let color = new cv.Scalar(255, 0, 0);
-    for (let i = 0; i < lines.rows; ++i) {
-        let startPoint = new cv.Point(lines.data32S[i * 4], lines.data32S[i * 4 + 1]);
-        let endPoint = new cv.Point(lines.data32S[i * 4 + 2], lines.data32S[i * 4 + 3]);
-        cv.line(dst, startPoint, endPoint, color);
-    }
-  // Now that we are finish, we want to write `dst` to file `output.png`. For this we create a `Jimp`
-  // image which accepts the image data as a [`Buffer`](https://nodejs.org/docs/latest-v10.x/api/buffer.html).
-  // `write('output.png')` will write it to disk and Jimp infers the output format from given file name:
+    let contours = new cv.MatVector();
+    let h = new cv.Mat();
+    cv.findContours(src, contours, h, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE);
+    let color = new cv.Scalar(0, 255, 0);
+    // cv.drawContours(dst, contours, -1, color, 2);
+    
+    _.range(contours.size()).forEach(i => {
+        const contour = contours.get(i);
+        const boundingRect = cv.boundingRect(contour);
+        if (boundingRect.width < 15 && boundingRect.height < 15) {
+            const topLeftPoint = new cv.Point(boundingRect.x, boundingRect.y);
+            const bottomRightPoint = new cv.Point(boundingRect.x + boundingRect.width, boundingRect.y + boundingRect.height);
+            cv.rectangle(dst, topLeftPoint, bottomRightPoint, color, 5);
+        }
+    });
 
     new Jimp({
         width: dst.cols,
