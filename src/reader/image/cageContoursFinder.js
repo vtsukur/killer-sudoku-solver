7808885336d21a4dd79b99dd4409c173e5659f95
Volodymyr Tsukur <volodymyrt@wix.com>
2023-01-02 13:54:50 +0200

    Drawing cell contours

--- src/reader/image/cageContoursFinder.js
+++ src/reader/image/cageContoursFinder.js
@@ -1,14 +1,17 @@
 import cv from '@techstark/opencv-js';
 import Jimp from 'jimp';
 import _ from 'lodash';
+import { House } from '../../problem/house';
 import { Rect } from './rect';
 
 const CAGE_BOUNDARY_DOT_MAX_SIZE = 15;
 const CANNY_THRESHOLD_MIN = 20;
 const CANNY_THRESHOLD_MAX = 100;
+const GRID_CONTOUR_ADJUSTMENT = 3;
 const TMP_CAGE_CONTOURS_DUMP_PATH = './tmp/cageContours.png';
 const TMP_CAGE_CONTOUR_COLOR = new cv.Scalar(0, 255, 0);
-const TMP_CAGE_CONTOUR_THICKNESS = 2;
+const TMP_CELL_CONTOUR_COLOR = new cv.Scalar(255, 0, 0);
+const TMP_CONTOUR_THICKNESS = 2;
 
 export async function findCageContours(imagePath) {
     // read image using Jimp.
@@ -18,15 +21,17 @@ export async function findCageContours(imagePath) {
     const src = cv.matFromImageData(jimpSrc.bitmap);
     prepareSourceMat(src);
 
-    // find cage contours and dump results
+    // find cage contours
     const contoursMatVector = new cv.MatVector();
     const allCageContours = findAllCageContours(src, contoursMatVector);
-    dumpTmpCageContoursOutput(src, allCageContours, TMP_CAGE_CONTOURS_DUMP_PATH);
 
     // find grid contour
-    const gridContour = findGridContourFromCageContours(allCageContours, src.rows, src.cols);
-    console.log(`Grid contour: (${gridContour.x} + ${gridContour.x + gridContour.width}, (${gridContour.y} + ${gridContour.y + gridContour.height})`);
+    const gridContour = findGridContourFromCageContours(allCageContours);
+    console.log(`Grid contour: (${gridContour.x} + ${gridContour.width}, ${gridContour.y} + ${gridContour.height})`);
     console.log(`Total size: ${src.rows} x ${src.cols}`);
+
+    // dump temporary processing result
+    dumpTmpCageContoursOutput(src, gridContour, allCageContours, TMP_CAGE_CONTOURS_DUMP_PATH);
 
     // cleanup
     contoursMatVector.delete();
@@ -64,7 +69,7 @@ function findAllCageContours(src, contoursMatVector) {
     return allCageContours;
 }
 
-function findGridContourFromCageContours(allCageContours, totalWidth, totalHeight) {
+function findGridContourFromCageContours(allCageContours) {
     const leftXMap = new Map();
     const rightXMap = new Map();
     const topYMap = new Map();
@@ -83,7 +88,8 @@ function findGridContourFromCageContours(allCageContours, totalWidth, totalHeigh
     const minY = findFirstSignificantCoord(topYMap);
     const maxY = findFirstSignificantCoord(bottomYMap, true);
 
-    return new Rect(minX, minY, maxX - minX, maxY - minY);
+    return new Rect(minX - GRID_CONTOUR_ADJUSTMENT, minY - GRID_CONTOUR_ADJUSTMENT,
+        maxX - minX + 2 * GRID_CONTOUR_ADJUSTMENT, maxY - minY + 2 * GRID_CONTOUR_ADJUSTMENT);
 }
 
 function accumCoordEntry(map, coord) {
@@ -102,15 +108,30 @@ function findFirstSignificantCoord(map, isReverse) {
     return significant ? significant : arr[isReverse ? arr.length - 1 : 0];
 }
 
-function dumpTmpCageContoursOutput(src, cageContours, outputPath) {
+function dumpTmpCageContoursOutput(src, gridContour, cageContours, outputPath) {
     const mat = cv.Mat.zeros(src.rows, src.cols, cv.CV_8UC3);
 
     for (const cageContour of cageContours) {
         const cvRect = cv.boundingRect(cageContour);
         const topLeft = new cv.Point(cvRect.x, cvRect.y);
         const bottomRight = new cv.Point(cvRect.x + cvRect.width, cvRect.y + cvRect.height);
-        cv.rectangle(mat, topLeft, bottomRight, TMP_CAGE_CONTOUR_COLOR, TMP_CAGE_CONTOUR_THICKNESS);
+        cv.rectangle(mat, topLeft, bottomRight, TMP_CAGE_CONTOUR_COLOR, TMP_CONTOUR_THICKNESS);
     }
+
+    const cellWidth = gridContour.width / House.SIZE;
+    const cellHeight = gridContour.height / House.SIZE;
+    _.range(House.SIZE + 1).forEach(i => {
+        cv.line(mat,
+            new cv.Point(gridContour.x, gridContour.y + i * cellHeight),
+            new cv.Point(gridContour.x + gridContour.width, gridContour.y + i * cellHeight),
+            TMP_CELL_CONTOUR_COLOR,
+            TMP_CONTOUR_THICKNESS);
+        cv.line(mat,
+            new cv.Point(gridContour.x + i * cellWidth, gridContour.y),
+            new cv.Point(gridContour.x + i * cellWidth, gridContour.y + gridContour.height),
+            TMP_CELL_CONTOUR_COLOR,
+            TMP_CONTOUR_THICKNESS);
+    });
 
     new Jimp({
         width: mat.cols,
