4940fc7fbb2bef2eca996c38e172f1caef638f7d
Volodymyr Tsukur <volodymyrt@wix.com>
2023-01-07 12:53:03 +0200

    Fixing test for cage contours finder

--- test/reader/image/cageContoursFinder.test.js
+++ test/reader/image/cageContoursFinder.test.js
@@ -2,7 +2,7 @@ import { findCageContours } from '../../../src/reader/image/cageContoursFinder';
 
 describe('Cage contours finder tests', () => {
     test('Read problem from image', async () => {
-        const contours = await findCageContours('./test/reader/image/samples/dailyKillerSudokuDotCom_24954.png');
-        expect(contours.length).toBeGreaterThan(0);
+        const problem = await findCageContours('./test/reader/image/samples/dailyKillerSudokuDotCom_24954.png');
+        expect(problem.cages.length).toBe(24);
     });
 });
