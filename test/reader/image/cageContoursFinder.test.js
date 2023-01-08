cb934f0863dbd922ef7442c9cf80edde8614d18e
Volodymyr Tsukur <volodymyrt@wix.com>
2023-01-08 14:07:40 +0200

    Adding 2 explicit tests for image-to-problem transformer

--- test/reader/image/cageContoursFinder.test.js
+++ test/reader/image/cageContoursFinder.test.js
@@ -1,7 +1,12 @@
 import { findCageContours } from '../../../src/reader/image/cageContoursFinder';
 
 describe('Cage contours finder tests', () => {
-    test('Read problem from image', async () => {
+    test('Read problem 24919 from image', async () => {
+        const problem = await findCageContours('./test/reader/image/samples/dailyKillerSudokuDotCom_24919.png');
+        expect(problem.cages.length).toBe(26);
+    });
+
+    test('Read problem 24954 from image', async () => {
         const problem = await findCageContours('./test/reader/image/samples/dailyKillerSudokuDotCom_24954.png');
         expect(problem.cages.length).toBe(24);
     });
