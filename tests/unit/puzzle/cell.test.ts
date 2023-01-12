e22ddcffe3c490498af9bba437c5f88a316d76c1
Volodymyr Tsukur <volodymyrt@wix.com>
2023-01-12 21:23:20 +0200

    Converting `Cell` unit tests to TypeScript

--- tests/unit/puzzle/cell.test.ts
+++ tests/unit/puzzle/cell.test.ts
@@ -1,12 +1,14 @@
-import _ from 'lodash';
+import * as _ from 'lodash';
 import { Cell } from '../../../src/puzzle/cell';
 
 describe('Cell tests', () => {
-    test('Construction of cell storing row, column and nonet', () => {
-        const cell = Cell.at(4, 4);
+    test('Construction of cell storing row, column and computing nonet, key and toString representation', () => {
+        const cell = Cell.at(4, 5);
         expect(cell.row).toBe(4);
-        expect(cell.col).toBe(4);
+        expect(cell.col).toBe(5);
         expect(cell.nonet).toBe(4);
+        expect(cell.key).toBe('(4, 5)');
+        expect(cell.toString()).toBe('(4, 5)');
     });
 
     test('Construction of invalid cell with row outside of the range: <0', () => {
@@ -23,13 +25,5 @@ describe('Cell tests', () => {
 
     test('Construction of invalid cell with column outside of the range: >8', () => {
         expect(() => Cell.at(4, 9)).toThrow('Invalid cell. Column outside of range. Expected to be within [0, 9). Actual: 9');
-    });
-
-    test('Cell key', () => {
-        expect(Cell.at(4, 5).key).toBe('(4, 5)');
-    });
-
-    test('Cell toString', () => {
-        expect(Cell.at(4, 5).toString()).toBe('(4, 5)');
     });
 });
