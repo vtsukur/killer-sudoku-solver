dae5e9a9765235958ae05e6bc45d12deb2332def
Volodymyr Tsukur <volodymyrt@wix.com>
2023-01-12 21:19:50 +0200

    Converting `Cell` to TypeScript

---
+++ src/puzzle/cell.ts
@@ -0,0 +1,58 @@
+import * as _ from 'lodash';
+import { House } from './house';
+import { Nonet } from './nonet';
+
+export class Cell {
+    readonly row: number;
+    readonly col: number;
+    readonly key: string;
+    readonly nonet: number;
+
+    constructor(row: number, col: number) {
+        this.row = Cell.validateIndex('Row', row);
+        this.col = Cell.validateIndex('Column', col);
+        this.key = Cell.keyOf(row, col);
+        this.nonet = Nonet.indexOf(row, col);
+    }
+
+    private static validateIndex(type: string, index: number) {
+        if (!Cell.indexWithinRange(index)) {
+            throw `Invalid cell. ${type} outside of range. Expected to be within [0, ${House.SIZE}). Actual: ${index}`;
+        } else {
+            return index;
+        }
+    }
+
+    private static indexWithinRange(index: number) {
+        return _.inRange(index, 0, House.SIZE);
+    }
+
+    static at(row: number, col: number) {
+        return new Cell(row, col);
+    }
+
+    static keyOf(row: number, col: number) {
+        return `(${row}, ${col})`
+    }
+
+    get absIdx() {
+        return this.row * House.SIZE + this.col;
+    }
+
+    toString() {
+        return this.key;
+    }
+
+    static setAndDuplicateKeysOf = (cells: Cell[]) => {
+        const set = new Set<string>();
+        const duplicateKeys = new Array<string>();
+        cells.forEach(cell => {
+            if (set.has(cell.key)) {
+                duplicateKeys.push(cell.key);
+            } else {
+                set.add(cell.key);
+            }
+        });
+        return { set, duplicateKeys };
+    }
+}
