ede4fb56ce59e656eb64251e3446314f7eb1fbc8
Volodymyr Tsukur <volodymyrt@wix.com>
2023-01-12 15:29:22 +0200

    Adding unit test for `TempFilePaths`

---
+++ tests/unit/util/tempFilePaths.test.ts
@@ -0,0 +1,16 @@
+import * as fs from 'node:fs';
+import { TempFilePaths } from '../../../src/util/tempFilePaths';
+
+describe('TempFilePaths tests', () => {
+    const baseDir = './tmp/tempFilePaths-test';
+    const paths = new TempFilePaths(baseDir);
+
+    test('Determine file path taking base dir into account', async () => {
+        expect(paths.filePath('file.tmp')).toBe('./tmp/tempFilePaths-test/file.tmp');
+    });
+
+    test('Recreate base dir', async () => {
+        paths.recreateDir();
+        expect(fs.existsSync(baseDir)).toBeTruthy();
+    });
+});
