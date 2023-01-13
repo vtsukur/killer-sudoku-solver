6a07f5300def396bbac04d6d3997c281ea6974e7
Volodymyr Tsukur <volodymyrt@wix.com>
2023-01-13 11:41:23 +0200

    Converting `Puzzle` to TypeScript

--- src/puzzle/puzzle.ts
+++ src/puzzle/puzzle.ts
@@ -1,36 +1,37 @@
-import _ from 'lodash';
+import * as _ from 'lodash';
 import { joinForReadability } from '../util/readableMessages';
+import { Cage } from './cage';
 import { Cell } from './cell';
 import { Grid } from './grid';
 
 export class Puzzle {
-    #cages;
+    readonly cages: Array<Cage>;
 
-    constructor(cages) {
-        this.#cages = [...Puzzle.#validate(cages)];
+    constructor(cages: Array<Cage>) {
+        this.cages = [...Puzzle.validate(cages)];
     }
 
-    static #validate(cages) {
-        Puzzle.#validateCageCells(cages.flatMap(cage => cage.cells));
-        Puzzle.#validateCages(cages);
+    private static validate(cages: Array<Cage>) {
+        Puzzle.validateCageCells(cages.flatMap(cage => cage.cells));
+        Puzzle.validateCages(cages);
         return cages;
     }
 
-    static #validateCageCells(cells) {
+    private static validateCageCells(cells: Array<Cell>) {
         const { set, duplicateKeys } = Cell.setAndDuplicateKeysOf(cells);
         if (set.size === Grid.CELL_COUNT) return; // cellSet size cannot be >Grid.CELL_COUNT since Cell and Cage construction control that
 
-        const missingCellKeys = Puzzle.#findMissingCellKeys(set);
+        const missingCellKeys = Puzzle.findMissingCellKeys(set);
 
         let message = `${missingCellKeys.length} missing cell(s): ${joinForReadability(missingCellKeys)}`;
         if (duplicateKeys.length > 0) {
             message = `${message}. ${duplicateKeys.length} duplicate cell(s): ${joinForReadability(duplicateKeys)}`;
         }
-        Puzzle.#throwValidationError(message);
+        Puzzle.throwValidationError(message);
     }
 
-    static #findMissingCellKeys(cellSet) {
-        const missing = [];
+    private static findMissingCellKeys(cellSet: Set<string>) {
+        const missing = new Array<string>();
         _.range(Grid.SIDE_LENGTH).forEach(row => {
             _.range(Grid.SIDE_LENGTH).forEach(col => {
                 const cellKey = Cell.keyOf(row, col);
@@ -42,18 +43,14 @@ export class Puzzle {
         return missing;
     }
 
-    static #validateCages(cages) {
+    private static validateCages(cages: Array<Cage>) {
         const actualGridSum = _.sum(cages.map(cage => cage.sum));
         if (actualGridSum !== Grid.TOTAL_SUM) {
-            this.#throwValidationError(`Expected sum of all cages to be ${Grid.TOTAL_SUM}. Actual: ${actualGridSum}`);
+            this.throwValidationError(`Expected sum of all cages to be ${Grid.TOTAL_SUM}. Actual: ${actualGridSum}`);
         }
     }
 
-    static #throwValidationError(detailedMessage) {
+    static throwValidationError(detailedMessage: string) {
         throw `Invalid puzzle. ${detailedMessage}`;
-    }
-
-    get cages() {
-        return this.#cages;
     }
 }
