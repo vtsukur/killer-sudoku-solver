3563e9d9ef696d2ec709cad41aa9434b24335249
Volodymyr Tsukur <volodymyrt@wix.com>
2023-01-14 13:19:13 +0200

    Migrating namespace to class according to ESLint check

--- src/puzzle/grid.ts
+++ src/puzzle/grid.ts
@@ -1,52 +1,51 @@
 import { Cell } from './cell';
 import { House } from './house';
 
-export namespace Grid {
-    export const SIDE_LENGTH = 9;
-    export const CELL_COUNT = Grid.SIDE_LENGTH * Grid.SIDE_LENGTH;
-    export const TOTAL_SUM = Grid.SIDE_LENGTH * House.SUM;
+export class Grid {
+    static readonly SIDE_LENGTH = 9;
+    static readonly CELL_COUNT = Grid.SIDE_LENGTH * Grid.SIDE_LENGTH;
+    static readonly TOTAL_SUM = Grid.SIDE_LENGTH * House.SUM;
 
-    export function rowFromAbs(idx: number) {
+    static rowFromAbs(idx: number) {
         return Math.floor(idx / House.SIZE);
     }
 
-    export function colFromAbs(idx: number) {
+    static colFromAbs(idx: number) {
         return idx % House.SIZE;
     }
 
-    export function indexWithinGrid(row: number, col: number) {
+    static indexWithinGrid(row: number, col: number) {
         return row * Grid.SIDE_LENGTH + col;
     }
 
-    export function newMatrix() {
+    static newMatrix() {
         return new Array(House.SIZE).fill(undefined).map(() => new Array(House.SIZE));
     }
 
-    export function cellsIterator(): CellsIterator {
-        let i = 0;
+    static cellsIterator(): CellsIterator {
         return new CellsIterator();
     }
+}
 
-    class CellsIterator implements Iterator<Cell> {
-        private i: number = 0;
+class CellsIterator implements Iterator<Cell> {
+    private i = 0;
 
-        [Symbol.iterator](): Iterator<Cell> {
-            return this;
-        }
+    [Symbol.iterator](): Iterator<Cell> {
+        return this;
+    }
 
-        next(): IteratorResult<Cell> {
-            const abs = this.i++;
-            if (abs < Grid.CELL_COUNT) {
-                return {
-                    value: Cell.at(
-                        Grid.rowFromAbs(abs),
-                        Grid.colFromAbs(abs),
-                    ),
-                    done: false
-                };
-            } else {
-                return { value: undefined, done: true };
-            }
+    next(): IteratorResult<Cell> {
+        const abs = this.i++;
+        if (abs < Grid.CELL_COUNT) {
+            return {
+                value: Cell.at(
+                    Grid.rowFromAbs(abs),
+                    Grid.colFromAbs(abs),
+                ),
+                done: false
+            };
+        } else {
+            return { value: undefined, done: true };
         }
     }
 }
