9fb8e0501678bf763e30ef0bd26c8ff47827f654
Volodymyr Tsukur <volodymyrt@wix.com>
2023-01-14 13:21:12 +0200

    Adding private constructor to `Nonet` as it is not supposed to be instantiated

--- src/puzzle/nonet.ts
+++ src/puzzle/nonet.ts
@@ -1,6 +1,10 @@
 export class Nonet {
     static SIDE_LENGTH = 3;
 
+    private constructor() {
+        //
+    }
+
     static indexOf(row: number, col: number) {
         return Math.floor(row / Nonet.SIDE_LENGTH) * Nonet.SIDE_LENGTH + Math.floor(col / Nonet.SIDE_LENGTH);
     }
