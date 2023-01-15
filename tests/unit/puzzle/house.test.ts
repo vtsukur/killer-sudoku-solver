28f2194e82c547f5a347e6cfcc65491a9288a398
Volodymyr Tsukur <volodymyrt@wix.com>
2023-01-15 21:24:22 +0200

    Added one more unit test for `House`

--- tests/unit/puzzle/house.test.ts
+++ tests/unit/puzzle/house.test.ts
@@ -1,6 +1,10 @@
 import { House } from '../../../src/puzzle/house';
 
 describe('House tests', () => {
+    test('There are 9 Houses of one type per Grid', () => {
+        expect(House.COUNT_OF_ONE_TYPE).toEqual(9);
+    });
+
     test('House size is 9', () => {
         expect(House.SIZE).toEqual(9);
     });
