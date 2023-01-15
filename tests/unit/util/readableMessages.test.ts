b026670ee5e836b979353d3ffb8d1834e8d0cd66
Volodymyr Tsukur <volodymyrt@wix.com>
2023-01-15 23:18:50 +0200

    Removing async function from the tests which do not require it

--- tests/unit/util/readableMessages.test.ts
+++ tests/unit/util/readableMessages.test.ts
@@ -1,11 +1,11 @@
 import { joinArray, joinSet } from '../../../src/util/readableMessages';
 
 describe('Readable messages util tests', () => {
-    test('Join array', async () => {
+    test('Join array', () => {
         expect(joinArray(['a', 1, true])).toBe('a, 1, true');
     });
 
-    test('Join set', async () => {
+    test('Join set', () => {
         expect(joinSet(new Set(['a', 1, true]))).toBe('a, 1, true');
     });
 });
