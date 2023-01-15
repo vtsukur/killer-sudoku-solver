850c23fc0ef95c0dc34ddac9a3c99d50e28224ca
Volodymyr Tsukur <volodymyrt@wix.com>
2023-01-15 12:28:52 +0200

    Refactoring `Nonet`

--- src/puzzle/nonet.ts
+++ src/puzzle/nonet.ts
@@ -1,7 +1,9 @@
-export const Nonet = Object.freeze({
-    SIDE_LENGTH: 3,
+class NonetAPI {
+    readonly SIDE_LENGTH = 3;
 
     indexForCellAt(row: number, col: number) {
         return Math.floor(row / Nonet.SIDE_LENGTH) * Nonet.SIDE_LENGTH + Math.floor(col / Nonet.SIDE_LENGTH);
     }
-});
+}
+
+export const Nonet = new NonetAPI();
