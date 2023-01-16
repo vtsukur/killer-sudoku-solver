823e5b7e70f38b44a0c9415fe05cbd1334652cec
Volodymyr Tsukur <volodymyrt@wix.com>
2023-01-16 13:28:40 +0200

    Fixing error in the use of dimensional matrix math

--- src/puzzle/nonet.ts
+++ src/puzzle/nonet.ts
@@ -2,26 +2,28 @@ import { to1D, rowFrom1D, colFrom1D } from '../util/dimensionalMatrixMath';
 import { Cell } from './cell';
 import { House } from './house';
 
+const NONET_SIDE_LENGTH = 3;
+
 class NonetAPI {
-    readonly SIDE_LENGTH = 3;
+    readonly SIDE_LENGTH = NONET_SIDE_LENGTH;
 
     indexForCellAt(row: number, col: number) {
         return to1D(
-            colFrom1D(row, this.SIDE_LENGTH),
-            colFrom1D(col, this.SIDE_LENGTH),
-            this.SIDE_LENGTH);
+            colFrom1D(row, NONET_SIDE_LENGTH),
+            colFrom1D(col, NONET_SIDE_LENGTH),
+            NONET_SIDE_LENGTH);
     }
 
     cellsIterator(nonet: number) {
         return House.cellsIterator((index: number) => {
             const row = to1D(
-                colFrom1D(nonet, this.SIDE_LENGTH),
-                colFrom1D(index, this.SIDE_LENGTH),
-                this.SIDE_LENGTH);
+                colFrom1D(nonet, NONET_SIDE_LENGTH),
+                colFrom1D(index, NONET_SIDE_LENGTH),
+                NONET_SIDE_LENGTH);
             const col = to1D(
-                rowFrom1D(nonet, this.SIDE_LENGTH),
-                rowFrom1D(index, this.SIDE_LENGTH),
-                this.SIDE_LENGTH);
+                rowFrom1D(nonet, NONET_SIDE_LENGTH),
+                rowFrom1D(index, NONET_SIDE_LENGTH),
+                NONET_SIDE_LENGTH);
             return Cell.at(row, col);
         });
     }
