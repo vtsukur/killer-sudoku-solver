9d73aec7c2caba92e8ef29f5f4876762e2c90bf7
Volodymyr Tsukur <volodymyrt@wix.com>
2023-01-17 18:25:56 +0200

    Migrating `Context` to TypeScript

---
+++ src/solver/strategies/context.ts
@@ -0,0 +1,57 @@
+import * as _ from 'lodash';
+import { CageModel } from '../models/elements/cageModel';
+import { CellModel } from '../models/elements/cellModel';
+import { MasterModel } from '../models/masterModel';
+import { CageSlicer } from '../transform/cageSlicer';
+
+export class Context {
+    readonly model;
+    readonly cageSlicer;
+    cageModelsToReevaluatePerms?: Array<CageModel>;
+    recentlySolvedCellModels: Array<CellModel>;
+    depth;
+    foundSolution?: Array<Array<number>>;
+
+    constructor(model: MasterModel, cageSlicer: CageSlicer) {
+        this.model = model;
+        this.cageSlicer = cageSlicer;
+        this.cageModelsToReevaluatePerms = undefined;
+        this.recentlySolvedCellModels = [];
+        this.depth = 0;
+        this.foundSolution = undefined;
+    }
+    
+    run(strategyFn: () => void) {
+        if (!this.isSolutionFound) {
+            strategyFn.apply(this);
+        }
+    }
+
+    get hasCageModelsToReevaluatePerms() {
+        return !_.isUndefined(this.cageModelsToReevaluatePerms);
+    }
+
+    clearCageModelsToReevaluatePerms() {
+        this.cageModelsToReevaluatePerms = undefined;
+    }
+
+    get hasRecentlySolvedCellModels() {
+        return this.recentlySolvedCellModels.length > 0;
+    }
+
+    clearRecentlySolvedCellModels() {
+        this.cageModelsToReevaluatePerms = [];
+    }
+
+    get isSolutionFound() {
+        return !_.isUndefined(this.foundSolution);
+    }
+
+    deepCopyForDeepTry() {
+        const modelCopy = this.model.deepCopy();
+        const cageSlicerCopy = new CageSlicer(modelCopy);
+        const copy = new Context(modelCopy, cageSlicerCopy);
+        copy.depth = this.depth + 1;
+        return copy;
+    }
+}
