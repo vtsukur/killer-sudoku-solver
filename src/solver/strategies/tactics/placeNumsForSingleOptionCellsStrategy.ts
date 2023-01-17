e00d3cc38f29f91d4725dfec5d205b8bf7a2ad9a
Volodymyr Tsukur <volodymyrt@wix.com>
2023-01-17 22:28:02 +0200

    Migrating `placeNumsForSingleOptionCellsStrategy` to TypeScript

--- src/solver/strategies/tactics/placeNumsForSingleOptionCellsStrategy.ts
+++ src/solver/strategies/tactics/placeNumsForSingleOptionCellsStrategy.ts
@@ -1,11 +1,13 @@
-import _ from 'lodash';
+import * as _ from 'lodash';
 import { House } from '../../../puzzle/house';
+import { CellModel } from '../../models/elements/cellModel';
+import { Context } from '../context';
 
-export function placeNumsForSingleOptionCellsStrategy() {
-    const solved = [];
+export function placeNumsForSingleOptionCellsStrategy(this: Context) {
+    const solved = new Array<CellModel>();
 
-    _.range(House.SIZE).forEach(row => {
-        _.range(House.SIZE).forEach(col => {
+    _.range(House.SIZE).forEach((row: number) => {
+        _.range(House.SIZE).forEach((col: number) => {
             const cellModel = this.model.cellModelAt(row, col);
             if (cellModel.numOpts().size === 1 && !cellModel.solved) {
                 this.model.placeNum(cellModel.cell, cellModel.numOpts().values().next().value);
