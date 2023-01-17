c84229e23d255049fd90a59ea509b455ad3f6af0
Volodymyr Tsukur <volodymyrt@wix.com>
2023-01-17 22:26:07 +0200

    Migrating `sliceCagesForSolvedCellsStrategy` to TypeScript

--- src/solver/strategies/tactics/sliceCagesForSolvedCellsStrategy.ts
+++ src/solver/strategies/tactics/sliceCagesForSolvedCellsStrategy.ts
@@ -1,10 +1,11 @@
 import { Cage } from '../../../puzzle/cage';
+import { Context } from '../context';
 
-export function sliceCagesForSolvedCellsStrategy() {
+export function sliceCagesForSolvedCellsStrategy(this: Context) {
     this.recentlySolvedCellModels.forEach(cellModel => {
         const withinCageModelsSet = cellModel.withinCageModels;
         if (!(withinCageModelsSet.size === 1 && withinCageModelsSet.values().next().value.positioningFlags.isSingleCellCage)) {
-            const firstChunkCage = Cage.ofSum(cellModel.placedNum).at(cellModel.cell.row, cellModel.cell.col).mk();
+            const firstChunkCage = Cage.ofSum(cellModel.placedNum as number).at(cellModel.cell.row, cellModel.cell.col).mk();
             this.cageSlicer.addAndSliceResidualCageRecursively(firstChunkCage);
         }
     });
