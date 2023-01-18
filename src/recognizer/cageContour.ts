488a0dac186db01a149a5850550310856439957a
Volodymyr Tsukur <volodymyrt@wix.com>
2023-01-18 14:22:44 +0200

    Migrating `CageContour` to TypeScript

---
+++ src/recognizer/cageContour.ts
@@ -0,0 +1,29 @@
+import * as _ from 'lodash';
+import { CellContour } from './cellContour';
+
+export class CageContour {
+    private _cellContours: Array<CellContour>;
+    topLeftCellContour?: CellContour;
+    sumImagePath?: string;
+
+    constructor() {
+        this._cellContours = [];
+        this.topLeftCellContour = undefined;
+        this.sumImagePath = undefined;
+    }
+
+    addCellContour(cellContour: CellContour) {
+        this._cellContours.push(cellContour);
+        this.#updateTopLeftCellContour(cellContour);
+    }
+
+    #updateTopLeftCellContour(cellContour: CellContour) {
+        if (_.isUndefined(this.topLeftCellContour) || cellContour.cell.indexWithinGrid < (this.topLeftCellContour as CellContour).cell.indexWithinGrid) {
+            this.topLeftCellContour = cellContour;
+        }
+    }
+
+    get cells() {
+        return this._cellContours.map(c => c.cell);
+    }
+}
