b27e3a19ea1008cbb0117092706fbed3fb3a6b25
Volodymyr Tsukur <volodymyrt@wix.com>
2023-01-18 14:06:25 +0200

    Migrating `Rect` to TypeScript

---
+++ src/recognizer/rect.ts
@@ -0,0 +1,21 @@
+export class Rect {
+    readonly x;
+    readonly y;
+    readonly width;
+    readonly height;
+
+    constructor(x: number, y: number, width: number, height: number) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    static from(rect: Rect) {
+        return new Rect(rect.x, rect.y, rect.width, rect.height);
+    }
+
+    toString() {
+        return `{ x: ${this.x}, y: ${this.y}, width: ${this.width}, height: ${this.height} }`;
+    }
+}
