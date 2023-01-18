fcbf84f2ead99faf7c455a48767bc53c6b03a309
Volodymyr Tsukur <volodymyrt@wix.com>
2023-01-18 13:48:11 +0200

    Migrating `reflectSolvedCellsStrategy` to TypeScript

--- src/solver/strategies/tactics/reflectSolvedCellsStrategy.ts
+++ src/solver/strategies/tactics/reflectSolvedCellsStrategy.ts
@@ -1,13 +1,14 @@
+import { Context } from '../context';
 import { reduceCageNumOptsBySolvedCellsStrategy } from './reduceCageNumOptsBySolvedCellsStrategy';
 import { reduceHousePermsBySolvedCellsStrategy } from './reduceHousePermsBySolvedCellsStrategy';
 import { sliceCagesForSolvedCellsStrategy } from './sliceCagesForSolvedCellsStrategy';
 
-export function reflectSolvedCellsStrategy() {
+export function reflectSolvedCellsStrategy(this: Context) {
     if (this.hasRecentlySolvedCellModels) {
         this.run(reduceCageNumOptsBySolvedCellsStrategy);
         this.run(reduceHousePermsBySolvedCellsStrategy);
         this.run(sliceCagesForSolvedCellsStrategy);
         this.clearRecentlySolvedCellModels();
-        this.cageModelsToReevaluatePerms = this.model.cageModelsMap.values();
+        this.cageModelsToReevaluatePerms = Array.from(this.model.cageModelsMap.values());
     }
 }
