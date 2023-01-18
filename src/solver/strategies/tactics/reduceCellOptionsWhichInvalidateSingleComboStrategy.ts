46bab448350e7f54a0895ade0701c51d763a0d02
Volodymyr Tsukur <volodymyrt@wix.com>
2023-01-18 13:39:11 +0200

    Migrating `reduceCellOptionsWhichInvalidateSingleComboStrategy` to TypeScript

--- src/solver/strategies/tactics/reduceCellOptionsWhichInvalidateSingleComboStrategy.ts
+++ src/solver/strategies/tactics/reduceCellOptionsWhichInvalidateSingleComboStrategy.ts
@@ -1,15 +1,20 @@
-import _ from 'lodash';
+import * as _ from 'lodash';
 import { Grid } from '../../../puzzle/grid';
+import { CageModel } from '../../models/elements/cageModel';
+import { CellModel } from '../../models/elements/cellModel';
+import { HouseModel } from '../../models/elements/houseModel';
+import { MasterModel } from '../../models/masterModel';
+import { Context } from '../context';
 
 const TARGET_CELL_NUM_OPTS_COUNT = 2;
 
-export function reduceCellOptionsWhichInvalidateSingleComboStrategy() {
+export function reduceCellOptionsWhichInvalidateSingleComboStrategy(this: Context) {
     if (this.hasCageModelsToReevaluatePerms) return;
 
-    let cageModelsToReduce = new Set();
+    let cageModelsToReduce = new Set<CageModel>();
 
-    _.range(0, Grid.SIDE_LENGTH).forEach(row => {
-        _.range(0, Grid.SIDE_LENGTH).forEach(col => {
+    _.range(0, Grid.SIDE_LENGTH).forEach((row: number) => {
+        _.range(0, Grid.SIDE_LENGTH).forEach((col: number) => {
             const cellM = this.model.cellModelsMatrix[row][col];
             if (cellM.solved || cellM.numOpts().size !== TARGET_CELL_NUM_OPTS_COUNT) return;
 
@@ -30,11 +35,11 @@ export function reduceCellOptionsWhichInvalidateSingleComboStrategy() {
         });
     });
 
-    this.cageModelsToReevaluatePerms = cageModelsToReduce.size > 0 ? cageModelsToReduce.values() : undefined;
+    this.cageModelsToReevaluatePerms = cageModelsToReduce.size > 0 ? Array.from(cageModelsToReduce.values()) : undefined;
 }
 
-function collectCellModelsToCheck(cellM, model) {
-    const cellMs = new Set();
+function collectCellModelsToCheck(cellM: CellModel, model: MasterModel) {
+    const cellMs = new Set<CellModel>();
     addCellsFromHouse(cellMs, model.rowModels[cellM.cell.row], model);
     addCellsFromHouse(cellMs, model.columnModels[cellM.cell.col], model);
     addCellsFromHouse(cellMs, model.nonetModels[cellM.cell.nonet], model);
@@ -42,14 +47,14 @@ function collectCellModelsToCheck(cellM, model) {
     return cellMs;
 }
 
-function addCellsFromHouse(cellMs, houseM, model) {
+function addCellsFromHouse(cellMs: Set<CellModel>, houseM: HouseModel, model: MasterModel) {
     for (const { row, col } of houseM.cellsIterator()) {
         cellMs.add(model.cellModelAt(row, col));
     }
 }
 
-function collectCageModelsToCheck(cellMs) {
-    const cageMs = new Set();
+function collectCageModelsToCheck(cellMs: Set<CellModel>) {
+    const cageMs = new Set<CageModel>();
     for (const cellM of cellMs) {
         if (cellM.solved) continue;
 
