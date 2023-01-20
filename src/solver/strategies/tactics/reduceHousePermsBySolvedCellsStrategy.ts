160cf858744d0f2cf65884ded545897fb72ec060
Volodymyr Tsukur <volodymyrt@wix.com>
2023-01-20 12:19:49 +0200

    Migrating `ReduceHousePermsBySolvedCellsStrategy` to `Strategy` infrastructure

--- src/solver/strategies/tactics/reduceHousePermsBySolvedCellsStrategy.ts
+++ src/solver/strategies/tactics/reduceHousePermsBySolvedCellsStrategy.ts
@@ -1,33 +1,35 @@
 import { CageModel } from '../../models/elements/cageModel';
-import { Context } from '../context';
+import { Strategy } from '../strategy';
 
-export function reduceHousePermsBySolvedCellsStrategy(this: Context) {
-    let cageMsToReduceSet = new Set<CageModel>();
+export class ReduceHousePermsBySolvedCellsStrategy extends Strategy {
+    execute() {
+        let cageMsToReduceSet = new Set<CageModel>();
 
-    this.recentlySolvedCellModels.forEach(cellM => {
-        const num = cellM.placedNum as number;
-        [
-            this.model.rowModels[cellM.cell.row],
-            this.model.columnModels[cellM.cell.col],
-            this.model.nonetModels[cellM.cell.nonet]
-        ].forEach(houseM => {
-            for (const { row, col } of houseM.cellsIterator()) {
-                if (row === cellM.cell.row && col === cellM.cell.col) continue;
-    
-                const aCellM = this.model.cellModelAt(row, col);
-                if (aCellM.hasNumOpt(num)) {
-                    aCellM.deleteNumOpt(num);
-                    cageMsToReduceSet = new Set([...cageMsToReduceSet, ...aCellM.withinCageModels]);
-                }
-            }    
+        this._context.recentlySolvedCellModels.forEach(cellM => {
+            const num = cellM.placedNum as number;
+            [
+                this._model.rowModels[cellM.cell.row],
+                this._model.columnModels[cellM.cell.col],
+                this._model.nonetModels[cellM.cell.nonet]
+            ].forEach(houseM => {
+                for (const { row, col } of houseM.cellsIterator()) {
+                    if (row === cellM.cell.row && col === cellM.cell.col) continue;
+        
+                    const aCellM = this._model.cellModelAt(row, col);
+                    if (aCellM.hasNumOpt(num)) {
+                        aCellM.deleteNumOpt(num);
+                        cageMsToReduceSet = new Set([...cageMsToReduceSet, ...aCellM.withinCageModels]);
+                    }
+                }    
+            });
         });
-    });
-
-    if (cageMsToReduceSet.size > 0) {
-        if (this.hasCageModelsToReevaluatePerms) {
-            this.cageModelsToReevaluatePerms = Array.from(new Set([...this.cageModelsToReevaluatePerms as Array<CageModel>, ...cageMsToReduceSet]));
-        } else {
-            this.cageModelsToReevaluatePerms = Array.from(cageMsToReduceSet);
+    
+        if (cageMsToReduceSet.size > 0) {
+            if (this._context.hasCageModelsToReevaluatePerms) {
+                this._context.cageModelsToReevaluatePerms = Array.from(new Set([...this._context.cageModelsToReevaluatePerms as Array<CageModel>, ...cageMsToReduceSet]));
+            } else {
+                this._context.cageModelsToReevaluatePerms = Array.from(cageMsToReduceSet);
+            }
         }
     }
 }
--- src/solver/strategies/tactics/reflectSolvedCellsStrategy.ts
+++ src/solver/strategies/tactics/reflectSolvedCellsStrategy.ts
@@ -1,13 +1,13 @@
 import { Strategy } from '../strategy';
 import { ReduceCageNumOptsBySolvedCellsStrategy } from './reduceCageNumOptsBySolvedCellsStrategy';
-import { reduceHousePermsBySolvedCellsStrategy } from './reduceHousePermsBySolvedCellsStrategy';
+import { ReduceHousePermsBySolvedCellsStrategy } from './reduceHousePermsBySolvedCellsStrategy';
 import { sliceCagesForSolvedCellsStrategy } from './sliceCagesForSolvedCellsStrategy';
 
 export class ReflectSolvedCellsStrategy extends Strategy {
     execute() {
         if (this._context.hasRecentlySolvedCellModels) {
             this.executeAnother(ReduceCageNumOptsBySolvedCellsStrategy);
-            this.executeAnotherFn(reduceHousePermsBySolvedCellsStrategy);
+            this.executeAnother(ReduceHousePermsBySolvedCellsStrategy);
             this.executeAnotherFn(sliceCagesForSolvedCellsStrategy);
             this._context.clearRecentlySolvedCellModels();
             this._context.cageModelsToReevaluatePerms = Array.from(this._model.cageModelsMap.values());
