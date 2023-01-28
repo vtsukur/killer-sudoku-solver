6f78fa581b5094e98150a0486cb323c46e4bab91
vtsukur <volodymyrt@wix.com>
2023-01-28 15:54:40 +0200

    Converting `Grid` to a non-constructible class with static members so that TSDoc can be properly generated as well as to remove overall complexity of declaration

--- src/puzzle/grid.ts
+++ src/puzzle/grid.ts
@@ -11,23 +11,41 @@ import { House } from './house';
  *
  * @public
  */
-class GridObject {
+export class Grid {
     /**
      * Amount of {@link Cell}s on Grid's side.
      */
-    readonly SIDE_CELL_COUNT = 9;
+    static readonly SIDE_CELL_COUNT = 9;
 
     /**
      * Total amount of {@link Cell}s on the Grid.
      */
-    readonly CELL_COUNT = this.SIDE_CELL_COUNT * this.SIDE_CELL_COUNT;
+    static readonly CELL_COUNT = this.SIDE_CELL_COUNT * this.SIDE_CELL_COUNT;
 
     /**
      * Total sum of all numbers on the Grid.
      */
-    readonly SUM = this.SIDE_CELL_COUNT * House.SUM;
+    static readonly SUM = this.SIDE_CELL_COUNT * House.SUM;
 
-    cellsIterator(): CellsIterator {
+    private constructor() {
+        // Non-contructible.
+    }
+
+    /**
+     * Produces new iterator over Grid's {@link Cell}s
+     * which consequently iterates from 'top left' `Cell` of the Grid to the 'right bottom' one.
+     *
+     * Rows are iterated consequently from first to last.
+     * Each row is iterated starting with its first column consequently to the last one.
+     * Row is iterated fully before proceeding to the next one, as follows:
+     * ```
+     * // (row, column)
+     * (0, 0) -> (0, 1) -> (0, 2) -> ...  -> (0, 7) -> (0, 8) -> (1, 0) -> (1, 1) -> ... -> (8, 8)
+     * ```
+     *
+     * @returns new iterator over Grid's {@link Cell}s.
+     */
+    static cellsIterator(): CellsIterator {
         return new CellsIterator((index: number) => {
             return Cell.at(
                 colFrom1D(index, Grid.SIDE_CELL_COUNT),
@@ -36,9 +54,7 @@ class GridObject {
         }, Grid.CELL_COUNT);
     }
 
-    newMatrix() {
+    static newMatrix() {
         return new Array(this.SIDE_CELL_COUNT).fill(undefined).map(() => new Array(this.SIDE_CELL_COUNT));
     }
 }
-
-export const Grid = Object.freeze(new GridObject());
