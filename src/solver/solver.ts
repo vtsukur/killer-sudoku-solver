9daeef226c8bc7a318634890e1c87406293665bc
vtsukur <volodymyrt@wix.com>
2023-01-29 21:52:46 +0200

    Adding TSDoc for `Solver`

--- src/solver/solver.ts
+++ src/solver/solver.ts
@@ -5,7 +5,20 @@ import { Context } from './strategies/context';
 import { MasterStrategy } from './strategies/masterStrategy';
 import { CageSlicer } from './transform/cageSlicer';
 
+/**
+ * `Solver` for Killer Sudoku `Puzzle`.
+ *
+ * @public
+ */
 export class Solver {
+
+    /**
+     * Solves the given Killer Sudoku `Puzzle`.
+     *
+     * @param puzzle - Problem definition for Killer Sudoku `Puzzle` described by {@link Cage}s.
+     *
+     * @returns Solution for Killer Sudoku `Puzzle` in the form of numbers matrix (array of arrays) indexed by row and then by column.
+     */
     solve(puzzle: Puzzle) {
         const model = new MasterModel(puzzle);
         const ctx = new Context(model, new CageSlicer(model));
