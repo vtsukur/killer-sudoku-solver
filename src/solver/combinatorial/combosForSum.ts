f1c3cd4e46933580eb09133922b714d00cfd246a
vtsukur <volodymyrt@wix.com>
2023-02-01 13:12:10 +0200

    Refactoring `combosForSum`

--- src/solver/combinatorial/combosForSum.ts
+++ src/solver/combinatorial/combosForSum.ts
@@ -15,62 +15,6 @@ export function combosForSum(sum: number, numCount: number): ReadonlyCombos {
     }
 }
 
-export function computeComboForSum(sum: number, numCount: number): ReadonlyCombos {
-    validate(sum, numCount);
-
-    if (sum < MIN_SUMS_PER_COUNT[numCount - 1] || sum > MAX_SUMS_PER_COUNT[numCount - 1]) {
-        return [];
-    }
-
-    const combos = new Array<Combo>();
-    const numbers = new Array<number>(numCount);
-    let currentSum = 0;
-
-    function combosRecursive(level: number, startWith: number) {
-        if (level > numCount) {
-            if (currentSum === sum) {
-                combos.push(Combo.of(...numbers));
-            }
-        } else {
-            for (let i = startWith; i <= House.CELL_COUNT; ++i) {
-                if (currentSum + i > sum) {
-                    return;
-                } else {
-                    numbers[level - 1] = i;
-                    currentSum += i;
-                    combosRecursive(level + 1, i + 1);
-                    currentSum -= i;
-                }
-            }
-        }
-    }
-
-    combosRecursive(1, 1);
-
-    return combos;
-}
-
-const validate = (sum: number, numCount: number) => {
-    if (sum < Numbers.MIN || sum > House.SUM) {
-        throw `Invalid sum. Value outside of range. Expected to be within [1, 45]. Actual: ${sum}`;
-    }
-    if (numCount < 1 || numCount > House.CELL_COUNT) {
-        throw `Invalid number count. Value outside of range. Expected to be within [1, 9]. Actual: ${numCount}`;
-    }
-};
-
-const MIN_SUMS_PER_COUNT = new Array(House.CELL_COUNT);
-const MAX_SUMS_PER_COUNT = new Array(House.CELL_COUNT);
-_.range(House.CELL_COUNT).forEach((count: number) => {
-    if (count == 0) {
-        MIN_SUMS_PER_COUNT[count] = 1;
-        MAX_SUMS_PER_COUNT[count] = House.CELL_COUNT;
-    } else {
-        MIN_SUMS_PER_COUNT[count] = MIN_SUMS_PER_COUNT[count - 1] + (count + 1);
-        MAX_SUMS_PER_COUNT[count] = MAX_SUMS_PER_COUNT[count - 1] + (House.CELL_COUNT - count);
-    }
-});
-
 const precomputeKey = (sum: number, numCount: number) => {
     return `${sum}_${numCount}`;
 };
@@ -83,7 +27,7 @@ const storePrecomputed = (source: string, numCount: number) => {
         const combos = new Array<Combo>();
         for (const combosStr of sumAndCombos[1].split(' ')) {
             const comboNumbers = new Array<number>();
-            for (const char of combosStr) {
+            for (const char of combosStr.trim()) {
                 comboNumbers.push(parseInt(char));
             }
             combos.push(Combo.of(...comboNumbers));
@@ -249,3 +193,59 @@ storePrecomputed(`
 storePrecomputed(`
     45: 123456789
 `, 9);
+
+export function computeComboForSum(sum: number, numCount: number): ReadonlyCombos {
+    validate(sum, numCount);
+
+    if (sum < MIN_SUMS_PER_COUNT[numCount - 1] || sum > MAX_SUMS_PER_COUNT[numCount - 1]) {
+        return [];
+    }
+
+    const combos = new Array<Combo>();
+    const numbers = new Array<number>(numCount);
+    let currentSum = 0;
+
+    function combosRecursive(level: number, startWith: number) {
+        if (level > numCount) {
+            if (currentSum === sum) {
+                combos.push(Combo.of(...numbers));
+            }
+        } else {
+            for (let i = startWith; i <= House.CELL_COUNT; ++i) {
+                if (currentSum + i > sum) {
+                    return;
+                } else {
+                    numbers[level - 1] = i;
+                    currentSum += i;
+                    combosRecursive(level + 1, i + 1);
+                    currentSum -= i;
+                }
+            }
+        }
+    }
+
+    combosRecursive(1, 1);
+
+    return combos;
+}
+
+const validate = (sum: number, numCount: number) => {
+    if (sum < Numbers.MIN || sum > House.SUM) {
+        throw `Invalid sum. Value outside of range. Expected to be within [1, 45]. Actual: ${sum}`;
+    }
+    if (numCount < 1 || numCount > House.CELL_COUNT) {
+        throw `Invalid number count. Value outside of range. Expected to be within [1, 9]. Actual: ${numCount}`;
+    }
+};
+
+const MIN_SUMS_PER_COUNT = new Array(House.CELL_COUNT);
+const MAX_SUMS_PER_COUNT = new Array(House.CELL_COUNT);
+_.range(House.CELL_COUNT).forEach((count: number) => {
+    if (count == 0) {
+        MIN_SUMS_PER_COUNT[count] = 1;
+        MAX_SUMS_PER_COUNT[count] = House.CELL_COUNT;
+    } else {
+        MIN_SUMS_PER_COUNT[count] = MIN_SUMS_PER_COUNT[count - 1] + (count + 1);
+        MAX_SUMS_PER_COUNT[count] = MAX_SUMS_PER_COUNT[count - 1] + (House.CELL_COUNT - count);
+    }
+});
