b9df33dbfa694ac6ba156b51fcd53a2f6bb444f8
vtsukur <volodymyrt@wix.com>
2023-02-02 23:59:27 +0200

    Adding TSDoc to `Combo`

--- src/solver/math/combo.ts
+++ src/solver/math/combo.ts
@@ -3,30 +3,72 @@ import { joinArray } from '../../util/readableMessages';
 
 export type ComboKey = string;
 
+/**
+ * Combination of numbers used mainly to represent addends of `Cage` sum.
+ *
+ * Combination allows duplication of numbers.
+ *
+ * @public
+ */
 export class Combo implements Iterable<number> {
+
+    /**
+     * Human-readable key describing combination of numbers.
+     */
     readonly key: ComboKey;
 
     private readonly _nums: ReadonlyArray<number>;
     private readonly _numSet: ReadonlySet<number>;
 
-    constructor(nums: ReadonlyArray<number>) {
-        this._nums = [...nums];
-        this._numSet = new MutableSet(nums);
-        this.key = joinArray(nums);
+    /**
+     * Constructs new combination of the given numbers.
+     *
+     * @param val - Numbers to construct a combination from.
+     */
+    constructor(val: ReadonlyArray<number>) {
+        this._nums = [...val];
+        this._numSet = new MutableSet(val);
+        this.key = joinArray(val);
     }
 
-    static of(...nums: ReadonlyArray<number>) {
-        return new Combo(nums);
+    /**
+     * Constructs new combination of the given numbers specified via rest parameters.
+     *
+     * @param val - Numbers to construct a combination from.
+     *
+     * @returns new combination of the given numbers.
+     */
+    static of(...val: ReadonlyArray<number>) {
+        return new Combo(val);
     }
 
+    /**
+     * Returns first number in the combination.
+     *
+     * @throws {RangeError} if the combination is empty.
+     */
     get number0() {
         return this.nthNumber(0);
     }
 
+    /**
+     * Returns second number in the combination.
+     *
+     * @throws {RangeError} if the combination doesn't have second element.
+     */
     get number1() {
         return this.nthNumber(1);
     }
 
+    /**
+     * Returns number with the n-th `index` in the combination.
+     *
+     * @param index - Index of the number in the combination.
+     *
+     * @returns number with the n-th `index` in the combination.
+     *
+     * @throws {RangeError} if the given index is outside of combination numbers range.
+     */
     nthNumber(index: number) {
         if (index < 0 || index > this._nums.length - 1) {
             throw new RangeError(`Number with index ${index} cannot be accessed. Combo has ${this._nums.length} elements`);
@@ -35,14 +77,36 @@ export class Combo implements Iterable<number> {
         }
     }
 
+    /**
+     * Convention-based method of iterator protocol that turns this object into iterable
+     * and allows its use in a `for...of` loop and various other syntaxes.
+     *
+     * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Iterators_and_Generators
+     *
+     * @returns iterator over numbers in this combination.
+     */
     [Symbol.iterator](): Iterator<number> {
         return this._nums.values();
     }
 
-    has(num: number) {
-        return this._numSet.has(num);
+    /**
+     * Checks if the given number is a part of this combination.
+     *
+     * @param val - Number to check for being a part of this combination.
+     *
+     * @returns `true` if the given number is a part of this combination, otherwise `false`.
+     */
+    has(val: number) {
+        return this._numSet.has(val);
     }
 
+    /**
+     * Checks if at least one number in the given `Iterable` is a part of this combination.
+     *
+     * @param val - `Iterable` of numbers to check against this combination.
+     *
+     * @returns `true` if at least one number in the given `Iterable` is a part of this combination, otherwise `false`.
+     */
     hasSome(val: Iterable<number>) {
         for (const num of val) {
             if (this._numSet.has(num)) {
@@ -52,6 +116,14 @@ export class Combo implements Iterable<number> {
         return false;
     }
 
+    /**
+     * Reduces the size of the combination by removing the given number.
+     *
+     * @param num - Number to reduce this combination by.
+     *
+     * @returns new combination without the given number if it is a part of this combination or
+     * this combination if the given number is not a part of this combination.
+     */
     reduce(num: number): Combo {
         const numIndex = this._nums.indexOf(num);
         if (numIndex !== -1) {
