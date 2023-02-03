5100ffb2bca70651a2538d8c4bd441690365dfce
vtsukur <volodymyrt@wix.com>
2023-02-03 12:58:44 +0200

    Adding one more unit test for `comboForHouse` and adding VS Code run configuration to be able to execute `comboForHouse` tests separately

--- tests/unit/solver/math/combosForHouse.test.ts
+++ tests/unit/solver/math/combosForHouse.test.ts
@@ -36,6 +36,28 @@ describe('Tests for the finder of number combinations to form a house model out 
         ]);
     });
 
+    test('Multiple combinations of numbers to form a complete house model with non-overlapping cages: test 2', () => {
+        expect(combosForHouse(houseMOf([
+            Cage.ofSum(10).at(0, 0).at(0, 1).new(),
+            Cage.ofSum(10).at(0, 2).at(1, 2).new(),
+            Cage.ofSum(11).at(1, 0).at(1, 1).new(),
+            Cage.ofSum(14).at(2, 0).at(2, 1).at(2, 2).new()
+        ]))).toEqual([
+            [ Combo.of(1, 9), Combo.of(2, 8), Combo.of(4, 7), Combo.of(3, 5, 6) ],
+            [ Combo.of(1, 9), Combo.of(2, 8), Combo.of(5, 6), Combo.of(3, 4, 7) ],
+            [ Combo.of(1, 9), Combo.of(3, 7), Combo.of(5, 6), Combo.of(2, 4, 8) ],
+            [ Combo.of(1, 9), Combo.of(4, 6), Combo.of(3, 8), Combo.of(2, 5, 7) ],
+            [ Combo.of(2, 8), Combo.of(1, 9), Combo.of(4, 7), Combo.of(3, 5, 6) ],
+            [ Combo.of(2, 8), Combo.of(1, 9), Combo.of(5, 6), Combo.of(3, 4, 7) ],
+            [ Combo.of(2, 8), Combo.of(3, 7), Combo.of(5, 6), Combo.of(1, 4, 9) ],
+            [ Combo.of(3, 7), Combo.of(1, 9), Combo.of(5, 6), Combo.of(2, 4, 8) ],
+            [ Combo.of(3, 7), Combo.of(2, 8), Combo.of(5, 6), Combo.of(1, 4, 9) ],
+            [ Combo.of(3, 7), Combo.of(4, 6), Combo.of(2, 9), Combo.of(1, 5, 8) ],
+            [ Combo.of(4, 6), Combo.of(1, 9), Combo.of(3, 8), Combo.of(2, 5, 7) ],
+            [ Combo.of(4, 6), Combo.of(3, 7), Combo.of(2, 9), Combo.of(1, 5, 8) ]
+        ]);
+    });
+
     test('Combination of numbers to form a complete house model with non-overlapping cages', () => {
         expect(combosForHouse(houseMOf([
             Cage.ofSum(4).at(1, 1).at(1, 2).new(),
