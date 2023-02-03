c7c4d006afaa30ed696f8efc3655cae986a196a2
vtsukur <volodymyrt@wix.com>
2023-02-03 18:00:58 +0200

    Adding relevant unit tests for `Sets`

--- tests/unit/util/sets.test.ts
+++ tests/unit/util/sets.test.ts
@@ -1,77 +1,33 @@
 import { Sets } from '../../../src/util/sets';
 
 describe('Sets tests', () => {
-    // test('Addition/removal of individual elements and collections', () => {
-    //     const set = new Set<number>();
-
-    //     expectSetWithValues(set, []);
-    //     expect(() => set.first).toThrow(new RangeError('Can\'t get first element. MutableSet has no values'));
-
-    //     set.add(1);
-    //     set.add(2);
-    //     expect(set.addCollection([ 1, 3, 4 ])).toBe(set);
-
-    //     expectSetWithValues(set, [ 1, 2, 3, 4 ]);
-    //     expect(set.first).toBe(1);
-    //     expect(set.has(5)).toBeFalsy();
-
-    //     set.delete(2);
-    //     expect(set.deleteCollection([ 1, 4 ])).toBe(set);
-
-    //     expectSetWithValues(set, [ 3 ]);
-    //     expect(set.first).toBe(3);
-    // });
-
-    // test('Construction of MutableSet from `Iterable`', () => {
-    //     const set = new Set([ 3, 2, 1 ]);
-
-    //     expectSetWithValues(set, [ 3, 2, 1 ]);
-    //     expect(set.first).toBe(3);
-    // });
-
-    // test('Construction of MutableSet using `of` static factory method', () => {
-    //     const set = SetUtils.of(3, 2, 1);
-
-    //     expectSetWithValues(set, [ 3, 2, 1 ]);
-    //     expect(set.first).toBe(3);
-    // });
-
-    // test('Construction of empty MutableSet', () => {
-    //     expectSetWithValues(new Set([]), []);
-    //     expectSetWithValues(SetUtils.of(), []);
-    // });
-
-    test('Clearing', () => {
-        const set = Sets.new(3, 2, 1);
-
-        set.clear();
-        expectSetWithValues(set, []);
-        expect(() => Sets.firstValue(set)).toThrow(new RangeError('Can\'t get first element. Set has no values'));
+    test('Construction of Set using `new` static factory method', () => {
+        expect(Sets.new(3, 2, 1)).toEqual(new Set([ 3, 2, 1 ]));
     });
 
-    const expectSetWithValues = <T>(set: Set<T>, values: ReadonlyArray<T>) => {
-        expect(set.size).toBe(values.length);
+    test('Adding `Iterable` values to the Set', () => {
+        const set = new Set<number>();
 
-        expect(Array.from(set)).toEqual(values);
-        expect(Array.from(set.keys())).toEqual(values);
-        expect(Array.from(set.values())).toEqual(values);
-        expect(Array.from(set.entries())).toEqual(values.map(val => [ val, val ]));
+        set.add(1);
+        Sets.unite(set, [ 2, 3 ]);
+        Sets.unite(set, [ 4, 5 ]);
 
-        for (const el of values) {
-            expect(set.has(el)).toBeTruthy();
-        }
+        expect(set).toEqual(new Set([ 1, 2, 3, 4, 5 ]));
+    });
 
-        const collectedViaForEach = new Array<T>();
-        const collector = (val: T) => {
-            collectedViaForEach.push(val);
-        };
-        set.forEach(collector);
-        expect(collectedViaForEach).toEqual(values);
+    test('Removing `Iterable` values from the Set', () => {
+        const set = new Set<number>([ 1, 2, 3, 4, 5 ]);
 
-        const collectedViaForOf = new Array<T>();
-        for (const val of set) {
-            collectedViaForOf.push(val);
-        }
-        expect(collectedViaForOf).toEqual(values);
-    };
+        Sets.differentiate(set, [ 2, 3 ]);
+
+        expect(set).toEqual(new Set([ 1, 4, 5 ]));
+    });
+
+    test('Getting first value from the non-empty Set', () => {
+        expect(Sets.firstValue(new Set<number>([ 1, 2, 3, 4, 5 ]))).toBe(1);
+    });
+
+    test('Getting first value from the empty Set', () => {
+        expect(() => Sets.firstValue(new Set())).toThrow(new RangeError('Can\'t get first value. Set has no values'));
+    });
 });
