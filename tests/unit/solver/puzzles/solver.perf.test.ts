ea7305c1d1a8a89d8485eb27b7afb64c3b3a8694
vtsukur <volodymyrt@wix.com>
2023-02-07 11:55:25 +0200

    Adding performance test for Solver

---
+++ tests/unit/solver/puzzles/solver.perf.test.ts
@@ -0,0 +1,28 @@
+import * as _ from 'lodash';
+import { Solver } from '../../../../src/solver/solver';
+import { puzzleSamples } from '../../puzzle/puzzleSamples';
+
+describe('Performance tests for Solver', () => {
+    const sudokuDotCom = puzzleSamples.sudokuDotCom;
+    const dailyKillerSudokuDotCom = puzzleSamples.dailyKillerSudokuDotCom;
+    const solver = new Solver();
+
+    test('Find solution for Daily Challenge (2022-04-06) by Sudoku.com', () => {
+        _.range(10).forEach(() => {
+            solver.solve(sudokuDotCom.dailyChallengeOf_2022_04_06);
+            solver.solve(sudokuDotCom.dailyChallengeOf_2022_08_12);
+            solver.solve(sudokuDotCom.dailyChallengeOf_2022_08_30);
+            solver.solve(sudokuDotCom.dailyChallengeOf_2022_10_18);
+            solver.solve(sudokuDotCom.dailyChallengeOf_2022_10_19);
+            solver.solve(sudokuDotCom.dailyChallengeOf_2022_10_22);
+            solver.solve(sudokuDotCom.dailyChallengeOf_2022_10_25);
+            solver.solve(sudokuDotCom.dailyChallengeOf_2022_11_01);
+            solver.solve(sudokuDotCom.dailyChallengeOf_2022_11_10);
+            solver.solve(sudokuDotCom.randomExpertLevelChallenge);
+            solver.solve(dailyKillerSudokuDotCom.puzzle24789_difficulty10);
+            solver.solve(dailyKillerSudokuDotCom.puzzle24889_difficulty10);
+            solver.solve(dailyKillerSudokuDotCom.puzzle24914_difficulty10);
+            solver.solve(dailyKillerSudokuDotCom.puzzle24919_difficulty10);
+        });
+    });
+});
