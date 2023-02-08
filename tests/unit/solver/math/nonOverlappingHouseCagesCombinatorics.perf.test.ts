54f2596d78e050088fce0a5f0343c7a30f7f61d2
vtsukur <volodymyrt@wix.com>
2023-02-08 15:14:42 +0200

    Adding performance tests for `NonOverlappingHouseCagesCombinatorics`

---
+++ tests/unit/solver/math/nonOverlappingHouseCagesCombinatorics.perf.test.ts
@@ -0,0 +1,36 @@
+import * as _ from 'lodash';
+import { Cage } from '../../../../src/puzzle/cage';
+import { Combo } from '../../../../src/solver/math';
+import { NonOverlappingHouseCagesCombinatorics } from '../../../../src/solver/math/nonOverlappingHouseCagesCombinatorics';
+
+describe('Performance tests for NonOverlappingHouseCagesCombinatorics', () => {
+    const ITERATION_COUNT = 50000;
+
+    test('Cages in descending order of combination count', () => {
+        const cages = [
+            Cage.ofSum(14).at(2, 0).at(2, 1).at(2, 2).new(),
+            Cage.ofSum(10).at(0, 0).at(0, 1).new(),
+            Cage.ofSum(10).at(0, 2).at(1, 2).new(),
+            Cage.ofSum(11).at(1, 0).at(1, 1).new()
+        ];
+
+        expect(NonOverlappingHouseCagesCombinatorics.computeCombosAndPerms(cages).perms).toEqual([
+            [ Combo.of(1, 4, 9), Combo.of(2, 8), Combo.of(3, 7), Combo.of(5, 6) ],
+            [ Combo.of(1, 4, 9), Combo.of(3, 7), Combo.of(2, 8), Combo.of(5, 6) ],
+            [ Combo.of(1, 5, 8), Combo.of(3, 7), Combo.of(4, 6), Combo.of(2, 9) ],
+            [ Combo.of(1, 5, 8), Combo.of(4, 6), Combo.of(3, 7), Combo.of(2, 9) ],
+            [ Combo.of(2, 4, 8), Combo.of(1, 9), Combo.of(3, 7), Combo.of(5, 6) ],
+            [ Combo.of(2, 4, 8), Combo.of(3, 7), Combo.of(1, 9), Combo.of(5, 6) ],
+            [ Combo.of(2, 5, 7), Combo.of(1, 9), Combo.of(4, 6), Combo.of(3, 8) ],
+            [ Combo.of(2, 5, 7), Combo.of(4, 6), Combo.of(1, 9), Combo.of(3, 8) ],
+            [ Combo.of(3, 4, 7), Combo.of(1, 9), Combo.of(2, 8), Combo.of(5, 6) ],
+            [ Combo.of(3, 4, 7), Combo.of(2, 8), Combo.of(1, 9), Combo.of(5, 6) ],
+            [ Combo.of(3, 5, 6), Combo.of(1, 9), Combo.of(2, 8), Combo.of(4, 7) ],
+            [ Combo.of(3, 5, 6), Combo.of(2, 8), Combo.of(1, 9), Combo.of(4, 7) ]
+        ]);
+
+        _.range(ITERATION_COUNT).forEach(() => {
+            NonOverlappingHouseCagesCombinatorics.computeCombosAndPerms(cages);
+        });
+    });
+});
