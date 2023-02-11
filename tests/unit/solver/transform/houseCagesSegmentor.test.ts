2584cf44699bb1fdf6299b687ac0fa131c87cc81
vtsukur <volodymyrt@wix.com>
2023-02-11 22:29:55 +0200

    Adding unit tests for `HouseCagesSegmentor`

---
+++ tests/unit/solver/transform/houseCagesSegmentor.test.ts
@@ -0,0 +1,29 @@
+import { Cage } from '../../../../src/puzzle/cage';
+import { HouseCagesSegmentor } from '../../../../src/solver/transform/houseCagesSegmentor';
+import { newHouseModel } from '../math/houseModelBuilder';
+
+describe('Unit tests for `HouseCagesSegmentor`', () => {
+    const segment = HouseCagesSegmentor.segmentByCellsOverlap;
+
+    test('Segmentation of `House` `Cage`s with 2 overlapping `Cage`s', () => {
+        const houseModel = newHouseModel([
+            Cage.ofSum(7).at(0, 0).at(0, 1).new(),
+            Cage.ofSum(18).at(1, 0).at(1, 1).at(2, 0).new(),
+            Cage.ofSum(20).at(0, 2).at(1, 2).at(2, 1).at(2, 2).new(),
+            Cage.ofSum(21).at(0, 2).at(1, 0).at(1, 1).at(1, 2).new(),
+            Cage.ofSum(17).at(2, 0).at(2, 1).at(2, 2).new()
+        ]);
+
+        expect(segment(houseModel.cages, houseModel.cells)).toEqual({
+            nonOverlappingCages: [
+                Cage.ofSum(7).at(0, 0).at(0, 1).new(),
+                Cage.ofSum(18).at(1, 0).at(1, 1).at(2, 0).new(),
+                Cage.ofSum(20).at(0, 2).at(1, 2).at(2, 1).at(2, 2).new()
+            ],
+            overlappingCages: [
+                Cage.ofSum(21).at(0, 2).at(1, 0).at(1, 1).at(1, 2).new(),
+                Cage.ofSum(17).at(2, 0).at(2, 1).at(2, 2).new()
+            ]
+        });
+    });
+});
