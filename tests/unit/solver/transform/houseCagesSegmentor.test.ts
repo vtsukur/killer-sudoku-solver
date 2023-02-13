118179db609a9fff70bcac5b43959fdbfac3fdf6
vtsukur <volodymyrt@wix.com>
2023-02-13 21:25:02 +0200

    Adding unit test for N-`House` `Cage`s segmentation

--- tests/unit/solver/transform/houseCagesSegmentor.test.ts
+++ tests/unit/solver/transform/houseCagesSegmentor.test.ts
@@ -50,4 +50,36 @@ describe('Unit tests for `HouseCagesSegmentor`', () => {
             ]
         });
     });
+
+    test('Segmentation of `Cage`s in a big 4-`House` area', () => {
+        const houseModel = newHouseModel([
+            Cage.ofSum(12).at(2, 3).at(3, 2).at(3, 3).new(),
+            Cage.ofSum(5).at(5, 0).at(5, 1).new(),
+            Cage.ofSum(24).at(4, 4).at(4, 5).at(5, 4).new(),
+            Cage.ofSum(7).at(3, 5).at(3, 6).new(),
+            Cage.ofSum(12).at(4, 6).at(4, 7).at(4, 8).new(),
+            Cage.ofSum(20).at(2, 4).at(2, 5).at(3, 4).at(3, 7).at(3, 8).new(),
+            Cage.ofSum(12).at(3, 0).at(3, 1).new(),
+            Cage.ofSum(10).at(4, 0).at(4, 1).new(),
+            Cage.ofSum(14).at(2, 4).at(2, 5).at(2, 6).at(3, 4).new(),
+            Cage.ofSum(20).at(2, 6).at(3, 2).at(3, 3).at(3, 4).new()
+        ]);
+
+        expect(segment(houseModel.cages, houseModel.cells, 36)).toEqual({
+            nonOverlappingCages: [
+                Cage.ofSum(12).at(2, 3).at(3, 2).at(3, 3).new(),
+                Cage.ofSum(5).at(5, 0).at(5, 1).new(),
+                Cage.ofSum(24).at(4, 4).at(4, 5).at(5, 4).new(),
+                Cage.ofSum(7).at(3, 5).at(3, 6).new(),
+                Cage.ofSum(12).at(4, 6).at(4, 7).at(4, 8).new(),
+                Cage.ofSum(20).at(2, 4).at(2, 5).at(3, 4).at(3, 7).at(3, 8).new(),
+                Cage.ofSum(12).at(3, 0).at(3, 1).new(),
+                Cage.ofSum(10).at(4, 0).at(4, 1).new()
+            ],
+            overlappingCages: [
+                Cage.ofSum(14).at(2, 4).at(2, 5).at(2, 6).at(3, 4).new(),
+                Cage.ofSum(20).at(2, 6).at(3, 2).at(3, 3).at(3, 4).new()
+            ]
+        });
+    });
 });
