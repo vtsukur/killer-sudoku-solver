dbef365380ff7ca440d2aae6bea033a2b5ece048
vtsukur <volodymyrt@wix.com>
2023-02-14 16:39:56 +0200

    [refactoring] Finishing up with remaining renamings

--- tests/unit/solver/math/numCheckingSet.test.ts
+++ tests/unit/solver/math/numCheckingSet.test.ts
@@ -4,19 +4,19 @@ import { NumCheckingSet } from '../../../../src/solver/math/numCheckingSet';
 
 describe('Unit tests for `NumCheckingSet`', () => {
     test('Construction of `NumCheckingSet` from array of numbers', () => {
-        expectFastNumSetWithValues(new NumCheckingSet([ 1, 6, 9 ]), [ 1, 6, 9 ]);
+        expectNumCheckingSetWithValues(new NumCheckingSet([ 1, 6, 9 ]), [ 1, 6, 9 ]);
     });
 
     test('Construction of `NumCheckingSet` using `of` static factory method', () => {
-        expectFastNumSetWithValues(NumCheckingSet.of(2, 4, 5), [ 2, 4, 5 ]);
+        expectNumCheckingSetWithValues(NumCheckingSet.of(2, 4, 5), [ 2, 4, 5 ]);
     });
 
     test('Construction of empty `NumCheckingSet` using constructor', () => {
-        expectFastNumSetWithValues(new NumCheckingSet(), []);
+        expectNumCheckingSetWithValues(new NumCheckingSet(), []);
     });
 
     test('Construction of empty `NumCheckingSet` using `of` static factory method', () => {
-        expectFastNumSetWithValues(NumCheckingSet.of(), []);
+        expectNumCheckingSetWithValues(NumCheckingSet.of(), []);
     });
 
     test('Adding and removing numbers with `hasAll` and `doesNotHaveAny` checks', () => {
@@ -26,7 +26,7 @@ describe('Unit tests for `NumCheckingSet`', () => {
         expect(numSet.doesNotHaveAny(NumCheckingSet.of(8, 9))).toBeFalsy();
 
         numSet.add(NumCheckingSet.of(1, 7, 8));
-        expectFastNumSetWithValues(numSet, [ 1, 6, 7, 8, 9 ]);
+        expectNumCheckingSetWithValues(numSet, [ 1, 6, 7, 8, 9 ]);
         expect(numSet.hasAll(NumCheckingSet.of(1, 6))).toBeTruthy();
         expect(numSet.hasAll(NumCheckingSet.of(7, 8, 9))).toBeTruthy();
         expect(numSet.hasAll(NumCheckingSet.of(1, 6, 7, 8, 9))).toBeTruthy();
@@ -36,7 +36,7 @@ describe('Unit tests for `NumCheckingSet`', () => {
         expect(numSet.doesNotHaveAny(NumCheckingSet.of(5, 9))).toBeFalsy();
 
         numSet.remove(NumCheckingSet.of(2, 6, 9));
-        expectFastNumSetWithValues(numSet, [ 1, 7, 8 ]);
+        expectNumCheckingSetWithValues(numSet, [ 1, 7, 8 ]);
         expect(numSet.hasAll(NumCheckingSet.of(7, 8))).toBeTruthy();
         expect(numSet.hasAll(NumCheckingSet.of(1, 7, 8))).toBeTruthy();
         expect(numSet.hasAll(NumCheckingSet.of(1, 9))).toBeFalsy();
@@ -58,7 +58,7 @@ describe('Unit tests for `NumCheckingSet`', () => {
         );
     });
 
-    const expectFastNumSetWithValues = (numSet: NumCheckingSet, values: ReadonlyArray<number>) => {
+    const expectNumCheckingSetWithValues = (numSet: NumCheckingSet, values: ReadonlyArray<number>) => {
         const set = new Set(values);
         _.range(Numbers.MIN, Numbers.MAX + 1).forEach(num => {
             expect(numSet.hasAll(NumCheckingSet.of(num))).toBe(set.has(num));
