8a8b13372ced2fd93cf3ebe3fddb2fbce76f84ca
vtsukur <volodymyrt@wix.com>
2023-02-15 22:02:36 +0200

    [comment] Adding elaborate implementation comments to explain bitwise arithmetic

--- src/solver/math/gridRangeNumCheckingSet.ts
+++ src/solver/math/gridRangeNumCheckingSet.ts
@@ -42,6 +42,20 @@ export class GridRangeNumCheckingSet implements ReadonlyGridRangeNumCheckingSet 
     private constructor(val: ReadonlyArray<number>) {
         for (const num of val) {
             const entry = GridRangeNumCheckingSet._NUM_TO_BIT_STORE_BUCKET_MAPPING[num];
+
+            //
+            // Applying bitwise OR with left-wise shift to mark bit at position `num` as `1`.
+            //
+            // Examples:
+            //  - for `num = 0`, `bitStore` will be bitwise OR-ed with `0b00000001`;
+            //  - for `num = 1`, `bitStore` will be bitwise OR-ed with `0b00000010`;
+            //  - for `num = 2`, `bitStore` will be bitwise OR-ed with `0b00000100`;
+            //  - ...
+            //  - for `num = 8`, `bitStore` will be bitwise OR-ed with `0b10000000`;
+            //  - and so on, up to `num` value of 27 per one bit store.
+            //
+            // For `num = 1` and `num = 4` `bitStore` will be `0b00010010`.
+            //
             this._bitStore[entry.bucketIndex] |= 1 << entry.shiftWithinBucket;
         }
     }
@@ -72,18 +86,78 @@ export class GridRangeNumCheckingSet implements ReadonlyGridRangeNumCheckingSet 
      * @see {ReadonlyCheckingSet.hasAll}
      */
     hasAll(val: ReadonlyGridRangeNumCheckingSet) {
-        return (this._bitStore[0] & val.bitStore[0]) === val.bitStore[0] &&
-            (this._bitStore[1] & val.bitStore[1]) === val.bitStore[1] &&
-            (this._bitStore[2] & val.bitStore[2]) === val.bitStore[2];
+        //
+        // Applying bitwise AND to check that each bit store of this checking set
+        // has `1`s at the same positions as each bit store of the `val` checking set.
+        //
+        // Example for `hasAll` returning `true`
+        // (applied to a single bit store with index `x` for simplicity):
+        // ```
+        //      this._bitStore[x]                   = 0b10010101
+        //      val.bitStore[x]                     = 0b10000100
+        //      this._bitStore[x] & val.bitStore[x] = 0b10000100
+        //
+        //      0b10000100 === 0b10000100
+        //      (this._bitStore[x] & val.bitStore[x]) === val.bitStore[x]
+        //
+        // ```
+        //
+        // Example for `hasAll` returning `false`
+        // (applied to a single bit store with index `x` for simplicity):
+        // ```
+        //      this._bitStore[x]                   = 0b10010101
+        //      val.bitStore[x]                     = 0b00001100
+        //      this._bitStore[x] & val.bitStore[x] = 0b00000100
+        //
+        //      0b00001100 !== 0b00000100
+        //      (this._bitStore[x] & val.bitStore[x]) !== val.bitStore[x]
+        // ```
+        //
+        return (
+            (this._bitStore[0] & val.bitStore[0]) === val.bitStore[0] // numbers in range [0, 26]
+            &&
+            (this._bitStore[1] & val.bitStore[1]) === val.bitStore[1] // numbers in range [27, 53]
+            &&
+            (this._bitStore[2] & val.bitStore[2]) === val.bitStore[2] // numbers in range [54, 80]
+        );
     }
 
     /**
      * @see {ReadonlyheckingSet.doesNotHaveAny}
      */
     doesNotHaveAny(val: ReadonlyGridRangeNumCheckingSet) {
-        return (this._bitStore[0] & val.bitStore[0]) === 0 &&
-            (this._bitStore[1] & val.bitStore[1]) === 0 &&
-            (this._bitStore[2] & val.bitStore[2]) === 0;
+        //
+        // Applying bitwise AND to check that each bit store of this checking set
+        // does NOT have `1`s at the same positions as each bit store of the `val` checking set.
+        //
+        // Example for `doesNotHaveAny` returning `true`
+        // (applied to a single bit store with index `x` for simplicity):
+        // ```
+        //      this._bitStore[x]                   = 0b10010101
+        //      val.bitStore[x]                     = 0b01101000
+        //      this._bitStore[x] & val.bitStore[x] = 0b00000100
+        //
+        //      (this._bitStore[x] & val.bitStore[x]) === 0
+        //
+        // ```
+        //
+        // Example for `doesNotHaveAny` returning `false`
+        // (applied to a single bit store with index `x` for simplicity):
+        // ```
+        //      this._bitStore[x]                   = 0b10010101
+        //      val.bitStore[x]                     = 0b00001100
+        //      this._bitStore[x] & val.bitStore[x] = 0b00000100
+        //
+        //      (this._bitStore[x] & val.bitStore[x]) !== 0
+        // ```
+        //
+        return (
+            (this._bitStore[0] & val.bitStore[0]) === 0 // numbers in range [0, 26]
+            &&
+            (this._bitStore[1] & val.bitStore[1]) === 0 // numbers in range [27, 53]
+            &&
+            (this._bitStore[2] & val.bitStore[2]) === 0 // numbers in range [54, 80]
+        );
     }
 
     /**
@@ -97,9 +171,20 @@ export class GridRangeNumCheckingSet implements ReadonlyGridRangeNumCheckingSet 
      * @param val - Another checking set containing numbers to add to this set.
      */
     add(val: ReadonlyGridRangeNumCheckingSet) {
-        this._bitStore[0] |= val.bitStore[0];
-        this._bitStore[1] |= val.bitStore[1];
-        this._bitStore[2] |= val.bitStore[2];
+        //
+        // Applying bitwise OR assignment on the bit stores of this checking set
+        // to merge `1`s from the bit stores of `val` checking set.
+        //
+        // Example (applied to a single bit store with index `x` for simplicity):
+        // ```
+        //      this._bitStore[x]                    = 0b10010001
+        //      val.bitStore[x]                      = 0b01001000
+        //      this._bitStore[x] |= val.bitStore[x] = 0b11011001
+        // ```
+        //
+        this._bitStore[0] |= val.bitStore[0]; // for numbers in range [0, 26]
+        this._bitStore[1] |= val.bitStore[1]; // for numbers in range [27, 53]
+        this._bitStore[2] |= val.bitStore[2]; // for numbers in range [54, 80]
     }
 
     /**
@@ -113,8 +198,20 @@ export class GridRangeNumCheckingSet implements ReadonlyGridRangeNumCheckingSet 
      * @param val - Another checking set containing numbers to remove from this checking set.
      */
     remove(val: ReadonlyGridRangeNumCheckingSet) {
-        this._bitStore[0] &= ~val.bitStore[0];
-        this._bitStore[1] &= ~val.bitStore[1];
-        this._bitStore[2] &= ~val.bitStore[2];
+        //
+        // Applying bitwise AND assignment on the bit stores of this checking set
+        // to merge `1`s from the bit stores of `val` checking set.
+        //
+        // Example (applied to a single bit store with index `x` for simplicity):
+        // ```
+        //      this._bitStore[x]                     = 0b10011001
+        //      val.bitStore[x]                       = 0b01001001
+        //      ~val.bitStore[x]                      = 0b10110110
+        //      this._bitStore[x] &= ~val.bitStore[x] = 0b10010000
+        // ```
+        //
+        this._bitStore[0] &= ~val.bitStore[0]; // for numbers in range [0, 26]
+        this._bitStore[1] &= ~val.bitStore[1]; // for numbers in range [27, 53]
+        this._bitStore[2] &= ~val.bitStore[2]; // for numbers in range [54, 80]
     }
 }
