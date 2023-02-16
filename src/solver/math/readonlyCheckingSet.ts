4b776f98cc9f9c28cbdaf86940b887576f57aefa
vtsukur <volodymyrt@wix.com>
2023-02-16 21:49:16 +0200

    [refactoring] Adding mutable version of `NumsCheckingSet` type

--- src/solver/math/readonlyCheckingSet.ts
+++ src/solver/math/readonlyCheckingSet.ts
@@ -10,7 +10,7 @@
 export type BitStore32 = number;
 
 /**
- * Checking set with efficient storage & fast checking/manipulation operations.
+ * Checking numbers set with efficient storage & fast checking operations.
  *
  * Both memory and speed are of O(1) complexity due to the use of bitwise arithmetic on numbers.
  *
@@ -19,26 +19,62 @@ export type BitStore32 = number;
  *
  * @typeParam T - Specific subtype of {@link ReadonlyNumsCheckingSet}
  * to be used as an argument for checking operations.
- *
- * @public
  */
 export interface ReadonlyNumsCheckingSet<T extends ReadonlyNumsCheckingSet<T>> {
 
     /**
-     * Checks if this set has ALL values from another checking set.
+     * Checks if this set has ALL numbers from another checking set.
      *
      * @param val - Another set to check against.
      *
-     * @returns `true` if this checking set has ALL values from another checking set; otherwise `false`.
+     * @returns `true` if this checking set has ALL numbers from another checking set; otherwise `false`.
      */
     hasAll(val: T): boolean;
 
     /**
-     * Checks if this set does NOT have any values from another checking set.
+     * Checks if this set does NOT have any numbers from another checking set.
      *
      * @param val - Another set to check against.
      *
-     * @returns `true` if this checking set does NOT have any values from another checking set; otherwise `false`.
+     * @returns `true` if this checking set does NOT have any numbers from another checking set; otherwise `false`.
      */
     doesNotHaveAny(val: T): boolean;
+}
+
+/**
+ * Checking numbers set with efficient storage & fast checking and manipulation operations.
+ *
+ * Both memory and speed are of O(1) complexity due to the use of bitwise arithmetic on numbers.
+ *
+ * For performance reasons, implementations of this interface are NEITHER required to do range checks,
+ * NOR to guarantee correct work for the values outside of the range.
+ *
+ * @typeParam T - Specific subtype of {@link ReadonlyNumsCheckingSet}
+ * to be used as an argument for checking and manipulation operations.
+ */
+export interface NumsCheckingSet<T extends ReadonlyNumsCheckingSet<T>> extends ReadonlyNumsCheckingSet<T> {
+
+    /**
+     * Adds all numbers from another checking set to this checking numbers set.
+     *
+     * This method changes this checking numbers set and does NOT modify `val` checking numbers set.
+     *
+     * Only the numbers which are NOT yet present in this checking set are added.
+     * Duplicate numbers are ignored.
+     *
+     * @param val - Another checking set containing numbers to add to this set.
+     */
+    add(val: T): void;
+
+    /**
+     * Removes all numbers present in another checking set from this checking numbers set.
+     *
+     * This method changes this checking numbers set and does NOT modify `val` checking numbers set.
+     *
+     * Only the numbers which are present in this checking set are removed.
+     * Missing numbers are ignored.
+     *
+     * @param val - Another checking set containing numbers to remove from this checking set.
+     */
+    remove(val: T): void;
 }
