84c89ee4ca7f228f98082a774063ee87ea7b722f
vtsukur <volodymyrt@wix.com>
2023-02-16 22:22:03 +0200

    [refactoring] Fixing incomplete renamings

--- tests/unit/solver/math/cellIndicesCheckingSet.test.ts
+++ tests/unit/solver/math/cellIndicesCheckingSet.test.ts
@@ -4,40 +4,40 @@ import { CellIndicesCheckingSet } from '../../../../src/solver/math';
 
 describe('Unit tests for `CellIndicesCheckingSet`', () => {
     test('Construction of `CellIndicesCheckingSet` using `of` static factory method', () => {
-        expectNumCheckingSetWithValues(CellIndicesCheckingSet.of(1, 30, 75), [ 1, 30, 75 ]);
+        expectNumsCheckingSetWithValues(CellIndicesCheckingSet.of(1, 30, 75), [ 1, 30, 75 ]);
     });
 
     test('Adding and removing numbers with `hasAll` and `doesNotHaveAny` checks', () => {
-        const numSet = CellIndicesCheckingSet.of(1, 30, 75);
+        const numsCheckingSet = CellIndicesCheckingSet.of(1, 30, 75);
 
-        expect(numSet.hasAll(CellIndicesCheckingSet.of(1, 30))).toBeTruthy();
-        expect(numSet.hasAll(CellIndicesCheckingSet.of(30, 75))).toBeTruthy();
-        expect(numSet.hasAll(CellIndicesCheckingSet.of(1, 30, 75))).toBeTruthy();
-        expect(numSet.hasAll(CellIndicesCheckingSet.of(2, 75))).toBeFalsy();
-        expect(numSet.doesNotHaveAny(CellIndicesCheckingSet.of(2, 40))).toBeTruthy();
-        expect(numSet.doesNotHaveAny(CellIndicesCheckingSet.of(30, 40))).toBeFalsy();
+        expect(numsCheckingSet.hasAll(CellIndicesCheckingSet.of(1, 30))).toBeTruthy();
+        expect(numsCheckingSet.hasAll(CellIndicesCheckingSet.of(30, 75))).toBeTruthy();
+        expect(numsCheckingSet.hasAll(CellIndicesCheckingSet.of(1, 30, 75))).toBeTruthy();
+        expect(numsCheckingSet.hasAll(CellIndicesCheckingSet.of(2, 75))).toBeFalsy();
+        expect(numsCheckingSet.doesNotHaveAny(CellIndicesCheckingSet.of(2, 40))).toBeTruthy();
+        expect(numsCheckingSet.doesNotHaveAny(CellIndicesCheckingSet.of(30, 40))).toBeFalsy();
 
-        numSet.add(CellIndicesCheckingSet.of(0, 1, 7, 28, 80));
-        expectNumCheckingSetWithValues(numSet, [ 0, 1, 7, 28, 30, 75, 80 ]);
-        expect(numSet.hasAll(CellIndicesCheckingSet.of(0, 7))).toBeTruthy();
-        expect(numSet.hasAll(CellIndicesCheckingSet.of(1, 7, 28, 80))).toBeTruthy();
-        expect(numSet.hasAll(CellIndicesCheckingSet.of(1, 5))).toBeFalsy();
-        expect(numSet.hasAll(CellIndicesCheckingSet.of(2, 6, 9))).toBeFalsy();
-        expect(numSet.doesNotHaveAny(CellIndicesCheckingSet.of(2, 3))).toBeTruthy();
-        expect(numSet.doesNotHaveAny(CellIndicesCheckingSet.of(1, 9))).toBeFalsy();
+        numsCheckingSet.add(CellIndicesCheckingSet.of(0, 1, 7, 28, 80));
+        expectNumsCheckingSetWithValues(numsCheckingSet, [ 0, 1, 7, 28, 30, 75, 80 ]);
+        expect(numsCheckingSet.hasAll(CellIndicesCheckingSet.of(0, 7))).toBeTruthy();
+        expect(numsCheckingSet.hasAll(CellIndicesCheckingSet.of(1, 7, 28, 80))).toBeTruthy();
+        expect(numsCheckingSet.hasAll(CellIndicesCheckingSet.of(1, 5))).toBeFalsy();
+        expect(numsCheckingSet.hasAll(CellIndicesCheckingSet.of(2, 6, 9))).toBeFalsy();
+        expect(numsCheckingSet.doesNotHaveAny(CellIndicesCheckingSet.of(2, 3))).toBeTruthy();
+        expect(numsCheckingSet.doesNotHaveAny(CellIndicesCheckingSet.of(1, 9))).toBeFalsy();
 
-        numSet.remove(CellIndicesCheckingSet.of(1, 7, 30, 75));
-        expectNumCheckingSetWithValues(numSet, [ 0, 28, 80 ]);
-        expect(numSet.hasAll(CellIndicesCheckingSet.of(0, 28))).toBeTruthy();
-        expect(numSet.hasAll(CellIndicesCheckingSet.of(0, 80))).toBeTruthy();
-        expect(numSet.hasAll(CellIndicesCheckingSet.of(28, 80))).toBeTruthy();
-        expect(numSet.hasAll(CellIndicesCheckingSet.of(0, 7))).toBeFalsy();
-        expect(numSet.hasAll(CellIndicesCheckingSet.of(1, 7, 28, 80))).toBeFalsy();
-        expect(numSet.doesNotHaveAny(CellIndicesCheckingSet.of(2, 3))).toBeTruthy();
-        expect(numSet.doesNotHaveAny(CellIndicesCheckingSet.of(0, 9))).toBeFalsy();
+        numsCheckingSet.remove(CellIndicesCheckingSet.of(1, 7, 30, 75));
+        expectNumsCheckingSetWithValues(numsCheckingSet, [ 0, 28, 80 ]);
+        expect(numsCheckingSet.hasAll(CellIndicesCheckingSet.of(0, 28))).toBeTruthy();
+        expect(numsCheckingSet.hasAll(CellIndicesCheckingSet.of(0, 80))).toBeTruthy();
+        expect(numsCheckingSet.hasAll(CellIndicesCheckingSet.of(28, 80))).toBeTruthy();
+        expect(numsCheckingSet.hasAll(CellIndicesCheckingSet.of(0, 7))).toBeFalsy();
+        expect(numsCheckingSet.hasAll(CellIndicesCheckingSet.of(1, 7, 28, 80))).toBeFalsy();
+        expect(numsCheckingSet.doesNotHaveAny(CellIndicesCheckingSet.of(2, 3))).toBeTruthy();
+        expect(numsCheckingSet.doesNotHaveAny(CellIndicesCheckingSet.of(0, 9))).toBeFalsy();
     });
 
-    const expectNumCheckingSetWithValues = (numSet: CellIndicesCheckingSet, values: ReadonlyArray<number>) => {
+    const expectNumsCheckingSetWithValues = (numSet: CellIndicesCheckingSet, values: ReadonlyArray<number>) => {
         const set = new Set(values);
         _.range(0, Grid.CELL_COUNT).forEach(num => {
             expect(numSet.hasAll(CellIndicesCheckingSet.of(num))).toBe(set.has(num));
