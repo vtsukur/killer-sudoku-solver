42678689d724e678ca2f6ae4d345a51c8fb779bb
vtsukur <volodymyrt@wix.com>
2023-02-18 21:36:07 +0200

    [refactoring] Renaming `CachedNumRanges` constants to be more precise and making them `readonly`

--- src/solver/math/cachedNumRanges.ts
+++ src/solver/math/cachedNumRanges.ts
@@ -2,8 +2,7 @@ import * as _ from 'lodash';
 
 export class CachedNumRanges {
 
-    // should be LTE
-    static ZERO_TO_N_LT_81: ReadonlyArray<ReadonlyArray<number>> = (() => {
+    static readonly ZERO_TO_N_LTE_81: ReadonlyArray<ReadonlyArray<number>> = (() => {
         const val = new Array<ReadonlyArray<number>>(81 + 1);
         for (const i of _.range(val.length)) {
             val[i] = _.range(i);
@@ -11,7 +10,7 @@ export class CachedNumRanges {
         return val;
     })();
 
-    static ONE_TO_N_LT_10: ReadonlyArray<ReadonlyArray<number>> = (() => {
+    static readonly ONE_TO_N_LTE_10: ReadonlyArray<ReadonlyArray<number>> = (() => {
         const val = new Array<ReadonlyArray<number>>(10 + 1);
         for (const i of _.range(val.length)) {
             val[i] = _.range(1, i);
