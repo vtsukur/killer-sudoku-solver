984e709d7c4ea96e8ffc51166d5ad975861e7579
vtsukur <volodymyrt@wix.com>
2023-02-20 12:31:31 +0200

    [test] [perf] Adding one more complex case for `GridAreaModel` construction with 20 `Cage`s and 4 without obvious overlap -- all of them making their way into non-overlapping area

--- tests/perf/solver/models/elements/gridAreaModel.perf.test.ts
+++ tests/perf/solver/models/elements/gridAreaModel.perf.test.ts
@@ -36,7 +36,7 @@ describe('Performance tests for `GridAreaModel`', () => {
             });
         });
 
-        test.only(`Segmentation of \`Cage\`s in a big 4-\`House\` area with 5 derived \`Cage\`s and 13 unfilled \`Cell\`s remaining (Sudoku.com 2022-10-22) [${i + 1}]`, () => {
+        test(`Segmentation of \`Cage\`s in a big 4-\`House\` area with 5 derived \`Cage\`s and 13 unfilled \`Cell\`s remaining (Sudoku.com 2022-10-22) [${i + 1}]`, () => {
             const cages = [
                 Cage.ofSum(19).at(0, 2).at(0, 3).at(0, 4).at(1, 2).new(),
                 Cage.ofSum(22).at(2, 2).at(3, 2).at(3, 3).new(),
@@ -74,6 +74,63 @@ describe('Performance tests for `GridAreaModel`', () => {
                     Cage.ofSum(15).at(1, 3).at(1, 4).setIsInput(false).new(),
                     Cage.ofSum(9).at(2, 3).at(2, 4).setIsInput(false).new(),
                     Cage.ofSum(25).at(0, 2).at(0, 3).at(1, 2).at(6, 3).at(8, 3).setIsInput(false).new()
+                ]
+            );
+
+            _.range(ITERATION_COUNT).forEach(() => {
+                GridAreaModel.from(cages, 4);
+            });
+        });
+
+        test.only(`Random expert level challenge: 20 \`Cage\`s, 5 are derived and 4 are executed with incl/excl algo [${i + 1}]`, () => {
+            const cages = [
+                Cage.ofSum(3).at(0, 4).new(),
+                Cage.ofSum(20).at(1, 3).at(1, 4).at(2, 4).new(),
+                Cage.ofSum(7).at(5, 3).at(5, 4).new(),
+                Cage.ofSum(13).at(6, 3).at(7, 3).new(),
+                Cage.ofSum(3).at(6, 4).at(7, 4).new(),
+                Cage.ofSum(1).at(0, 3).new(),
+                Cage.ofSum(5).at(0, 2).new(),
+                Cage.ofSum(14).at(0, 5).at(1, 5).at(2, 5).setIsInput(false).new(),
+                Cage.ofSum(8).at(3, 3).new(),
+                Cage.ofSum(7).at(2, 3).new(),
+                Cage.ofSum(1).at(4, 2).new(),
+                Cage.ofSum(3).at(5, 2).new(),
+                Cage.ofSum(7).at(3, 5).at(5, 5).setIsInput(false).new(),
+                Cage.ofSum(29).at(1, 2).at(2, 2).at(3, 2).at(6, 2).at(7, 2).setIsInput(false).new(),
+                Cage.ofSum(15).at(6, 5).at(7, 5).at(8, 5).setIsInput(false).new(),
+                Cage.ofSum(14).at(8, 3).at(8, 4).new(),
+                Cage.ofSum(7).at(8, 2).new(),
+                Cage.ofSum(14).at(3, 4).at(4, 3).at(4, 4).new(),
+                Cage.ofSum(9).at(4, 5).new(),
+                Cage.ofSum(16).at(1, 3).at(4, 3).at(5, 3).at(8, 3).setIsInput(false).new()
+            ];
+
+            expectGridAreaModel(GridAreaModel.from(cages, 4),
+                [
+                    Cage.ofSum(3).at(0, 4).new(),
+                    Cage.ofSum(20).at(1, 3).at(1, 4).at(2, 4).new(),
+                    Cage.ofSum(7).at(5, 3).at(5, 4).new(),
+                    Cage.ofSum(13).at(6, 3).at(7, 3).new(),
+                    Cage.ofSum(3).at(6, 4).at(7, 4).new(),
+                    Cage.ofSum(1).at(0, 3).new(),
+                    Cage.ofSum(5).at(0, 2).new(),
+                    Cage.ofSum(8).at(3, 3).new(),
+                    Cage.ofSum(7).at(2, 3).new(),
+                    Cage.ofSum(1).at(4, 2).new(),
+                    Cage.ofSum(3).at(5, 2).new(),
+                    Cage.ofSum(14).at(8, 3).at(8, 4).new(),
+                    Cage.ofSum(7).at(8, 2).new(),
+                    Cage.ofSum(14).at(3, 4).at(4, 3).at(4, 4).new(),
+                    Cage.ofSum(9).at(4, 5).new(),
+
+                    Cage.ofSum(14).at(0, 5).at(1, 5).at(2, 5).setIsInput(false).new(),
+                    Cage.ofSum(7).at(3, 5).at(5, 5).setIsInput(false).new(),
+                    Cage.ofSum(29).at(1, 2).at(2, 2).at(3, 2).at(6, 2).at(7, 2).setIsInput(false).new(),
+                    Cage.ofSum(15).at(6, 5).at(7, 5).at(8, 5).setIsInput(false).new(),
+                ],
+                [
+                    Cage.ofSum(16).at(1, 3).at(4, 3).at(5, 3).at(8, 3).setIsInput(false).new()
                 ]
             );
 
