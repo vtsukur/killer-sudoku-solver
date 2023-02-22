f15c40041aa0ed99267057bac2267ef134182200
vtsukur <volodymyrt@wix.com>
2023-02-22 14:57:44 +0200

    [test] Renaming unit tests

--- tests/unit/solver/models/elements/gridAreaModel.test.ts
+++ tests/unit/solver/models/elements/gridAreaModel.test.ts
@@ -3,7 +3,7 @@ import { CellIndicesCheckingSet } from '../../../../../src/solver/math';
 import { GridAreaModel } from '../../../../../src/solver/models/elements/gridAreaModel';
 
 describe('Unit tests for `GridAreaModel`', () => {
-    test('Segmentation of `House` `Cage`s with 2 derived `Cage`s', () => {
+    test('Creation of instance with all input `Cage`s forming non-overlapping area and all derived `Cage`s forming overlapping area', () => {
         const cages = [
             Cage.ofSum(7).at(0, 0).at(0, 1).new(),
             Cage.ofSum(18).at(1, 0).at(1, 1).at(2, 0).new(),
@@ -25,7 +25,7 @@ describe('Unit tests for `GridAreaModel`', () => {
         );
     });
 
-    test('Segmentation of `House` `Cage`s with 2 derived `Cage`s (`Nonet` 8 from Sudoku.com 2022-10-19)', () => {
+    test('Sudoku.com 2022-10-19: Creation of instance with 2 derived `Cage`s maximizing non-overlapping area', () => {
         const cages = [
             Cage.ofSum(9).at(7, 8).at(8, 8).new(),
             Cage.ofSum(11).at(8, 7).at(8, 8).setIsInput(false).new(),
@@ -49,7 +49,7 @@ describe('Unit tests for `GridAreaModel`', () => {
         );
     });
 
-    test('Segmentation of `Cage`s in a big 4-`House` area with 2 derived `Cage`s and 15 unfilled `Cell`s remaining', () => {
+    test('Creation of instance for the big area with all input `Cage`s forming non-overlapping area and all derived `Cage`s forming overlapping area', () => {
         const cages = [
             Cage.ofSum(12).at(2, 3).at(3, 2).at(3, 3).new(),
             Cage.ofSum(14).at(2, 4).at(2, 5).at(2, 6).at(3, 4).new(),
@@ -81,7 +81,7 @@ describe('Unit tests for `GridAreaModel`', () => {
         );
     });
 
-    test('Segmentation of `Cage`s in a big 4-`House` area with 8 derived `Cage`s and 1 unfilled `Cell` remaining (Sudoku.com random challenge)', () => {
+    test('Sudoku.com random challenge: creation of instance for the big area with many `Cage`s with all input `Cage`s forming non-overlapping area and all derived `Cage`s forming overlapping area', () => {
         const cages = [
             Cage.ofSum(6).at(0, 5).at(0, 6).new(),
             Cage.ofSum(18).at(1, 5).at(1, 6).at(2, 5).at(2, 6).new(),
@@ -139,49 +139,7 @@ describe('Unit tests for `GridAreaModel`', () => {
         );
     });
 
-    test('Segmentation of `Cage`s in a big 4-`House` area with 5 derived `Cage`s and 13 unfilled `Cell` remaining (Sudoku.com 2022-10-22)', () => {
-        const cages = [
-            Cage.ofSum(19).at(0, 2).at(0, 3).at(0, 4).at(1, 2).new(),
-            Cage.ofSum(22).at(2, 2).at(3, 2).at(3, 3).new(),
-            Cage.ofSum(10).at(1, 3).at(2, 3).new(),
-            Cage.ofSum(14).at(1, 4).at(2, 4).new(),
-            Cage.ofSum(6).at(4, 3).at(5, 3).new(),
-            Cage.ofSum(8).at(3, 4).at(3, 5).new(),
-            Cage.ofSum(9).at(4, 4).at(4, 5).new(),
-            Cage.ofSum(17).at(5, 4).at(6, 3).at(6, 4).new(),
-            Cage.ofSum(6).at(5, 5).new(),
-            Cage.ofSum(10).at(8, 3).at(8, 4).new(),
-            Cage.ofSum(23).at(0, 3).at(0, 4).at(3, 2).at(3, 3).setIsInput(false).new(),
-            Cage.ofSum(23).at(2, 2).at(3, 2).at(6, 3).at(6, 4).setIsInput(false).new(),
-            Cage.ofSum(15).at(1, 3).at(1, 4).setIsInput(false).new(),
-            Cage.ofSum(9).at(2, 3).at(2, 4).setIsInput(false).new(),
-            Cage.ofSum(25).at(0, 2).at(0, 3).at(1, 2).at(6, 3).at(8, 3).setIsInput(false).new()
-        ];
-
-        expectGridAreaModel(GridAreaModel.from(cages, 4),
-            [
-                Cage.ofSum(19).at(0, 2).at(0, 3).at(0, 4).at(1, 2).new(),
-                Cage.ofSum(22).at(2, 2).at(3, 2).at(3, 3).new(),
-                Cage.ofSum(10).at(1, 3).at(2, 3).new(),
-                Cage.ofSum(14).at(1, 4).at(2, 4).new(),
-                Cage.ofSum(6).at(4, 3).at(5, 3).new(),
-                Cage.ofSum(8).at(3, 4).at(3, 5).new(),
-                Cage.ofSum(9).at(4, 4).at(4, 5).new(),
-                Cage.ofSum(17).at(5, 4).at(6, 3).at(6, 4).new(),
-                Cage.ofSum(6).at(5, 5).new(),
-                Cage.ofSum(10).at(8, 3).at(8, 4).new()
-            ],
-            [
-                Cage.ofSum(23).at(0, 3).at(0, 4).at(3, 2).at(3, 3).setIsInput(false).new(),
-                Cage.ofSum(23).at(2, 2).at(3, 2).at(6, 3).at(6, 4).setIsInput(false).new(),
-                Cage.ofSum(15).at(1, 3).at(1, 4).setIsInput(false).new(),
-                Cage.ofSum(9).at(2, 3).at(2, 4).setIsInput(false).new(),
-                Cage.ofSum(25).at(0, 2).at(0, 3).at(1, 2).at(6, 3).at(8, 3).setIsInput(false).new()
-            ]
-        );
-    });
-
-    test('Segmentation of no `Cage`s', () => {
+    test('Creation of empty instance', () => {
         expectGridAreaModel(GridAreaModel.from([]), [], []);
     });
 });
