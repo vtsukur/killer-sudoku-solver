ae08e67284de04311326e612e3e55417082002a9
vtsukur <volodymyrt@wix.com>
2023-02-23 16:20:43 +0200

    [test] Full unit test for 'Applying strategy for single `Row`s to Daily Challenge (2022-10-22) by Sudoku.com'

--- tests/unit/solver/strategies/tactics/findAndSliceComplementsForGridAreasStrategy.test.ts
+++ tests/unit/solver/strategies/tactics/findAndSliceComplementsForGridAreasStrategy.test.ts
@@ -1,3 +1,4 @@
+import { Cage } from '../../../../../src/puzzle/cage';
 import { House } from '../../../../../src/puzzle/house';
 import { Puzzle } from '../../../../../src/puzzle/puzzle';
 import { MasterModel } from '../../../../../src/solver/models/masterModel';
@@ -26,6 +27,24 @@ describe('Unit tests for `FindAndSliceComplementsForGridAreasStrategy`', () => {
 
         // then
         expect(context.model.cageModelsMap.size).toBe(originalCageCount + House.COUNT_OF_ONE_TYPE_PER_GRID);
+        // (`Row` 0)
+        expect(context.model.cageModelsMap.has(Cage.ofSum(33).at(0, 2).at(0, 3).at(0, 4).at(0, 5).at(0, 6).at(0, 7).at(0, 8).new().key));
+        // (`Row` 1)
+        expect(context.model.cageModelsMap.has(Cage.ofSum(35).at(1, 2).at(1, 3).at(1, 4).at(1, 5).at(1, 6).at(1, 7).at(1, 8).new().key));
+        // (`Row` 2)
+        expect(context.model.cageModelsMap.has(Cage.ofSum(31).at(2, 2).at(2, 3).at(2, 4).at(2, 7).at(2, 8).new().key));
+        // (`Row` 3)
+        expect(context.model.cageModelsMap.has(Cage.ofSum(35).at(3, 2).at(3, 3).at(3, 4).at(3, 5).at(3, 6).at(3, 7).at(3, 8).new().key));
+        // (`Row` 4)
+        expect(context.model.cageModelsMap.has(Cage.ofSum(27).at(4, 0).at(4, 3).at(4, 6).at(4, 7).at(4, 8).new().key));
+        // (`Row` 5)
+        expect(context.model.cageModelsMap.has(Cage.ofSum(39).at(5, 0).at(5, 1).at(5, 2).at(5, 3).at(5, 4).at(5, 6).at(5, 7).at(5, 8).new().key));
+        // (`Row` 6)
+        expect(context.model.cageModelsMap.has(Cage.ofSum(45).at(6, 0).at(6, 1).at(6, 2).at(6, 3).at(6, 4).at(6, 5).at(6, 6).at(6, 7).at(6, 8).new().key));
+        // (`Row` 7)
+        expect(context.model.cageModelsMap.has(Cage.ofSum(45).at(7, 0).at(7, 4).at(7, 5).at(7, 6).at(7, 7).at(7, 8).new().key));
+        // (`Row` 8)
+        expect(context.model.cageModelsMap.has(Cage.ofSum(18).at(8, 0).at(8, 5).at(8, 8).new().key));
     });
 
     const newContext = (puzzle: Puzzle) => {
