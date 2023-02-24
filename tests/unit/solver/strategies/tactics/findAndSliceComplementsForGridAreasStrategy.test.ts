efa0478ea48b24c7ac3dfde7388df629625651f8
vtsukur <volodymyrt@wix.com>
2023-02-24 11:54:21 +0200

    [refactor] [test] Simplifying test preparation by removing unnecessary strategy execution and exposing `Context` creation API for the future use in performance tests

--- tests/unit/solver/strategies/tactics/findAndSliceComplementsForGridAreasStrategy.test.ts
+++ tests/unit/solver/strategies/tactics/findAndSliceComplementsForGridAreasStrategy.test.ts
@@ -3,9 +3,7 @@ import { House } from '../../../../../src/puzzle/house';
 import { Puzzle } from '../../../../../src/puzzle/puzzle';
 import { MasterModel } from '../../../../../src/solver/models/masterModel';
 import { Context } from '../../../../../src/solver/strategies/context';
-import { Strategy } from '../../../../../src/solver/strategies/strategy';
 import { FindAndSliceComplementsForGridAreasStrategy } from '../../../../../src/solver/strategies/tactics/findAndSliceComplementsForGridAreasStrategy';
-import { FindRedundantNonetSumsStrategy } from '../../../../../src/solver/strategies/tactics/findRedundantNonetSumsStrategy';
 import { CageSlicer } from '../../../../../src/solver/transform/cageSlicer';
 import { puzzleSamples } from '../../../puzzle/puzzleSamples';
 
@@ -17,7 +15,6 @@ describe('Unit tests for `FindAndSliceComplementsForGridAreasStrategy`', () => {
     // Given:
     beforeEach(() => {
         context = newContext(puzzleSamples.sudokuDotCom.dailyChallengeOf_2022_10_22);
-        runSetUpStrategies(new FindRedundantNonetSumsStrategy(context));
         originalCageCount = context.model.cageModelsMap.size;
     });
 
@@ -188,15 +185,10 @@ describe('Unit tests for `FindAndSliceComplementsForGridAreasStrategy`', () => {
         expect(context.model.hasCage(Cage.ofSum(3).at(6, 6).at(7, 6).new())).toBeTruthy();
     });
 
-    const newContext = (puzzle: Puzzle) => {
-        const masterModel = new MasterModel(puzzle);
-        const cageSlider = new CageSlicer(masterModel);
-        return new Context(masterModel, cageSlider);
-    };
-
-    const runSetUpStrategies = (...strategies: ReadonlyArray<Strategy>) => {
-        for (const strategy of strategies) {
-            strategy.execute();
-        }
-    };
 });
+
+export const newContext = (puzzle: Puzzle) => {
+    const masterModel = new MasterModel(puzzle);
+    const cageSlider = new CageSlicer(masterModel);
+    return new Context(masterModel, cageSlider);
+};
