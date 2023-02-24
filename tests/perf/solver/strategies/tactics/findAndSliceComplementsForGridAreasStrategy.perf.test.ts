c03a07a4422162e9b897d56242121077e14420d1
vtsukur <volodymyrt@wix.com>
2023-02-24 11:54:43 +0200

    [perf] [test] Adding performance test for `FindAndSliceComplementsForGridAreasStrategy`

---
+++ tests/perf/solver/strategies/tactics/findAndSliceComplementsForGridAreasStrategy.perf.test.ts
@@ -0,0 +1,20 @@
+import * as _ from 'lodash';
+import { FindAndSliceComplementsForGridAreasStrategy } from '../../../../../src/solver/strategies/tactics/findAndSliceComplementsForGridAreasStrategy';
+import { puzzleSamples } from '../../../../unit/puzzle/puzzleSamples';
+import { newContext } from '../../../../unit/solver/strategies/tactics/findAndSliceComplementsForGridAreasStrategy.test';
+
+describe('Performance tests for `FindAndSliceComplementsForGridAreasStrategy`', () => {
+
+    const TESTS_COUNT = 10;
+    const ITERATION_COUNT = 500;
+
+    _.range(TESTS_COUNT).forEach(i => {
+        test(`Applying strategy within Daily Challenge (2022-10-22) by Sudoku.com [${i}]`, () => {
+            _.range(ITERATION_COUNT).forEach(() => {
+                const context = newContext(puzzleSamples.sudokuDotCom.dailyChallengeOf_2022_10_22);
+                new FindAndSliceComplementsForGridAreasStrategy(context).execute();
+            });
+        });
+    });
+
+});
