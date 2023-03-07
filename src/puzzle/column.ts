d757772b5e8e5b3a3edb12914c3a169ff2255607
vtsukur <volodymyrt@wix.com>
2023-03-07 14:19:49 +0200

    [refactoring] Removing `Column.newCellsIterator`

--- src/puzzle/column.ts
+++ src/puzzle/column.ts
@@ -1,6 +1,5 @@
 import { Cell, ReadonlyCells } from './cell';
 import { Grid } from './grid';
-import { House, HouseIndex } from './house';
 
 /**
  * Supportive class for Killer Sudoku `Column`
@@ -30,28 +29,5 @@ export class Column {
         }
         return val;
     })();
-
-    /**
-     * Constructs new iterator over {@link Cell}s for a `Column` with the given index
-     * which iterates `Cell`s consequently from _top_ to _bottom_.
-     *
-     * Sample iteration for `Column` of index 3 looks as follows:
-     * ```
-     * // (row, column)
-     * (0, 3) -> (1, 3) -> (2, 3) -> ... -> (7, 3) -> (8, 3) -> done
-     * ```
-     *
-     * @param col - Index of the `Column` to iterate `Cell`s for.
-     *
-     * @returns new iterator over {@link Cell}s for a `Column` with the given index.
-     *
-     * @throws {InvalidProblemDefError} if `Column` index is outside of valid [0, 9) range.
-     */
-    static newCellsIterator(col: HouseIndex) {
-        House.validateIndex(col);
-        return House.newCellsIterator((row: HouseIndex) => {
-            return Cell.at(row, col);
-        });
-    }
 
 }
