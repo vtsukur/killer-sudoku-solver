107e0b50cc6d1bff9a80a3f1a37538e6a1e0b5ba
vtsukur <volodymyrt@wix.com>
2023-03-07 12:11:28 +0200

    [refactoring] [test] Adjusting test names

--- tests/unit/puzzle/grid.test.ts
+++ tests/unit/puzzle/grid.test.ts
@@ -3,23 +3,23 @@ import { Grid } from '../../../src/puzzle/grid';
 
 describe('Unit tests for `Grid`', () => {
 
-    test('Amount of Cells on Grid\'s side is 9', () => {
+    test('Amount of `Cell`s on the Grid\'s side is `9`', () => {
         expect(Grid.SIDE_CELL_COUNT).toEqual(9);
     });
 
-    test('Range for `Grid`\'s side indices is [0, 8]', () => {
+    test('Range of `Grid`\'s side indices is `[0, 8]`', () => {
         expect(Grid.SIDE_INDICES_RANGE).toEqual([ 0, 1, 2, 3, 4, 5, 6, 7, 8 ]);
     });
 
-    test('Grid has 81 Cells', () => {
+    test('Grid has `81` Cells', () => {
         expect(Grid.CELL_COUNT).toEqual(81);
     });
 
-    test('Range for `Grid`\'s `Cell` indices is [0, 80]', () => {
+    test('Range of all `Grid`\'s `Cell` indices is `[0, 80]`', () => {
         expect(Grid.CELL_INDICES_RANGE).toEqual(_.range(Grid.CELL_COUNT));
     });
 
-    test('Sum of all `Cell`s in the `Grid` add up to 405', () => {
+    test('Sum of all `Cell`s in the `Grid` add up to `405`', () => {
         expect(Grid.SUM).toEqual(405);
     });
 
