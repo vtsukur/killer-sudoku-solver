10f4ff151ee00b39dd15442d230bbd2dde099a65
vtsukur <volodymyrt@wix.com>
2023-03-07 12:03:33 +0200

    [doc] Fixing TSDoc

--- src/puzzle/grid.ts
+++ src/puzzle/grid.ts
@@ -4,8 +4,8 @@ import { House } from './house';
 /**
  * Supportive class for Killer Sudoku `Grid`
  * which holds useful constants that describe mathematical properties of any `Grid`
- * as well as utility methods that simplify iteration over `Grid` {@link Cell}s,
- * {@link CellRowAndColumn}s and creation of supplementary ranges and matrices with `Grid`'s size.
+ * as well as utility methods that simplify iteration over `Grid` {@link Cell}s
+ * and creation of matrices with `Grid`'s size.
  *
  * @public
  */
