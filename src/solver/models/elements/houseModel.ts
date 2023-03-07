75962238171131c12e7d6e7fd40ff5908d41d0ee
vtsukur <volodymyrt@wix.com>
2023-03-07 15:23:20 +0200

    [refactoring] Removing unused code

--- src/solver/models/elements/houseModel.ts
+++ src/solver/models/elements/houseModel.ts
@@ -1,4 +1,3 @@
-import { Cage } from '../../../puzzle/cage';
 import { ReadonlyCells } from '../../../puzzle/cell';
 import { CageModel } from './cageModel';
 
@@ -8,7 +7,6 @@ export class HouseModel {
     readonly cells: ReadonlyCells;
 
     private readonly _cageMs: Array<CageModel> = [];
-    private readonly _cages: Array<Cage> = [];
 
     constructor(index: number, cells: ReadonlyCells) {
         this.index = index;
@@ -19,20 +17,14 @@ export class HouseModel {
         return this._cageMs;
     }
 
-    get cages() {
-        return this._cages;
-    }
-
     addCageModel(val: CageModel) {
         this._cageMs.push(val);
-        this._cages.push(val.cage);
     }
 
     removeCageModel(val: CageModel) {
         const indexToRemove = this._cageMs.indexOf(val);
         if (indexToRemove !== -1) {
             this._cageMs.splice(indexToRemove, 1);
-            this._cages.splice(indexToRemove, 1);
         }
     }
 
