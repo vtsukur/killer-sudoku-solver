8c8da8f963ad7eff6bc28b699cefd8de2b8cb6d4
vtsukur <volodymyrt@wix.com>
2023-03-07 14:19:00 +0200

    [refactoring] Removing `Row.newCellsIterator`

--- src/puzzle/row.ts
+++ src/puzzle/row.ts
@@ -1,5 +1,4 @@
 import { Cell, ReadonlyCells } from './cell';
-import { House, HouseIndex } from './house';
 
 /**
  * Supportive class for Killer Sudoku `Row`
@@ -21,28 +20,5 @@ export class Row {
      * represented as a readonly array of {@link ReadonlyCells} indexed by {@link Row}.
      */
     static readonly CELLS: ReadonlyArray<ReadonlyCells> = Cell.GRID;
-
-    /**
-     * Constructs new iterator over {@link Cell}s for a `Row` with the given index
-     * which iterates `Cell`s consequently from _left_ to _right_.
-     *
-     * Sample iteration for `Row` of index 3 looks as follows:
-     * ```
-     * // (row, column)
-     * (3, 0) -> (3, 1) -> (3, 2) -> ... -> (3, 7) -> (3, 8) -> done
-     * ```
-     *
-     * @param row - Index of the `Row` to iterate `Cell`s for.
-     *
-     * @returns new iterator over {@link Cell}s for a `Row` with the given index.
-     *
-     * @throws {InvalidProblemDefError} if `Row` index is outside of valid [0, 9) range.
-     */
-    static newCellsIterator(row: HouseIndex) {
-        House.validateIndex(row);
-        return House.newCellsIterator((col: HouseIndex) => {
-            return Cell.at(row, col);
-        });
-    }
 
 }
