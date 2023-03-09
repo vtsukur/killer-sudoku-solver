e8d4a89ca49d2ba45d2b97fba6a11b093f6c5a9f
vtsukur <volodymyrt@wix.com>
2023-03-09 11:24:12 +0200

    [perf] Adding performance test for `FindProtrusiveCagesStrategy`

---
+++ tests/perf/solver/strategies/tactics/findProtrusiveCagesStrategy.perf.test.ts
@@ -0,0 +1,20 @@
+import * as _ from 'lodash';
+import { FindProtrusiveCagesStrategy } from '../../../../../src/solver/strategies/tactics/findProtrusiveCagesStrategy';
+import { puzzleSamples } from '../../../../unit/puzzle/puzzleSamples';
+import { newContext } from '../../../../unit/solver/strategies/tactics/contextBuilder';
+
+describe('Performance tests for `FindProtrusiveCagesStrategy`', () => {
+
+    const TESTS_COUNT = 10;
+    const ITERATION_COUNT = 2000;
+
+    _.range(TESTS_COUNT).forEach(i => {
+        test(`Applying strategy onto \`Nonet\`s within Daily Challenge (2022-11-01) by Sudoku.com [${i}]`, () => {
+            _.range(ITERATION_COUNT).forEach(() => {
+                const context = newContext(puzzleSamples.sudokuDotCom.dailyChallengeOf_2022_10_22);
+                new FindProtrusiveCagesStrategy(context).execute();
+            });
+        });
+    });
+
+});
