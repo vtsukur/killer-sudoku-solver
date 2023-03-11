2edbdb9dc3cd9b0f1c13d3c475da08b1f1b49bb6
vtsukur <volodymyrt@wix.com>
2023-03-11 12:49:08 +0200

    [doc] Adding TSDoc

--- src/solver/strategies/tactics/init/masterInitStrategy.ts
+++ src/solver/strategies/tactics/init/masterInitStrategy.ts
@@ -1,8 +1,25 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Puzzle } from '../../../../puzzle/puzzle';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Context } from '../../context';
 import { Strategy } from '../../strategy';
 import { FindCombosForHouseCagesStrategy } from './findCombosForHouseCagesStrategy';
 import { FindComplementingCagesStrategy } from './findComplementingCagesStrategy';
 import { FindProtrusiveCagesStrategy } from './findProtrusiveCagesStrategy';
 
+/**
+ * {@link Strategy} for solving the Killer Sudoku {@link Puzzle}
+ * which executes a group of several {@link Strategy}-ies
+ * so it is applied just once on the particular {@link Puzzle}.
+ *
+ * No {@link Strategy}-ies are executed if {@link Context.skipInit} is set to `true`.
+ *
+ * @public
+ *
+ * @see FindProtrusiveCagesStrategy
+ * @see FindComplementingCagesStrategy
+ * @see FindCombosForHouseCagesStrategy
+ */
 export class MasterInitStrategy extends Strategy {
 
     /**
