8cdc80726f0ec6f9a368b4c9c65657852648dd61
vtsukur <volodymyrt@wix.com>
2023-03-13 21:33:51 +0200

    [perf] Tweaking performance test for `ReducePermsForCagesStrategy`

--- tests/perf/solver/strategies/tactics/loop/reducePermsForCagesStrategy.perf.test.ts
+++ tests/perf/solver/strategies/tactics/loop/reducePermsForCagesStrategy.perf.test.ts
@@ -6,11 +6,11 @@ import { newContext } from '../../../../../unit/solver/strategies/tactics/init/b
 describe('Performance tests for `ReducePermsForCagesStrategy`', () => {
 
     const TESTS_COUNT = 10;
-    const ITERATIONS = _.range(500);
+    const ITERATIONS = _.range(10);
 
     _.range(TESTS_COUNT).forEach(i => {
 
-        test(`Applying \`Strategy\` onto \`Nonet\`s within Daily Challenge (2022-11-01) by Sudoku.com [${i}]`, () => {
+        test(`Applying \`Strategy\` for Daily Challenge (2022-11-01) by Sudoku.com [${i}]`, () => {
             ITERATIONS.forEach(() => {
                 const context = newContext(puzzleSamples.sudokuDotCom.dailyChallengeOf_2022_10_22);
                 context.setCageModelsToReduceToAll();
