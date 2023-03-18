546dcebaafc70eb50d13ab5f26ae4404d810c074
vtsukur <volodymyrt@wix.com>
2023-03-18 14:01:56 +0200

    [refactor] Renaming `SudokuNumsCheckingSet` module into `SudokuNumsSet`

--- src/solver/sets/index.ts
+++ src/solver/sets/index.ts
@@ -1,6 +1,6 @@
 import { CellIndicesCheckingSet, ReadonlyCellIndicesCheckingSet } from './cellIndicesCheckingSet';
 import { BitStore32, ReadonlyNumsSet } from './numsSet';
-import { ReadonlySudokuNumsCheckingSet, SudokuNumsCheckingSet } from './sudokuNumsCheckingSet';
+import { ReadonlySudokuNumsCheckingSet, SudokuNumsCheckingSet } from './sudokuNumsSet';
 
 export {
     BitStore32,
