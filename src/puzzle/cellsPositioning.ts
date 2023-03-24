a08487b79cb7799eb4943a8199a0cb177387aad0
vtsukur <volodymyrt@wix.com>
2023-03-24 17:46:00 +0200

    [comment] Adding implementation comments

--- src/puzzle/cellsPositioning.ts
+++ src/puzzle/cellsPositioning.ts
@@ -12,24 +12,46 @@ export class CellsPositioning {
         this.isSingleCell = cells.length === 1;
         if (this.isSingleCell) {
             //
-            // [PERFORMANCE] By definition, a single `Cell` resides within a single `Row`, `Column`, and `Nonet`,
+            // [PERFORMANCE] By definition, a single `Cell` resides
+            // within a single `Row`, `Column`, and `Nonet`,
             // initializing all respective flags with a `true` value.
             //
             this.isWithinRow = this.isWithinColumn = this.isWithinNonet = this.isWithinHouse = true;
         } else {
+            //
+            // [PERFORMANCE] The following implementation of determining flags
+            // does *not* leverage built-in ECMAScript collections and methods
+            // since `do ... while` based iteration with boolean operators
+            // and no extra method calls are several times faster,
+            // even for a small number of `Cell`s.
+            //
+
             const firstCell = cells[0];
 
+            // Saving reference values of `House` indices to be used in the comparisons.
             const refRow = firstCell.row;
             const refCol = firstCell.col;
             const refNonet = firstCell.nonet;
 
+            // Assuming that all flags are `true` by default.
             let isWithinRow = true;
             let isWithinColumn = true;
             let isWithinNonet = true;
 
+            // Iterating over each `Cell` starting with the second one.
+            // (Logic already processed first `Cell` by capturing reference indices.)
             let i = 1;
             do {
                 const cell = cells[i];
+
+                //
+                // If the `House` of the current `Cell` is *not* the same as the reference `House`,
+                // it is clear that at least two `Cell`s are in different `House`s,
+                // so the value of the respective flag should be `false`.
+                // (And subsequent iterations will short-circuit for performance.)
+                // Otherwise, `isWithin${houseType}` is kept with the default value of `true`.
+                //
+
                 if (isWithinRow && refRow !== cell.row) {
                     isWithinRow = false;
                 }
@@ -41,10 +63,10 @@ export class CellsPositioning {
                 }
             } while (++i < cells.length);
 
+            // Setting fields.
             this.isWithinRow = isWithinRow;
             this.isWithinColumn = isWithinColumn;
             this.isWithinNonet = isWithinNonet;
-
             this.isWithinHouse = this.isWithinRow || this.isWithinColumn || this.isWithinNonet;
         }
     }
