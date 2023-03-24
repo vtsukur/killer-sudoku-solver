1284eb6d258b5e8a23976a7337bed980d4c97071
vtsukur <volodymyrt@wix.com>
2023-03-24 16:06:51 +0200

    [refactoring] Removing `CellsPositioning.cells` property as it is not used outside of the class

--- src/puzzle/cellsPositioning.ts
+++ src/puzzle/cellsPositioning.ts
@@ -2,7 +2,6 @@ import { Cell, ReadonlyCells } from './cell';
 
 export class CellsPositioning {
 
-    readonly cells;
     readonly isSingleCellCage;
     readonly isWithinRow;
     readonly isWithinColumn;
@@ -10,16 +9,15 @@ export class CellsPositioning {
     readonly isWithinHouse;
 
     constructor(cells: ReadonlyCells) {
-        this.cells = cells;
         this.isSingleCellCage = cells.length === 1;
-        this.isWithinRow = this.isSingleCellCage || this.isSameForAll((cell: Cell) => cell.row);
-        this.isWithinColumn = this.isSingleCellCage || this.isSameForAll((cell: Cell) => cell.col);
-        this.isWithinNonet = this.isSingleCellCage || this.isSameForAll((cell: Cell) => cell.nonet);
+        this.isWithinRow = this.isSingleCellCage || CellsPositioning.isSameForAll(cells, (cell: Cell) => cell.row);
+        this.isWithinColumn = this.isSingleCellCage || CellsPositioning.isSameForAll(cells, (cell: Cell) => cell.col);
+        this.isWithinNonet = this.isSingleCellCage || CellsPositioning.isSameForAll(cells, (cell: Cell) => cell.nonet);
         this.isWithinHouse = this.isWithinRow || this.isWithinColumn || this.isWithinNonet;
     }
 
-    private isSameForAll(whatFn: (cell: Cell) => number) {
-        return new Set(this.cells.map(whatFn)).size === 1;
+    private static isSameForAll(cells: ReadonlyCells, whatFn: (cell: Cell) => number) {
+        return new Set(cells.map(whatFn)).size === 1;
     }
 
 }
