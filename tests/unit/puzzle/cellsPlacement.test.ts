af90dde67dde8954c96052da645c8a156a31418d
vtsukur <volodymyrt@wix.com>
2023-03-24 21:25:46 +0200

    [test] [refactoring] Simplifying tests

--- tests/unit/puzzle/cellsPlacement.test.ts
+++ tests/unit/puzzle/cellsPlacement.test.ts
@@ -4,9 +4,7 @@ import { CellsPlacement } from '../../../src/puzzle/cellsPlacement';
 describe('Unit tests for `CellsPlacement`', () => {
 
     test('Construction of `CellsPlacement` with a single `Cell`', () => {
-        const positioning = new CellsPlacement([ Cell.at(3, 4) ]);
-
-        expect(positioning).toEqual({
+        expect(new CellsPlacement([ Cell.at(3, 4) ])).toEqual({
             isSingleCell: true,
             isWithinRow: true,
             isWithinColumn: true,
@@ -16,11 +14,9 @@ describe('Unit tests for `CellsPlacement`', () => {
     });
 
     test('Construction of `CellsPlacement` with the `Cell`s from a `Cage` placed within `Row`', () => {
-        const positioning = new CellsPlacement([
+        expect(new CellsPlacement([
             Cell.at(3, 5), Cell.at(3, 6)
-        ]);
-
-        expect(positioning).toEqual({
+        ])).toEqual({
             isSingleCell: false,
             isWithinRow: true,
             isWithinColumn: false,
@@ -30,12 +26,10 @@ describe('Unit tests for `CellsPlacement`', () => {
     });
 
     test('Construction of `CellsPlacement` with the `Cell`s from a `Cage` placed within `Column`', () => {
-        const positioning = new CellsPlacement([
+        expect(new CellsPlacement([
             Cell.at(5, 3),
             Cell.at(6, 3)
-        ]);
-
-        expect(positioning).toEqual({
+        ])).toEqual({
             isSingleCell: false,
             isWithinRow: false,
             isWithinColumn: true,
@@ -45,12 +39,10 @@ describe('Unit tests for `CellsPlacement`', () => {
     });
 
     test('Construction of `CellsPlacement` with the `Cell`s from a `Cage` placed within `Nonet`', () => {
-        const positioning = new CellsPlacement([
+        expect(new CellsPlacement([
             Cell.at(0, 0), Cell.at(0, 1),
             Cell.at(1, 0), Cell.at(1, 1)
-        ]);
-
-        expect(positioning).toEqual({
+        ])).toEqual({
             isSingleCell: false,
             isWithinRow: false,
             isWithinColumn: false,
@@ -60,11 +52,9 @@ describe('Unit tests for `CellsPlacement`', () => {
     });
 
     test('Construction of `CellsPlacement` with the `Cell`s from a `Cage` placed within `Row` and `Nonet`', () => {
-        const positioning = new CellsPlacement([
+        expect(new CellsPlacement([
             Cell.at(0, 0), Cell.at(0, 1)
-        ]);
-
-        expect(positioning).toEqual({
+        ])).toEqual({
             isSingleCell: false,
             isWithinRow: true,
             isWithinColumn: false,
@@ -74,12 +64,10 @@ describe('Unit tests for `CellsPlacement`', () => {
     });
 
     test('Construction of `CellsPlacement` with the `Cell`s from a `Cage` placed within `Column` and `Nonet`', () => {
-        const positioning = new CellsPlacement([
+        expect(new CellsPlacement([
             Cell.at(0, 0),
             Cell.at(1, 0)
-        ]);
-
-        expect(positioning).toEqual({
+        ])).toEqual({
             isSingleCell: false,
             isWithinRow: false,
             isWithinColumn: true,
@@ -89,12 +77,10 @@ describe('Unit tests for `CellsPlacement`', () => {
     });
 
     test('Construction of `CellsPlacement` with the `Cell`s outside of a single `House`', () => {
-        const positioning = new CellsPlacement([
+        expect(new CellsPlacement([
             Cell.at(2, 2), Cell.at(2, 3),
             Cell.at(3, 2), Cell.at(3, 3)
-        ]);
-
-        expect(positioning).toEqual({
+        ])).toEqual({
             isSingleCell: false,
             isWithinRow: false,
             isWithinColumn: false,
