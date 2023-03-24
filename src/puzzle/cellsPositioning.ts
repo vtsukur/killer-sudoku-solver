999661cb0eeec4a3f1b2835e2aee300f13bf3c72
vtsukur <volodymyrt@wix.com>
2023-03-24 16:13:03 +0200

    [perf] Improving performance of `CellsPositioning` construction by optimizing conditional logic (5% faster in the test)

--- src/puzzle/cellsPositioning.ts
+++ src/puzzle/cellsPositioning.ts
@@ -2,18 +2,22 @@ import { Cell, ReadonlyCells } from './cell';
 
 export class CellsPositioning {
 
-    readonly isSingleCell;
-    readonly isWithinRow;
-    readonly isWithinColumn;
-    readonly isWithinNonet;
-    readonly isWithinHouse;
+    readonly isSingleCell: boolean;
+    readonly isWithinRow: boolean;
+    readonly isWithinColumn: boolean;
+    readonly isWithinNonet: boolean;
+    readonly isWithinHouse: boolean;
 
     constructor(cells: ReadonlyCells) {
         this.isSingleCell = cells.length === 1;
-        this.isWithinRow = this.isSingleCell || CellsPositioning.isSameForAll(cells, (cell: Cell) => cell.row);
-        this.isWithinColumn = this.isSingleCell || CellsPositioning.isSameForAll(cells, (cell: Cell) => cell.col);
-        this.isWithinNonet = this.isSingleCell || CellsPositioning.isSameForAll(cells, (cell: Cell) => cell.nonet);
-        this.isWithinHouse = this.isWithinRow || this.isWithinColumn || this.isWithinNonet;
+        if (this.isSingleCell) {
+            this.isWithinRow = this.isWithinColumn = this.isWithinNonet = this.isWithinHouse = true;
+        } else {
+            this.isWithinRow = CellsPositioning.isSameForAll(cells, (cell: Cell) => cell.row);
+            this.isWithinColumn = CellsPositioning.isSameForAll(cells, (cell: Cell) => cell.col);
+            this.isWithinNonet = CellsPositioning.isSameForAll(cells, (cell: Cell) => cell.nonet);
+            this.isWithinHouse = this.isWithinRow || this.isWithinColumn || this.isWithinNonet;
+        }
     }
 
     private static isSameForAll(cells: ReadonlyCells, whatFn: (cell: Cell) => number) {
