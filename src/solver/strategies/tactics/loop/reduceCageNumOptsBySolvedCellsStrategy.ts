ab06ab603c26eaf2e53b017aa591076052d8f0a7
vtsukur <volodymyrt@wix.com>
2023-03-26 21:59:41 +0300

    [refactoring] [perf] Avoid creation of `NumsReduction` in favor of already present `Context.reduction`

--- src/solver/strategies/tactics/loop/reduceCageNumOptsBySolvedCellsStrategy.ts
+++ src/solver/strategies/tactics/loop/reduceCageNumOptsBySolvedCellsStrategy.ts
@@ -1,11 +1,9 @@
 import * as _ from 'lodash';
-import { NumsReduction } from '../../numsReduction';
 import { Strategy } from '../../strategy';
 
 export class ReduceCageNumOptsBySolvedCellsStrategy extends Strategy {
 
     execute() {
-        const reduction = new NumsReduction();
         this._context.recentlySolvedCellModels.forEach(solvedCellM => {
             const num = solvedCellM.placedNum as number;
             for (const cageM of solvedCellM.withinCageModels) {
@@ -13,12 +11,11 @@ export class ReduceCageNumOptsBySolvedCellsStrategy extends Strategy {
                     if (!_.isUndefined(cellM.placedNum)) continue;
 
                     if (cellM.hasNumOpt(num)) {
-                        reduction.deleteNumOpt(cellM, num);
+                        this._context.reduction.deleteNumOpt(cellM, num);
                     }
                 }
             }
         });
-        this._context.setReduction(reduction);
     }
 
 }
