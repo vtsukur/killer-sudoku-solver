bb8b19cc809795e1f65e752a1bb2ccc886057b06
vtsukur <volodymyrt@wix.com>
2023-03-27 16:59:22 +0300

    [feature] Adding tracking of deleted number options in `NumsReduction` internals

--- src/solver/strategies/numsReduction.ts
+++ src/solver/strategies/numsReduction.ts
@@ -1,21 +1,25 @@
+import { Grid } from '../../puzzle/grid';
 import { Sets } from '../../util/sets';
 import { CageModel } from '../models/elements/cageModel';
 import { CellModel } from '../models/elements/cellModel';
-import { ReadonlySudokuNumsSet } from '../sets';
+import { ReadonlySudokuNumsSet, SudokuNumsSet } from '../sets';
 
 export class NumsReduction {
 
     private _cellMs = new Set<CellModel>();
     private _impactedCageMs = new Set<CageModel>();
+    private _deletedNumOptsPerCell = Grid.CELL_INDICES.map(() => SudokuNumsSet.newEmpty());
 
     deleteNumOpt(cellM: CellModel, num: number) {
         cellM.deleteNumOpt(num);
+        this._deletedNumOptsPerCell[cellM.cell.index].add(num);
         this.add(cellM);
     }
 
     reduceNumOpts(cellM: CellModel, nums: ReadonlySudokuNumsSet) {
         const deletedNums = cellM.reduceNumOpts(nums);
         if (deletedNums.isNotEmpty) {
+            this._deletedNumOptsPerCell[cellM.cell.index].addAll(deletedNums);
             this.add(cellM);
         }
     }
