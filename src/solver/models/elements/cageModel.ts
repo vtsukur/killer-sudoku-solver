d3dd6e7bf3245d6cad27451ddbef12760d54e75f
vtsukur <volodymyrt@wix.com>
2023-03-29 15:25:32 +0300

    [perf] Faster deletion of num options during reduction of `CageModel`s with 2 `Cell`s

--- src/solver/models/elements/cageModel.ts
+++ src/solver/models/elements/cageModel.ts
@@ -162,8 +162,8 @@ export class CageModel {
             if (!(cell0HasNum0 && cell0HasNum1 && cell1HasNum0 && cell1HasNum1)) {
                 if (!cell0HasNum0) {
                     if (!cell1HasNum0) {
-                        newReduction.tryDeleteNumOpt(cellM0, num1);
-                        newReduction.tryDeleteNumOpt(cellM1, num1);
+                        if (cell0HasNum1) newReduction.deleteNumOpt(cellM0, num1);
+                        if (cell1HasNum1) newReduction.deleteNumOpt(cellM1, num1);
                         this.deleteCombo(combo);
                     } else if (cell1HasNum1) {
                         newReduction.deleteNumOpt(cellM1, num1);
@@ -173,8 +173,8 @@ export class CageModel {
                 }
                 if (!cell0HasNum1) {
                     if (!cell1HasNum1) {
-                        newReduction.tryDeleteNumOpt(cellM0, num0);
-                        newReduction.tryDeleteNumOpt(cellM1, num0);
+                        if (cell0HasNum0) newReduction.deleteNumOpt(cellM0, num0);
+                        if (cell1HasNum0) newReduction.deleteNumOpt(cellM1, num0);
                         this.deleteCombo(combo);
                     } else if (cell1HasNum0) {
                         newReduction.deleteNumOpt(cellM1, num0);
