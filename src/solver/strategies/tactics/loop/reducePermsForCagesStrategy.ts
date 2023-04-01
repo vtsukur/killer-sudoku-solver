8cc6bf59220139811a2df307b1cc154d61799a0d
vtsukur <volodymyrt@wix.com>
2023-04-01 20:57:41 +0300

    [perf] Skipping `CageModel`s which are no longer present in the `MasterModel`

--- src/solver/strategies/tactics/loop/reducePermsForCagesStrategy.ts
+++ src/solver/strategies/tactics/loop/reducePermsForCagesStrategy.ts
@@ -6,7 +6,9 @@ export class ReducePermsForCagesStrategy extends Strategy {
         const reduction = this._context.reduction;
         let cageM = reduction.peek();
         while (cageM) {
-            cageM.reduce(reduction, reduction);
+            if (this._model.cageModelsMap.has(cageM.cage.key)) {
+                cageM.reduce(reduction, reduction);
+            }
             cageM = reduction.peek();
         }
     }
