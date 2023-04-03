49979179c369bfa557de17e4744c838398ea798c
vtsukur <volodymyrt@wix.com>
2023-04-03 17:18:50 +0300

    [refactoring] Renaming

--- src/solver/strategies/reduction/cageModelOfSize2DeletedNumsReducer.ts
+++ src/solver/strategies/reduction/cageModelOfSize2DeletedNumsReducer.ts
@@ -10,7 +10,7 @@ import { MasterModelReduction } from './masterModelReduction';
  *
  * @public
  */
-export class CageModelOfSize2Reducer implements CageModelReducer {
+export class CageModelOfSize2DeletedNumsReducer implements CageModelReducer {
 
     /**
      * The {@link CageModel} to reduce.
