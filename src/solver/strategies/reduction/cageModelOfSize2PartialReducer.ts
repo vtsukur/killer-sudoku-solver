8c9c796670126bfdef58b3876ddee9fbeaadd80c
vtsukur <volodymyrt@wix.com>
2023-04-05 21:11:38 +0300

    [perf] [refactoring] Faster `Combo` deletion

--- src/solver/strategies/reduction/cageModelOfSize2PartialReducer.ts
+++ src/solver/strategies/reduction/cageModelOfSize2PartialReducer.ts
@@ -1,5 +1,7 @@
+import { Combo, SumAddendsCombinatorics } from '../../math';
 import { CageModel } from '../../models/elements/cageModel';
 import { CellModel } from '../../models/elements/cellModel';
+import { SudokuNumsSet } from '../../sets';
 import { CageModelReducer } from './cageModelReducer';
 import { MasterModelReduction } from './masterModelReduction';
 
@@ -27,6 +29,8 @@ export class CageModelOfSize2PartialReducer implements CageModelReducer {
      */
     private readonly _cellM1: CellModel;
 
+    private readonly _combosByNum: Array<Combo>;
+
     /**
      * Constructs a new reducer of possible numbers for {@link CellModel}s
      * within a {@link CageModel} of a {@link Cage} with 2 {@link Cell}s.
@@ -37,6 +41,11 @@ export class CageModelOfSize2PartialReducer implements CageModelReducer {
         this._cageM = cageM;
         this._cellM0 = cageM.cellMs[0];
         this._cellM1 = cageM.cellMs[1];
+        this._combosByNum = new Array(SudokuNumsSet.MAX_NUM + 1);
+        for (const combo of SumAddendsCombinatorics.enumerate(cageM.cage.sum, 2).combosSet.combos) {
+            this._combosByNum[combo.number0] = combo;
+            this._combosByNum[combo.number1] = combo;
+        }
     }
 
     /**
@@ -51,15 +60,16 @@ export class CageModelOfSize2PartialReducer implements CageModelReducer {
         if (deletedNumOpts_cellM0.isNotEmpty) {
             for (const num of deletedNumOpts_cellM0.nums) {
                 const complementNum = this._cageM.cage.sum - num;
-                if (complementNum > 9) continue;
+                if (complementNum < 1 || complementNum > 9) continue;
                 reduction.tryDeleteNumOpt(this._cellM1, complementNum, this._cageM);
-                if (!(this._cellM0.hasNumOpt(complementNum) && this._cellM1.hasNumOpt(num))) {
-                    for (const combo of cageMCombos.combos) {
-                        if (combo.numsSet.has(num)) {
-                            cageMCombos.deleteCombo(combo);
-                            break;
-                        }
-                    }
+                if (this._combosByNum[num] && !(this._cellM0.hasNumOpt(complementNum) && this._cellM1.hasNumOpt(num))) {
+                    cageMCombos.deleteCombo(this._combosByNum[num]);
+                    // for (const combo of cageMCombos.combos) {
+                    //     if (combo.numsSet.has(num)) {
+                    //         cageMCombos.deleteCombo(combo);
+                    //         break;
+                    //     }
+                    // }
                 }
             }
         }
@@ -67,15 +77,16 @@ export class CageModelOfSize2PartialReducer implements CageModelReducer {
         if (deletedNumOpts_cellM1.isNotEmpty) {
             for (const num of deletedNumOpts_cellM1.nums) {
                 const complementNum = this._cageM.cage.sum - num;
-                if (complementNum > 9) continue;
+                if (complementNum < 1 || complementNum > 9) continue;
                 reduction.tryDeleteNumOpt(this._cellM0, complementNum, this._cageM);
-                if (!(this._cellM1.hasNumOpt(complementNum) && this._cellM0.hasNumOpt(num))) {
-                    for (const combo of cageMCombos.combos) {
-                        if (combo.numsSet.has(num)) {
-                            cageMCombos.deleteCombo(combo);
-                            break;
-                        }
-                    }
+                if (this._combosByNum[num] && !(this._cellM1.hasNumOpt(complementNum) && this._cellM0.hasNumOpt(num))) {
+                    cageMCombos.deleteCombo(this._combosByNum[num]);
+                    // for (const combo of cageMCombos.combos) {
+                    //     if (combo.numsSet.has(num)) {
+                    //         cageMCombos.deleteCombo(combo);
+                    //         break;
+                    //     }
+                    // }
                 }
             }
         }
