aec05478e5ebb83c5530097b1693b12c1721d623
vtsukur <volodymyrt@wix.com>
2023-04-08 21:34:55 +0300

    [perf] Dumping current state of optimal reducer based on stage 2 optimal reducer

--- src/solver/strategies/reduction/cageModelOfSize2OptimalReducer.ts
+++ src/solver/strategies/reduction/cageModelOfSize2OptimalReducer.ts
@@ -247,8 +247,8 @@ export class CageModelOfSize2OptimalReducer implements CageModelReducer {
      * @see CageModelReducer.reduce
      */
     reduce(reduction: MasterModelReduction): void {
-        const cellM0NumSetValue = this._cellM0.numOptsSet().bitStore;
-        const cellM1NumSetValue = this._cellM1.numOptsSet().bitStore;
+        const cellM0NumSetValue = this._cellM0._numOptsSet.bitStore;
+        const cellM1NumSetValue = this._cellM1._numOptsSet.bitStore;
 
         const cageMCombos = this._cageM.comboSet;
 
@@ -257,11 +257,10 @@ export class CageModelOfSize2OptimalReducer implements CageModelReducer {
             const num0 = combo.number0;
             const num1 = combo.number1;
 
-            const optionsCellM0 = ((cellM0NumSetValue & (1 << num0)) >> num0) |
-            ((cellM0NumSetValue & (1 << num1)) >> (num1 - 1));
-            const optionsCellM1 = ((cellM1NumSetValue & (1 << num0)) >> num0) |
-                    ((cellM1NumSetValue & (1 << num1)) >> (num1 - 1));
-            const options = optionsCellM1 | optionsCellM0 << 2;
+            const options = (((cellM1NumSetValue & (1 << num0)) >> num0) |
+                    ((cellM1NumSetValue & (1 << num1)) >> (num1 - 1))) |
+                    (((cellM0NumSetValue & (1 << num0)) >> num0) |
+                    ((cellM0NumSetValue & (1 << num1)) >> (num1 - 1))) << 2;
 
             tacticalReducers[options](this._cellM0, this._cellM1, this._cageM, cageMCombos, combo, num0, num1, reduction);
         }
