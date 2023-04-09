1eb418aababa5d92bb31bdef9eaf70422d4d0eb8
vtsukur <volodymyrt@wix.com>
2023-04-09 13:20:46 +0300

    [style] Reordering declarations for readability

--- src/solver/strategies/reduction/cageModelOfSize2Reducer.ts
+++ src/solver/strategies/reduction/cageModelOfSize2Reducer.ts
@@ -15,8 +15,66 @@ type DenormalizedTactialReducer = (
         num1: number,
         reduction: MasterModelReduction) => void;
 
-function NOTHING_TO_REDUCE() {
-    // No-op.
+/**
+ * Reduces possible numbers for {@link CellModel}s
+ * within a {@link CageModel} of a {@link Cage} with 2 {@link Cell}s
+ * by checking the validity of numbers' options given possible {@link Combo}s for the {@link CageModel}.
+ *
+ * @public
+ */
+export class CageModelOfSize2Reducer implements CageModelReducer {
+
+    /**
+     * The {@link CageModel} to reduce.
+     */
+    private readonly _cageM: CageModel;
+
+    /**
+     * The first {@link CellModel} of the {@link CageModel}.
+     */
+    private readonly _cellM0: CellModel;
+
+    /**
+     * The second {@link CellModel} of the {@link CageModel}.
+     */
+    private readonly _cellM1: CellModel;
+
+    /**
+     * Constructs a new reducer of possible numbers for {@link CellModel}s
+     * within a {@link CageModel} of a {@link Cage} with 2 {@link Cell}s.
+     *
+     * @param cageM — The {@link CageModel} to reduce.
+     */
+    constructor(cageM: CageModel) {
+        this._cageM = cageM;
+        this._cellM0 = cageM.cellMs[0];
+        this._cellM1 = cageM.cellMs[1];
+    }
+
+    /**
+     * @see CageModelReducer.reduce
+     */
+    reduce(reduction: MasterModelReduction): void {
+        const cellM0NumSetValue = this._cellM0._numOptsSet.bitStore;
+        const cellM1NumSetValue = this._cellM1._numOptsSet.bitStore;
+
+        const cageMCombos = this._cageM.comboSet;
+
+        // Iterating over each registered `Combo` (there are up to 4 `Combo`s for a `Cage` with 2 `Cell`s) ...
+        for (const combo of cageMCombos.combos) {
+            const num0 = combo.number0;
+            const num1 = combo.number1;
+
+            const options =
+                    (((cellM0NumSetValue & (1 << num0)) >> num0) |
+                    ((cellM0NumSetValue & (1 << num1)) >> (num1 - 1))) |
+                    (((cellM1NumSetValue & (1 << num0)) >> num0) |
+                    ((cellM1NumSetValue & (1 << num1)) >> (num1 - 1))) << 2;
+
+            DENORMALIZED_TACTICAL_REDUCERS[options](this._cellM0, this._cellM1, this._cageM, cageMCombos, combo, num0, num1, reduction);
+        }
+    }
+
 }
 
 const DENORMALIZED_TACTICAL_REDUCERS: ReadonlyArray<DenormalizedTactialReducer> = [
@@ -175,64 +233,6 @@ const DENORMALIZED_TACTICAL_REDUCERS: ReadonlyArray<DenormalizedTactialReducer> 
     NOTHING_TO_REDUCE, // 15
 ];
 
-/**
- * Reduces possible numbers for {@link CellModel}s
- * within a {@link CageModel} of a {@link Cage} with 2 {@link Cell}s
- * by checking the validity of numbers' options given possible {@link Combo}s for the {@link CageModel}.
- *
- * @public
- */
-export class CageModelOfSize2Reducer implements CageModelReducer {
-
-    /**
-     * The {@link CageModel} to reduce.
-     */
-    private readonly _cageM: CageModel;
-
-    /**
-     * The first {@link CellModel} of the {@link CageModel}.
-     */
-    private readonly _cellM0: CellModel;
-
-    /**
-     * The second {@link CellModel} of the {@link CageModel}.
-     */
-    private readonly _cellM1: CellModel;
-
-    /**
-     * Constructs a new reducer of possible numbers for {@link CellModel}s
-     * within a {@link CageModel} of a {@link Cage} with 2 {@link Cell}s.
-     *
-     * @param cageM — The {@link CageModel} to reduce.
-     */
-    constructor(cageM: CageModel) {
-        this._cageM = cageM;
-        this._cellM0 = cageM.cellMs[0];
-        this._cellM1 = cageM.cellMs[1];
-    }
-
-    /**
-     * @see CageModelReducer.reduce
-     */
-    reduce(reduction: MasterModelReduction): void {
-        const cellM0NumSetValue = this._cellM0._numOptsSet.bitStore;
-        const cellM1NumSetValue = this._cellM1._numOptsSet.bitStore;
-
-        const cageMCombos = this._cageM.comboSet;
-
-        // Iterating over each registered `Combo` (there are up to 4 `Combo`s for a `Cage` with 2 `Cell`s) ...
-        for (const combo of cageMCombos.combos) {
-            const num0 = combo.number0;
-            const num1 = combo.number1;
-
-            const options =
-                    (((cellM0NumSetValue & (1 << num0)) >> num0) |
-                    ((cellM0NumSetValue & (1 << num1)) >> (num1 - 1))) |
-                    (((cellM1NumSetValue & (1 << num0)) >> num0) |
-                    ((cellM1NumSetValue & (1 << num1)) >> (num1 - 1))) << 2;
-
-            DENORMALIZED_TACTICAL_REDUCERS[options](this._cellM0, this._cellM1, this._cageM, cageMCombos, combo, num0, num1, reduction);
-        }
-    }
-
+function NOTHING_TO_REDUCE() {
+    // No-op.
 }
