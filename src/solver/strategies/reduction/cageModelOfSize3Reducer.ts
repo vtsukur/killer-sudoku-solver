867064ebe76596ace1cf12648abdddc00a5666c8
vtsukur <volodymyrt@wix.com>
2023-04-09 22:13:16 +0300

    [refactoring] Adding stub for `CageModelOfSize3Reducer`

---
+++ src/solver/strategies/reduction/cageModelOfSize3Reducer.ts
@@ -0,0 +1,55 @@
+import { CageModel } from '../../models/elements/cageModel';
+import { CellModel } from '../../models/elements/cellModel';
+import { CageModelReducer } from './cageModelReducer';
+import { MasterModelReduction } from './masterModelReduction';
+
+/**
+ * Reduces possible numbers for {@link CellModel}s
+ * within a {@link CageModel} of a {@link Cage} with 3 {@link Cell}s
+ * by checking the validity of numbers' options given possible {@link Combo}s for the {@link CageModel}.
+ *
+ * @public
+ */
+export class CageModelOfSize3Reducer implements CageModelReducer {
+
+    /**
+     * The {@link CageModel} to reduce.
+     */
+    private readonly _cageM: CageModel;
+
+    /**
+     * The first {@link CellModel} of the {@link CageModel}.
+     */
+    private readonly _cellM1: CellModel;
+
+    /**
+     * The second {@link CellModel} of the {@link CageModel}.
+     */
+    private readonly _cellM2: CellModel;
+
+    /**
+     * The third {@link CellModel} of the {@link CageModel}.
+     */
+    private readonly _cellM3: CellModel;
+
+    /**
+     * Constructs a new reducer of possible numbers for {@link CellModel}s
+     * within a {@link CageModel} of a {@link Cage} with 3 {@link Cell}s.
+     *
+     * @param cageM — The {@link CageModel} to reduce.
+     */
+    constructor(cageM: CageModel) {
+        this._cageM = cageM;
+        this._cellM1 = cageM.cellMs[0];
+        this._cellM2 = cageM.cellMs[1];
+        this._cellM3 = cageM.cellMs[2];
+    }
+
+    /**
+     * @see CageModelReducer.reduce
+     */
+    reduce(reduction: MasterModelReduction): void {
+        // TBD
+    }
+
+}
