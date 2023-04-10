691df795cf5c687deddd7fe5d7172fc837d3fc20
vtsukur <flushdia@gmail.com>
2023-04-10 20:39:56 +0300

    [test] [refactoring] Introducing type alias for `CageModelReducerProducer`

--- tests/unit/solver/strategies/reduction/cageModelReducerTestConfig.ts
+++ tests/unit/solver/strategies/reduction/cageModelReducerTestConfig.ts
@@ -1,9 +1,11 @@
 import { CageModel } from '../../../../../src/solver/models/elements/cageModel';
 import { CageModelReducer } from '../../../../../src/solver/strategies/reduction/cageModelReducer';
 
+type CageModelReducerProducer = (cageM: CageModel) => CageModelReducer;
+
 export type CageModelReducerTestConfig = {
 
-    readonly newReducer: (cageM: CageModel) => CageModelReducer;
+    readonly newReducer: CageModelReducerProducer;
 
     readonly type: string;
 
