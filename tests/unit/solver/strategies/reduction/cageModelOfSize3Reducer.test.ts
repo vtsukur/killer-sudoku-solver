4a92d875f6eff48a7db322f30f13996447f40e4b
vtsukur <flushdia@gmail.com>
2023-04-12 21:52:27 +0300

    [perf] [test] Generalizing reduction DB generation

--- tests/unit/solver/strategies/reduction/cageModelOfSize3Reducer.test.ts
+++ tests/unit/solver/strategies/reduction/cageModelOfSize3Reducer.test.ts
@@ -2,7 +2,7 @@ import * as fs from 'node:fs';
 import { stringify } from 'yaml';
 import { Cage } from '../../../../../src/puzzle/cage';
 import { Cell } from '../../../../../src/puzzle/cell';
-import { Combo } from '../../../../../src/solver/math';
+import { Combo, SumAddendsCombinatorics } from '../../../../../src/solver/math';
 import { CageModel } from '../../../../../src/solver/models/elements/cageModel';
 import { CellModel } from '../../../../../src/solver/models/elements/cellModel';
 import { CageModelOfSize3FullReducer } from '../../../../../src/solver/strategies/reduction/archive/cageModelOfSize3FullReducer';
@@ -13,6 +13,8 @@ import { logFactory } from '../../../../../src/util/logFactory';
 import { joinSet } from '../../../../../src/util/readableMessages';
 import { CageModelReducerTestConfig } from './cageModelReducerTestConfig';
 import { CageModelOfSize3DbReducer } from '../../../../../src/solver/strategies/reduction/cageModelOfSize3DbReducer';
+import { SumReductions } from '../../../../../src/solver/strategies/reduction/db/reductionDb';
+import { ComboReductions } from '../../../../../src/solver/strategies/reduction/db/reductionDb';
 
 const log = logFactory.withLabel('cageModelOfSize3Reducers.test');
 
@@ -63,155 +65,162 @@ describe('CageModelOfSize3Reducers', () => {
         const cell1 = Cell.at(0, 0);
         const cell2 = Cell.at(0, 1);
         const cell3 = Cell.at(0, 2);
-        const cage = Cage.ofSum(6).withCell(cell1).withCell(cell2).withCell(cell3).new();
 
         let validPerms = 0;
         let reductionActionable = 0;
 
         const padding = '    ';
         let tacticalReducersCode = '\n';
-        const entries: Array<ReductionEntry> = [];
+        const sums: Array<SumReductions> = [];
         const reductionsDb: CageSizeNReductionsDb = {
             cageSize: 3,
-            sums: [
-                {
-                    sum: 6,
-                    combos: [
-                        {
-                            combo: Combo.of(1, 2, 3).numsSet.nums,
-                            entries
-                        }
-                    ]
-                }
-            ]
+            sums
         };
-        const wrapCodeLine = (line: string) => `${padding}${line}\n`;
-        let state = 0;
-        const combo = Combo.of(1, 2, 3);
-        while (state < 512) {
-            const cellM1 = new CellModel(cell1);
-            const cellM2 = new CellModel(cell2);
-            const cellM3 = new CellModel(cell3);
-            const cageM = new CageModel(cage, [ cellM1, cellM2, cellM3 ]);
-            cellM1.addWithinCageModel(cageM);
-            cellM2.addWithinCageModel(cageM);
-            cellM3.addWithinCageModel(cageM);
-            cageM.initialReduce();
 
-            let isPotentialReductionFailure;
-            // `0b0000 =  0`
+        for (const sum of [ 6 ]) {
+            const combos: Array<ComboReductions> = [];
+            const sumReductions: SumReductions = { sum, combos };
 
-            const stateRadix2 = state.toString(2);
-            const paddedStateRadix2 = stateRadix2.padStart(9, '0');
-            const stateRadix2_last3Bits = paddedStateRadix2.substring(0, 3);
-            const stateRadix2_middle3Bits = paddedStateRadix2.substring(3, 6);
-            const stateRadix2_first3Bits = paddedStateRadix2.substring(6);
-            const commentCode = `// \`0b${stateRadix2_last3Bits}_${stateRadix2_middle3Bits}_${stateRadix2_first3Bits} = ${state}\``;
-            tacticalReducersCode += wrapCodeLine(commentCode);
+            for (const combo of SumAddendsCombinatorics.enumerate(sum, 3).val) {
+                const entries: Array<ReductionEntry> = [];
+                const comboReductions: ComboReductions = {
+                    combo: combo.numsSet.nums,
+                    entries
+                };
 
-            try {
-                isPotentialReductionFailure = false;
-                if (!(state & (1 << 0))) cellM1.deleteNumOpt(combo.nthNumber(0));
-                if (!(state & (1 << 1))) cellM1.deleteNumOpt(combo.nthNumber(1));
-                if (!(state & (1 << 2))) cellM1.deleteNumOpt(combo.nthNumber(2));
+                const cage = Cage.ofSum(sum).withCell(cell1).withCell(cell2).withCell(cell3).new();
 
-                if (!(state & (1 << 3))) cellM2.deleteNumOpt(combo.nthNumber(0));
-                if (!(state & (1 << 4))) cellM2.deleteNumOpt(combo.nthNumber(1));
-                if (!(state & (1 << 5))) cellM2.deleteNumOpt(combo.nthNumber(2));
+                const wrapCodeLine = (line: string) => `${padding}${line}\n`;
+                let state = 0;
+                while (state < 512) {
+                    const cellM1 = new CellModel(cell1);
+                    const cellM2 = new CellModel(cell2);
+                    const cellM3 = new CellModel(cell3);
+                    const cageM = new CageModel(cage, [ cellM1, cellM2, cellM3 ]);
+                    cellM1.addWithinCageModel(cageM);
+                    cellM2.addWithinCageModel(cageM);
+                    cellM3.addWithinCageModel(cageM);
+                    cageM.initialReduce();
 
-                if (!(state & (1 << 6))) cellM3.deleteNumOpt(combo.nthNumber(0));
-                if (!(state & (1 << 7))) cellM3.deleteNumOpt(combo.nthNumber(1));
-                if (!(state & (1 << 8))) cellM3.deleteNumOpt(combo.nthNumber(2));
+                    let isPotentialReductionFailure;
 
-                const cellM3NumOptsBefore = new Set(cellM3.numOpts());
-                const cellM2NumOptsBefore = new Set(cellM2.numOpts());
-                const cellM1NumOptsBefore = new Set(cellM1.numOpts());
-                log.info(`${state}: BEFORE CellM3(${joinSet(cellM3NumOptsBefore)}), CellM2(${joinSet(cellM2NumOptsBefore)}), CellM1(${joinSet(cellM1NumOptsBefore)})`);
+                    const stateRadix2 = state.toString(2);
+                    const paddedStateRadix2 = stateRadix2.padStart(9, '0');
+                    const stateRadix2_last3Bits = paddedStateRadix2.substring(0, 3);
+                    const stateRadix2_middle3Bits = paddedStateRadix2.substring(3, 6);
+                    const stateRadix2_first3Bits = paddedStateRadix2.substring(6);
+                    const commentCode = `// \`0b${stateRadix2_last3Bits}_${stateRadix2_middle3Bits}_${stateRadix2_first3Bits} = ${state}\``;
+                    tacticalReducersCode += wrapCodeLine(commentCode);
 
-                isPotentialReductionFailure = true;
-                const reduction = new MasterModelReduction();
-                cageM.reduce(reduction);
+                    try {
+                        isPotentialReductionFailure = false;
+                        if (!(state & (1 << 0))) cellM1.deleteNumOpt(combo.nthNumber(0));
+                        if (!(state & (1 << 1))) cellM1.deleteNumOpt(combo.nthNumber(1));
+                        if (!(state & (1 << 2))) cellM1.deleteNumOpt(combo.nthNumber(2));
 
-                const cellM3NumOptsAfter = new Set(cellM3.numOpts());
-                const cellM2NumOptsAfter = new Set(cellM2.numOpts());
-                const cellM1NumOptsAfter = new Set(cellM1.numOpts());
-                log.info(`${state}: AFTER SUCCESS CellM3(${joinSet(cellM3NumOptsAfter)}), CellM2(${joinSet(cellM2NumOptsAfter)}), CellM1(${joinSet(cellM1NumOptsAfter)})`);
+                        if (!(state & (1 << 3))) cellM2.deleteNumOpt(combo.nthNumber(0));
+                        if (!(state & (1 << 4))) cellM2.deleteNumOpt(combo.nthNumber(1));
+                        if (!(state & (1 << 5))) cellM2.deleteNumOpt(combo.nthNumber(2));
 
-                let singleTacticalReducerCode = '';
-                let cellM1Used = false;
-                let cellM2Used = false;
-                let cellM3Used = false;
-                const cellM1DeletedNums = new Array<number>();
-                const cellM2DeletedNums = new Array<number>();
-                const cellM3DeletedNums = new Array<number>();
-                for (const originalNum of cellM1NumOptsBefore) {
-                    if (!cellM1NumOptsAfter.has(originalNum)) {
-                        singleTacticalReducerCode += wrapCodeLine(`${padding}reduction.deleteNumOpt(cellM1, ${originalNum}, cageM);`);
-                        cellM1DeletedNums.push(originalNum);
-                        cellM1Used = true;
+                        if (!(state & (1 << 6))) cellM3.deleteNumOpt(combo.nthNumber(0));
+                        if (!(state & (1 << 7))) cellM3.deleteNumOpt(combo.nthNumber(1));
+                        if (!(state & (1 << 8))) cellM3.deleteNumOpt(combo.nthNumber(2));
+
+                        const cellM3NumOptsBefore = new Set(cellM3.numOpts());
+                        const cellM2NumOptsBefore = new Set(cellM2.numOpts());
+                        const cellM1NumOptsBefore = new Set(cellM1.numOpts());
+                        log.info(`${state}: BEFORE CellM3(${joinSet(cellM3NumOptsBefore)}), CellM2(${joinSet(cellM2NumOptsBefore)}), CellM1(${joinSet(cellM1NumOptsBefore)})`);
+
+                        isPotentialReductionFailure = true;
+                        const reduction = new MasterModelReduction();
+                        cageM.reduce(reduction);
+
+                        const cellM3NumOptsAfter = new Set(cellM3.numOpts());
+                        const cellM2NumOptsAfter = new Set(cellM2.numOpts());
+                        const cellM1NumOptsAfter = new Set(cellM1.numOpts());
+                        log.info(`${state}: AFTER SUCCESS CellM3(${joinSet(cellM3NumOptsAfter)}), CellM2(${joinSet(cellM2NumOptsAfter)}), CellM1(${joinSet(cellM1NumOptsAfter)})`);
+
+                        let singleTacticalReducerCode = '';
+                        let cellM1Used = false;
+                        let cellM2Used = false;
+                        let cellM3Used = false;
+                        const cellM1DeletedNums = new Array<number>();
+                        const cellM2DeletedNums = new Array<number>();
+                        const cellM3DeletedNums = new Array<number>();
+                        for (const originalNum of cellM1NumOptsBefore) {
+                            if (!cellM1NumOptsAfter.has(originalNum)) {
+                                singleTacticalReducerCode += wrapCodeLine(`${padding}reduction.deleteNumOpt(cellM1, ${originalNum}, cageM);`);
+                                cellM1DeletedNums.push(originalNum);
+                                cellM1Used = true;
+                            }
+                        }
+                        for (const originalNum of cellM2NumOptsBefore) {
+                            if (!cellM2NumOptsAfter.has(originalNum)) {
+                                singleTacticalReducerCode += wrapCodeLine(`${padding}reduction.deleteNumOpt(cellM2, ${originalNum}, cageM);`);
+                                cellM2DeletedNums.push(originalNum);
+                                cellM2Used = true;
+                            }
+                        }
+                        for (const originalNum of cellM3NumOptsBefore) {
+                            if (!cellM3NumOptsAfter.has(originalNum)) {
+                                singleTacticalReducerCode += wrapCodeLine(`${padding}reduction.deleteNumOpt(cellM3, ${originalNum}, cageM);`);
+                                cellM3DeletedNums.push(originalNum);
+                                cellM3Used = true;
+                            }
+                        }
+
+                        let actions: ReductionActions | undefined;
+                        if (cellM1Used || cellM2Used || cellM3Used) {
+                            actions = {
+                                isDeleteCombo: false,
+                                deleteNumsInCell1: cellM1DeletedNums.length ? cellM1DeletedNums : undefined,
+                                deleteNumsInCell2: cellM2DeletedNums.length ? cellM2DeletedNums : undefined,
+                                deleteNumsInCell3: cellM3DeletedNums.length ? cellM3DeletedNums : undefined
+                            };
+                        }
+
+                        if (singleTacticalReducerCode) {
+                            log.info(`${state}: REDUCTION ACTIONS\n${singleTacticalReducerCode}`);
+                            ++reductionActionable;
+                            const cellM1ParamDeclaration = cellM1Used ? 'cellM1' : '_cellM1';
+                            const cellM2ParamConditionalDeclaration = cellM2Used ? ', cellM2' : (cellM3Used ? ', _cellM2' : '');
+                            const cellM3ParamConditionalDeclaration = cellM3Used ? ', cellM3' : '';
+                            tacticalReducersCode += wrapCodeLine(`(reduction, cageM, _combosSet, _combo, ${cellM1ParamDeclaration}${cellM2ParamConditionalDeclaration}${cellM3ParamConditionalDeclaration}) => {`);
+                            tacticalReducersCode += singleTacticalReducerCode;
+                            tacticalReducersCode += wrapCodeLine('},');
+                        } else {
+                            log.info(`${state}: NO REDUCTION ACTIONS`);
+                            tacticalReducersCode += wrapCodeLine('NOTHING_TO_REDUCE,');
+                        }
+
+                        entries.push({
+                            state,
+                            stateRadix2String: `0b${stateRadix2_last3Bits}_${stateRadix2_middle3Bits}_${stateRadix2_first3Bits}`,
+                            isValid: true,
+                            actions
+                        });
+
+                        ++validPerms;
+                    } catch (e) {
+                        // Can fail, that's OK.
+                        log.info(`${state}: FAIL FOR ${state.toString(2)} (${isPotentialReductionFailure ? 'reduction' : 'num deletion'})`);
+                        tacticalReducersCode += wrapCodeLine('IMPOSSIBLE_TO_REDUCE,');
+                        entries.push({
+                            state,
+                            stateRadix2String: `0b${stateRadix2_last3Bits}_${stateRadix2_middle3Bits}_${stateRadix2_first3Bits}`,
+                            isValid: false,
+                            actions: undefined
+                        });
                     }
-                }
-                for (const originalNum of cellM2NumOptsBefore) {
-                    if (!cellM2NumOptsAfter.has(originalNum)) {
-                        singleTacticalReducerCode += wrapCodeLine(`${padding}reduction.deleteNumOpt(cellM2, ${originalNum}, cageM);`);
-                        cellM2DeletedNums.push(originalNum);
-                        cellM2Used = true;
-                    }
-                }
-                for (const originalNum of cellM3NumOptsBefore) {
-                    if (!cellM3NumOptsAfter.has(originalNum)) {
-                        singleTacticalReducerCode += wrapCodeLine(`${padding}reduction.deleteNumOpt(cellM3, ${originalNum}, cageM);`);
-                        cellM3DeletedNums.push(originalNum);
-                        cellM3Used = true;
-                    }
+                    log.info('');
+
+                    ++state;
                 }
 
-                let actions: ReductionActions | undefined;
-                if (cellM1Used || cellM2Used || cellM3Used) {
-                    actions = {
-                        isDeleteCombo: false,
-                        deleteNumsInCell1: cellM1DeletedNums.length ? cellM1DeletedNums : undefined,
-                        deleteNumsInCell2: cellM2DeletedNums.length ? cellM2DeletedNums : undefined,
-                        deleteNumsInCell3: cellM3DeletedNums.length ? cellM3DeletedNums : undefined
-                    };
-                }
-
-                if (singleTacticalReducerCode) {
-                    log.info(`${state}: REDUCTION ACTIONS\n${singleTacticalReducerCode}`);
-                    ++reductionActionable;
-                    const cellM1ParamDeclaration = cellM1Used ? 'cellM1' : '_cellM1';
-                    const cellM2ParamConditionalDeclaration = cellM2Used ? ', cellM2' : (cellM3Used ? ', _cellM2' : '');
-                    const cellM3ParamConditionalDeclaration = cellM3Used ? ', cellM3' : '';
-                    tacticalReducersCode += wrapCodeLine(`(reduction, cageM, _combosSet, _combo, ${cellM1ParamDeclaration}${cellM2ParamConditionalDeclaration}${cellM3ParamConditionalDeclaration}) => {`);
-                    tacticalReducersCode += singleTacticalReducerCode;
-                    tacticalReducersCode += wrapCodeLine('},');
-                } else {
-                    log.info(`${state}: NO REDUCTION ACTIONS`);
-                    tacticalReducersCode += wrapCodeLine('NOTHING_TO_REDUCE,');
-                }
-
-                entries.push({
-                    state,
-                    stateRadix2String: `0b${stateRadix2_last3Bits}_${stateRadix2_middle3Bits}_${stateRadix2_first3Bits}`,
-                    isValid: true,
-                    actions
-                });
-
-                ++validPerms;
-            } catch (e) {
-                // Can fail, that's OK.
-                log.info(`${state}: FAIL FOR ${state.toString(2)} (${isPotentialReductionFailure ? 'reduction' : 'num deletion'})`);
-                tacticalReducersCode += wrapCodeLine('IMPOSSIBLE_TO_REDUCE,');
-                entries.push({
-                    state,
-                    stateRadix2String: `0b${stateRadix2_last3Bits}_${stateRadix2_middle3Bits}_${stateRadix2_first3Bits}`,
-                    isValid: false,
-                    actions: undefined
-                });
+                combos.push(comboReductions);
             }
-            log.info('');
 
-            ++state;
+            sums.push(sumReductions);
         }
 
         log.info(`Valid perms: ${validPerms} out of 512`);
