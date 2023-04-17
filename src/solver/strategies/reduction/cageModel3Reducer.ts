19b659f9005c228a6ac190215103e0dcfc2392a9
vtsukur <flushdia@gmail.com>
2023-04-17 21:17:27 +0300

    [cleanup] Removing unused code

--- src/solver/strategies/reduction/cageModel3Reducer.ts
+++ src/solver/strategies/reduction/cageModel3Reducer.ts
@@ -1,25 +1,12 @@
 import * as fs from 'node:fs';
 import { parse } from 'yaml';
-import { InvalidSolverStateError } from '../../invalidSolverStateError';
 import { CageModel } from '../../models/elements/cageModel';
 import { CellModel } from '../../models/elements/cellModel';
-import { ReadonlySudokuNumsSet, SudokuNumsSet } from '../../sets';
+import { CombosSet, ReadonlySudokuNumsSet, SudokuNumsSet } from '../../sets';
 import { CageModelReducer } from './cageModelReducer';
 import { MasterModelReduction } from './masterModelReduction';
 import { CageSizeNReductionsDb } from './db/reductionDb';
 import { Combo, SumAddendsCombinatorics } from '../../math';
-
-/**
- * Type alias for pre-coded denormalized reducing function
- * with hardcoded actions relevant to the specific `Combo` numbers in the `CellModel`s.
- */
-type DenormalizedTacticalReducer = (
-    reduction: MasterModelReduction,
-    cageM: CageModel,
-    cellM1: CellModel,
-    cellM2: CellModel,
-    cellM3: CellModel
-) => void;
 
 type ReductionState = {
     isValid: boolean;
@@ -45,89 +32,11 @@ const INVALID_REDUCTION_STATE: ReductionState = {
     deleteNumsInCell3Bits: 0
 };
 
-
-type DenormalizedTacticalReducerProducer = (
-    cellM1DeletedNums: ReadonlySudokuNumsSet,
-    cellM2DeletedNums: ReadonlySudokuNumsSet,
-    cellM3DeletedNums: ReadonlySudokuNumsSet
-) => DenormalizedTacticalReducer;
-
-const DENORMALIZED_TACTICAL_REDUCERS_PRODUCERS: ReadonlyArray<DenormalizedTacticalReducerProducer> = [
-// `0b000 = 0`
-() => NOTHING_TO_REDUCE,
-// `0b001 = 1`
-(cellM1DeletedNums) => {
-    return (reduction, cageM, cellM1) => {
-        reduction.deleteNumOpts(cellM1, cellM1DeletedNums, cageM);
-    };
-},
-// `0b010 = 2`
-(_cellM1DeletedNums, cellM2DeletedNums) => {
-    return (reduction, cageM, _cellM1, cellM2) => {
-        reduction.deleteNumOpts(cellM2, cellM2DeletedNums, cageM);
-    };
-},
-// `0b011 = 3`
-(cellM1DeletedNums, cellM2DeletedNums) => {
-    return (reduction, cageM, cellM1, cellM2) => {
-        reduction.deleteNumOpts(cellM1, cellM1DeletedNums, cageM);
-        reduction.deleteNumOpts(cellM2, cellM2DeletedNums, cageM);
-    };
-},
-// `0b100 = 4`
-(_cellM1DeletedNums, _cellM2DeletedNums, cellM3DeletedNums) => {
-    return (reduction, cageM, _cellM1, _cellM2, cellM3) => {
-        reduction.deleteNumOpts(cellM3, cellM3DeletedNums, cageM);
-    };
-},
-// `0b101 = 5`
-(cellM1DeletedNums, _cellM2DeletedNums, cellM3DeletedNums) => {
-    return (reduction, cageM, cellM1, _cellM2, cellM3) => {
-        reduction.deleteNumOpts(cellM1, cellM1DeletedNums, cageM);
-        reduction.deleteNumOpts(cellM3, cellM3DeletedNums, cageM);
-    };
-},
-// `0b110 = 6`
-(_cellM1DeletedNums, cellM2DeletedNums, cellM3DeletedNums) => {
-    return (reduction, cageM, _cellM1, cellM2, cellM3) => {
-        reduction.deleteNumOpts(cellM2, cellM2DeletedNums, cageM);
-        reduction.deleteNumOpts(cellM3, cellM3DeletedNums, cageM);
-    };
-},
-// `0b111 = 7`
-(cellM1DeletedNums, cellM2DeletedNums, cellM3DeletedNums) => {
-    return (reduction, cageM, cellM1, cellM2, cellM3) => {
-        reduction.deleteNumOpts(cellM1, cellM1DeletedNums, cageM);
-        reduction.deleteNumOpts(cellM2, cellM2DeletedNums, cageM);
-        reduction.deleteNumOpts(cellM3, cellM3DeletedNums, cageM);
-    };
-}
-];
-
 const dbString = fs.readFileSync('./src/solver/strategies/reduction/db/cage3_reductions.yaml', 'utf-8');
 const db = parse(dbString) as CageSizeNReductionsDb;
 
 const ALL_REDUCTION_STATES: Array<Array<ReadonlyArray<ReductionState>>> = new Array(db[db.length - 1].sum + 1);
-const DENORMALIZED_TACTICAL_REDUCERS_FOR_SUMS: Array<Array<ReadonlyArray<DenormalizedTacticalReducer>>> = new Array(db[db.length - 1].sum + 1);
 db.forEach(sumReductions => {
-    DENORMALIZED_TACTICAL_REDUCERS_FOR_SUMS[sumReductions.sum] = sumReductions.combos.map(comboReductions => {
-        const reducers = new Array<DenormalizedTacticalReducer>(512).fill(IMPOSSIBLE_TO_REDUCE);
-        for (const entry of comboReductions.entries) {
-            if (entry.actions) {
-                const cellM1DeletedNums = SudokuNumsSet.of(...entry.actions.deleteNums[0]);
-                const cellM2DeletedNums = SudokuNumsSet.of(...entry.actions.deleteNums[1]);
-                const cellM3DeletedNums = SudokuNumsSet.of(...entry.actions.deleteNums[2]);
-                const state =
-                            (cellM1DeletedNums?.bitStore ? 1 : 0) |
-                        ((cellM2DeletedNums?.bitStore ? 1 : 0) << 1) |
-                        ((cellM3DeletedNums?.bitStore ? 1 : 0) << 2);
-                reducers[entry.state] = DENORMALIZED_TACTICAL_REDUCERS_PRODUCERS[state](cellM1DeletedNums, cellM2DeletedNums, cellM3DeletedNums);
-            } else {
-                reducers[entry.state] = NOTHING_TO_REDUCE;
-            }
-        }
-        return reducers;
-    });
     ALL_REDUCTION_STATES[sumReductions.sum] = sumReductions.combos.map(comboReductions => {
         const comboNumsSet = SudokuNumsSet.of(...comboReductions.combo);
         const reductionStates = new Array<ReductionState>(512).fill(INVALID_REDUCTION_STATE);
@@ -152,20 +61,6 @@ db.forEach(sumReductions => {
 });
 
 /**
- * Empty reducing function.
- */
-function NOTHING_TO_REDUCE() {
-    // No-op.
-}
-
-/**
- * Throwing reducing function which is executed if the combination of numbers is not possible.
- */
-function IMPOSSIBLE_TO_REDUCE() {
-    throw new InvalidSolverStateError('Met compressed numbers state which should not be possible');
-}
-
-/**
  * Reduces possible numbers for {@link CellModel}s
  * within a {@link CageModel} of a {@link Cage} with 3 {@link Cell}s
  * by checking the validity of numbers' options given possible {@link Combo}s for the {@link CageModel}.
@@ -180,19 +75,42 @@ export class CageModel3Reducer implements CageModelReducer {
     private readonly _cageM: CageModel;
 
     /**
-     * The first {@link CellModel} of the {@link CageModel}.
+     * Cache for the {@link CageModel}'s {@link CombosSet}.
+     */
+    private readonly _combosSet: CombosSet;
+
+    /**
+     * Cache for the first {@link CellModel} of the {@link CageModel}.
      */
     private readonly _cellM1: CellModel;
 
     /**
-     * The second {@link CellModel} of the {@link CageModel}.
+     * Cache for {@link SudokuNumsSet} of possible number's options
+     * for the first {@link CellModel} of the {@link CageModel}.
+     */
+    private readonly _cellM1NumsSet: ReadonlySudokuNumsSet;
+
+    /**
+     * Cache for the second {@link CellModel} of the {@link CageModel}.
      */
     private readonly _cellM2: CellModel;
 
     /**
-     * The third {@link CellModel} of the {@link CageModel}.
+     * Cache for {@link SudokuNumsSet} of possible number's options
+     * for the second {@link CellModel} of the {@link CageModel}.
+     */
+    private readonly _cellM2NumsSet: ReadonlySudokuNumsSet;
+
+    /**
+     * Cache for the third {@link CellModel} of the {@link CageModel}.
      */
     private readonly _cellM3: CellModel;
+
+    /**
+     * Cache for {@link SudokuNumsSet} of possible number's options
+     * for the third {@link CellModel} of the {@link CageModel}.
+     */
+    private readonly _cellM3NumsSet: ReadonlySudokuNumsSet;
 
     /**
      * Constructs a new reducer of possible numbers for {@link CellModel}s
@@ -202,27 +120,31 @@ export class CageModel3Reducer implements CageModelReducer {
      */
     constructor(cageM: CageModel) {
         this._cageM = cageM;
+
+        // [PERFORMANCE] Caching references for faster access in the `reduce` method.
+        this._combosSet = cageM.comboSet;
         this._cellM1 = cageM.cellMs[0];
+        this._cellM1NumsSet = this._cellM1._numOptsSet;
         this._cellM2 = cageM.cellMs[1];
+        this._cellM2NumsSet = this._cellM2._numOptsSet;
         this._cellM3 = cageM.cellMs[2];
+        this._cellM3NumsSet = this._cellM3._numOptsSet;
     }
 
     /**
      * @see CageModelReducer.reduce
      */
     reduce(reduction: MasterModelReduction): void {
-        const combos = this._cageM.comboSet.combos;
-
         const referenceReductionStates = ALL_REDUCTION_STATES[this._cageM.cage.sum];
-        const cellM1NumsBits = this._cellM1._numOptsSet.bitStore;
-        const cellM2NumsBits = this._cellM2._numOptsSet.bitStore;
-        const cellM3NumsBits = this._cellM3._numOptsSet.bitStore;
+        const cellM1NumsBits = this._cellM1NumsSet.bitStore;
+        const cellM2NumsBits = this._cellM2NumsSet.bitStore;
+        const cellM3NumsBits = this._cellM3NumsSet.bitStore;
 
         let actualReductionStateCellM1 = 0;
         let actualReductionStateCellM2 = 0;
         let actualReductionStateCellM3 = 0;
 
-        for (const combo of combos) {
+        for (const combo of this._combosSet.combos) {
             const num1 = combo.number1;
             const num2 = combo.number2;
             const num3 = combo.number3;
@@ -250,7 +172,7 @@ export class CageModel3Reducer implements CageModelReducer {
                 actualReductionStateCellM2 |= (cellM2NumsBits & combo.numsSet.bitStore & ~reductionState.deleteNumsInCell2Bits);
                 actualReductionStateCellM3 |= (cellM3NumsBits & combo.numsSet.bitStore & ~reductionState.deleteNumsInCell3Bits);
             } else {
-                this._cageM.comboSet.deleteCombo(combo);
+                this._combosSet.deleteCombo(combo);
             }
         }
 
