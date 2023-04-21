76a05d331fa164bb582b8c906a2d67a1fce8ab59
vtsukur <flushdia@gmail.com>
2023-04-21 21:18:39 +0300

    [perf] [test] #194 - Running with array access is twice faster than function call

--- tests/perf/solver/math/sumCombos.perf.test.ts
+++ tests/perf/solver/math/sumCombos.perf.test.ts
@@ -4,13 +4,13 @@ import { SumCombos } from '../../../../src/solver/math';
 describe('Performance tests for `SumCombos`', () => {
 
     const TESTS_COUNT = 10;
-    const ITERATIONS = _.range(500_000);
+    const ITERATIONS = _.range(1_000_000);
 
     _.range(TESTS_COUNT).forEach(i => {
 
         test(`Number combinations to form a sum out of 5 unique numbers [${i}]`, () => {
             ITERATIONS.forEach(() => {
-                SumCombos.enumerate(25, 5);
+                SumCombos.BY_COUNT_BY_SUM[5][25];
             });
         });
 
