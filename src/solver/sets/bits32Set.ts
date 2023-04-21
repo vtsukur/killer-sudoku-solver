6873536fc2c6e610f9acaf0c12e2323657076842
vtsukur <flushdia@gmail.com>
2023-04-21 11:55:28 +0300

    [doc] Fixing TSDoc generation by adding missing import

--- src/solver/sets/bits32Set.ts
+++ src/solver/sets/bits32Set.ts
@@ -1,4 +1,6 @@
 import { BitStore32, NumsSet, ReadonlyNumsSet } from './numsSet';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { BaseReadonlySudokuNumsSet } from './sudokuNumsSet';
 
 export interface ReadonlyBits32Set<T> extends ReadonlyNumsSet<ReadonlyBits32Set<T>> {
 
