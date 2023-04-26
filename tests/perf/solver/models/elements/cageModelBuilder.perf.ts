52bf2cdbf0cc134e0c2c843cbdb50331d9c5074c
vtsukur <flushdia@gmail.com>
2023-04-26 17:41:06 +0300

    [perf] [test] [refactoring] #198 -- Simplifyingreusable `CageModel` creator method to avoid reduction

--- tests/perf/solver/models/elements/cageModelBuilder.perf.ts
+++ tests/perf/solver/models/elements/cageModelBuilder.perf.ts
@@ -1,11 +1,10 @@
 import { Cage } from '../../../../../src/puzzle/cage';
 import { Cell } from '../../../../../src/puzzle/cell';
-import { MasterModelReduction } from '../../../../../src/solver/strategies/reduction/masterModelReduction';
 import { CachedNumRanges } from '../../../../../src/util/cachedNumRanges';
 import { LockableCageModel } from '../../strategies/reduction/lockableCageModel';
 import { LockableCellModel } from '../../strategies/reduction/lockableCellModel';
 
-export const createAndInitPerfCageM = (cellCount: number, sum: number, reduction?: MasterModelReduction) => {
+export const createAndInitPerfCageM = (cellCount: number, sum: number) => {
     const cells = CachedNumRanges.ZERO_TO_N_LTE_81[cellCount].map(index => Cell.at(0, index));
     const cellMs = cells.map(cell => new LockableCellModel(cell));
 
@@ -16,7 +15,7 @@ export const createAndInitPerfCageM = (cellCount: number, sum: number, reduction
         cellM.addWithinCageModel(cageM);
     }
 
-    cageM.initialReduce(reduction);
+    cageM.initialReduce();
 
     return cageM;
 };
