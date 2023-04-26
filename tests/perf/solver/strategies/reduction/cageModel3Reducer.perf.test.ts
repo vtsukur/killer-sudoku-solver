a2ed0e385ba0fff22dfbd35328f186f316762352
vtsukur <flushdia@gmail.com>
2023-04-26 17:38:10 +0300

    [perf] [test] [refactoring] #198 -- Applying reusable `CageModel` creator method in `CageModel3Reducer` performance tests

--- tests/perf/solver/strategies/reduction/cageModel3Reducer.perf.test.ts
+++ tests/perf/solver/strategies/reduction/cageModel3Reducer.perf.test.ts
@@ -1,13 +1,10 @@
-import { Cell } from '../../../../../src/puzzle/cell';
-import { Cage } from '../../../../../src/puzzle/cage';
 import { CageModel } from '../../../../../src/solver/models/elements/cageModel';
 import { SudokuNumsSet } from '../../../../../src/solver/sets';
 import { Combo } from '../../../../../src/solver/math';
-import { LockableCellModel } from './lockableCellModel';
-import { LockableCageModel } from './lockableCageModel';
 import { ComparablePerformanceTestConfig, doRunFunctionalAndPerformanceTests } from './commons';
 import { CageModel3FullReducer } from '../../../../../src/solver/strategies/reduction/archive/cageModel3FullReducer';
 import { CageModel3Reducer } from '../../../../../src/solver/strategies/reduction/cageModel3Reducer';
+import { createAndInitPerfCageM } from '../../models/elements/cageModelBuilder.perf';
 
 const IS_RUN_SLOWER_ALTERNATIVES = false;
 
@@ -15,7 +12,7 @@ describe('Performance tests for `CageModel3Reducer`', () => {
 
     test('Comparable test for `CageModel` of sum 8, 2 `Combo`s and 8 present numbers', () => {
         runComparablePerformanceTests({
-            createReferenceCageModel: () => createReferenceCageM(8),
+            createReferenceCageModel: () => createAndInitPerfCageM(3, 8),
             prepareForReduction: (cageM, reduction) => {
                 reduction.tryReduceNumOpts(cageM.cellMs[0], SudokuNumsSet.of(1, 2, 4, 5));
                 expect(cageM.cellMs[0].numOpts()).toEqual([ 1, 2, 4, 5 ]);
@@ -45,7 +42,7 @@ describe('Performance tests for `CageModel3Reducer`', () => {
 
     test('Comparable test from real production scenario #1', () => {
         runComparablePerformanceTests({
-            createReferenceCageModel: () => createReferenceCageM(19),
+            createReferenceCageModel: () => createAndInitPerfCageM(3, 19),
             prepareForReduction: (cageM) => {
                 cageM.cellMs[0].reduceNumOpts(SudokuNumsSet.of(2, 3, 4, 5, 8, 9));
                 cageM.cellMs[1].reduceNumOpts(SudokuNumsSet.of(8));
@@ -65,26 +62,6 @@ describe('Performance tests for `CageModel3Reducer`', () => {
             }
         });
     });
-
-    const createReferenceCageM = (sum: number) => {
-        const cell1 = Cell.at(0, 0);
-        const cell2 = Cell.at(0, 1);
-        const cell3 = Cell.at(0, 2);
-        const cage = Cage.ofSum(sum).withCell(cell1).withCell(cell2).withCell(cell3).new();
-
-        const cellM1 = new LockableCellModel(cell1);
-        const cellM2 = new LockableCellModel(cell2);
-        const cellM3 = new LockableCellModel(cell3);
-        const cageM = new LockableCageModel(cage, [ cellM1, cellM2, cellM3 ]);
-
-        cellM1.addWithinCageModel(cageM);
-        cellM2.addWithinCageModel(cageM);
-        cellM3.addWithinCageModel(cageM);
-
-        cageM.initialReduce();
-
-        return cageM;
-    };
 
     const runComparablePerformanceTests = (config: ComparablePerformanceTestConfig) => {
         if (IS_RUN_SLOWER_ALTERNATIVES) {
