3c93d83f7651a9a40f2d0136cc06bfe969fa858a
vtsukur <flushdia@gmail.com>
2023-04-27 20:56:02 +0200

    [doc] Improven TSDoc

--- src/solver/strategies/reduction/cageModelReducer.ts
+++ src/solver/strategies/reduction/cageModelReducer.ts
@@ -8,7 +8,7 @@ import { MasterModelReduction } from './masterModelReduction';
 
 /**
  * Reduces possible numbers for {@link CellModel}s within a {@link CageModel}
- * by checking the validity of numbers' options given possible {@link Combo}s for the {@link CageModel}.
+ * by checking the validity of numbers given possible {@link Combo}s for the {@link CageModel}.
  *
  * @public
  */
@@ -16,7 +16,7 @@ export interface CageModelReducer {
 
     /**
      * Reduces possible numbers for {@link CellModel}s
-     * by checking the validity of numbers' option given possible {@link Combo}s for the {@link CageModel}.
+     * by checking the validity of numbers given possible {@link Combo}s for the {@link CageModel}.
      *
      * @param reduction - {@link MasterModelReduction} that tracks deleted number options.
      */
