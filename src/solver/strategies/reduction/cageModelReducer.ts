bafec26ca7d9cd32f16c00ebf7d1b49584773799
vtsukur <flushdia@gmail.com>
2023-05-20 21:28:10 +0300

    [doc] Improving TSDoc

--- src/solver/strategies/reduction/cageModelReducer.ts
+++ src/solver/strategies/reduction/cageModelReducer.ts
@@ -11,6 +11,17 @@ import { MasterModelReduction } from './masterModelReduction';
  * by checking the validity of _currently possible numbers_
  * given _possible {@link Combo}s_ for the {@link CageModel}.
  *
+ * For example, let's assume we have the {@link CageModel} of sum `11` of size 2
+ * with the `Combo` of numbers `[5, 6]` as the only _possible {@link Combo}_.
+ * Let's take that the first {@link CellModel} has the possible number `5`,
+ * and the second {@link CellModel} has both `5` and `6`.
+ * In this case, reduction should happen for the second {@link CellModel}
+ * by removing `5` as the _possible number_,
+ * effectively hinting that the first {@link CellModel} has the number `5` and
+ * the second {@link CellModel} has the number `6`.
+ *
+ * The {@link MasterModelReduction} object should reflect the reduction action.
+ *
  * @public
  */
 export interface CageModelReducer {
